0a4c3709a96f1753d72fdbef14573e790769b9b8
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2019-11-18 18:46:09 +0100

    first steps towards matrix-based CG

--- src/probnum/linalg/linear_solvers.py
+++ src/probnum/linalg/linear_solvers.py
@@ -64,20 +64,22 @@ class MatrixBasedConjugateGradients(ProbabilisticLinearSolver):
     method described in [1]_.
 
     .. [1] Hennig, P., Probabilistic Interpretation of Linear Solvers, *SIAM Journal on Optimization*, 2015, 25, 234-260
+
+    Attributes
+    ----------
+    H_mean : array-like
+        Mean of the Gaussian prior on :math:`H=A^{-1}`.
+    H_cov_kronfac
+        Kronecker factor of the covariance :math:`W_H \\otimes W_H` of the Gaussian prior on :math:`H=A^{-1}`.
     """
 
-    def __init__(self, prior_mean_inverse, prior_cov_kron_fac):
-        """
+    def __init__(self, H_mean, H_cov_kronfac):
+        self.H_mean = H_mean
+        self.H_cov_kronfac = H_cov_kronfac
 
-        Parameters
-        ----------
-        prior_mean_inverse
-        prior_cov_kron_fac
+    def solve(self, A, b, maxiter=None, resid_tol=10 ** -6, reorth=False):
         """
-
-    def solve(self, A, b, maxiter=None, reorth=False):
-        """
-        Solve the given linear system.
+        Solve the given linear system for symmetric, positive-definite :math:`A`.
 
         Parameters
         ----------
@@ -87,6 +89,9 @@ class MatrixBasedConjugateGradients(ProbabilisticLinearSolver):
             The right-hand-side of the linear system.
         maxiter : int
             Maximum number of iterations.
+        resid_tol : float
+            Residual tolerance. If :math:`\\lVert r_i \\rVert = \\lVert Ax_i - b \\rVert < \\text{tol}`, the iteration
+            terminates.
         reorth : bool, default=False
             Reorthogonalize search directions at each step with all previous ones.
 
@@ -98,7 +103,7 @@ class MatrixBasedConjugateGradients(ProbabilisticLinearSolver):
         A = np.asarray(A)  # make sure this doesn't de-sparsify the matrix, replace with more general type checking
         b = np.asarray(b)
 
-        # Check for correct dimensions
+        # Check linear system dimensionality
         _check_linear_system(A=A, b=b)
 
         # Setup
@@ -106,28 +111,43 @@ class MatrixBasedConjugateGradients(ProbabilisticLinearSolver):
         if maxiter is None:
             maxiter = n * 10
 
-        # Matrix-based Gaussian inference
+        # Initialization
         iter = 0
+        x = np.matmul(self.H_mean, b)
+        resid = np.matmul(A, x) - b
+
+        # Iteration
         while True:
 
             # Compute search direction
+            search_dir = - np.matmul(self.H_mean, resid)
 
-            # Reorthogonalize to all previous search directions
             if reorth:
+                # Reorthogonalize to all previous search directions
                 s = 0
             else:
+                # Orthogonalization
                 s = 0
 
             # Perform action and observe
+            obs = np.matmul(A, search_dir)
 
             # Compute step size
+            step_size = - np.dot(search_dir, resid) / np.dot(search_dir, obs)
 
-            # Stopping conditions
+            # Step and residual update
+            x = x + step_size * search_dir
+            resid = resid + step_size * obs
+
+            # Mean and covariance update
+
+            # Stopping criteria
             if iter == maxiter - 1:
                 # Maximum iterations
                 break
-
+            if np.linalg.norm(resid) < resid_tol:
                 # Residual below error tolerance
+                break
 
                 # Uncertainty-based
 
