ad86d8e3442e553993020f7a175cc1f09368f033
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-01-09 22:08:13 +0100

    img folder as custom static files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,7 +182,7 @@ html_favicon = "img/favicons/favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["_static", "img"]
 
 
 def setup(app):
