bc2ee9fa2831dde6ae149f93ef293ca6728aff3c
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-08-24 13:33:18 +0200

    mini benchmark for ivp solver written

---
+++ benchmarks/ivpsolve.py
@@ -0,0 +1,36 @@
+"""
+Benchmarks for probabilistic ivp solvers
+"""
+import numpy as np
+from probnum.diffeq import lotkavolterra, probsolve_ivp
+from probnum.prob import RandomVariable, Dirac
+
+
+def load_lotkavolterra():
+    """Load LV system as a basic IVP."""
+    initrv = RandomVariable(distribution=Dirac(np.array([20, 20])))
+    return lotkavolterra(timespan=[0, 0.55], initrv=initrv, params=(0.5, 0.05, 0.5, 0.05))
+
+
+class IVPSolve:
+    """Benchmark ODE-filter and ODE-smoother on small steps with high-order priors"""
+
+    param_names = ["method", "precond"]
+    params = [["eks0", "ekf0"], ["with", "without"]]
+
+    def setup(self, method, precond):
+        self.ivp = load_lotkavolterra()
+        self.stepsize = 1e-3
+
+    def time_solve(self, method, precond):
+        if precond == "with":
+            probsolve_ivp(self.ivp, method=method, which_prior="ibm4", step=self.stepsize, precond_step=self.stepsize)
+        else:
+            probsolve_ivp(self.ivp, method=method, which_prior="ibm4", step=self.stepsize, precond_step=1.0)
+
+    def peakmem_solve(self, method, precond):
+        if precond == "with":
+            probsolve_ivp(self.ivp, method=method, which_prior="ibm4", step=self.stepsize, precond_step=self.stepsize)
+        else:
+            probsolve_ivp(self.ivp, method=method, which_prior="ibm4", step=self.stepsize, precond_step=1.0)
+            
