5189a66a359167d1208524551cebb6b12d14156e
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-08-24 16:59:33 +0200

    issubclass isinstance

--- src/probnum/filtsmooth/gaussfiltsmooth/_utils.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/_utils.py
@@ -10,13 +10,13 @@ from probnum.filtsmooth.statespace import (
 
 def is_cont_disc(dynamod, measmod):
     """Checks whether the state space model is continuous-discrete."""
-    dyna_is_cont = issubclass(type(dynamod), ContinuousModel)
-    meas_is_disc = issubclass(type(measmod), DiscreteModel)
+    dyna_is_cont = isinstance(dynamod, ContinuousModel)
+    meas_is_disc = isinstance(measmod, DiscreteModel)
     return dyna_is_cont and meas_is_disc
 
 
 def is_disc_disc(dynamod, measmod):
     """Checks whether the state space model is discrete-discrete."""
-    dyna_is_disc = issubclass(type(dynamod), DiscreteModel)
-    meas_is_disc = issubclass(type(measmod), DiscreteModel)
+    dyna_is_disc = isinstance(dynamod, DiscreteModel)
+    meas_is_disc = isinstance(measmod, DiscreteModel)
     return dyna_is_disc and meas_is_disc
