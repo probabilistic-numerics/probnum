ae4fb8ba2d0da30cd201c287a688491d02fc3860
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-26 08:54:42 +0200

    Better and more explanatory script

--- script.py
+++ script.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 
-toxini = Path("tox.ini")
-pyprojecttoml = Path("pyproject.toml")
+TOXINI_FILE = Path("tox.ini")
+PYPROJECTTOML_FILE = Path("pyproject.toml")
+GLOBAL_DISABLES = {
+    "invalid-name",
+    "no-else-raise",
+    "no-else-return",
+}
 
 
-tox_lines = toxini.read_text().splitlines()
+# Parse ./tox.ini
+tox_lines = TOXINI_FILE.read_text().splitlines()
 tox_pylint_lines = [l for l in tox_lines if l.strip().startswith("pylint")]
-tox_disables = set([i for l in tox_pylint_lines for i in l.split('"')[1].split(",")])
+tox_disables = {i for l in tox_pylint_lines for i in l.split('"')[1].split(",")}
 
 
-pyproject_lines = pyprojecttoml.read_text().splitlines()
+# Parse ./pyproject.toml
+pyproject_lines = PYPROJECTTOML_FILE.read_text().splitlines()
 is_in_disable_string = False
 pyproject_disables = set()
 for line in pyproject_lines:
@@ -23,9 +30,45 @@ for line in pyproject_lines:
         is_in_disable_string = True
 
 
-assert pyproject_disables.difference(tox_disables) == {
-    "invalid-name",
-    "no-else-raise",
-    "no-else-return",
-}
-assert len(tox_disables.difference(pyproject_disables)) == 0
+# Check correctness and raise errors
+try:
+    in_pyproject_butnot_tox = pyproject_disables.difference(tox_disables).difference(
+        GLOBAL_DISABLES
+    )
+    assert not in_pyproject_butnot_tox
+except AssertionError:
+    raise Exception(
+        f"""
+    The following pylint messages seem to be disabled in `./pyproject.toml`, but not in
+    any of the pylint calls in `./tox.ini`: {in_pyproject_butnot_tox}. This could have
+    two reasons with different solutions:
+    1. You fixed one or many pylint errors in one of the modules and there is no module
+       left that needs this specific message disabled. Then, please also remove it from
+       the `./pyproject.toml` file such that pylint uses the most up-to-date
+       configuration file.
+    2. You added a new global exception to `./pyproject.toml` after deciding that this
+       is a message that we do not want to enforce anywhere currently. Then, please add
+       this exception also to the `GLOBAL_DISABLES` variable in this this python script
+       (`./github/workflows/pylint_check.py`).
+
+    If you are not sure what exactly you are supposed to do, or if you think that this
+    message is wrong please feel free to ping @nathanaelbosch.
+    """
+    )
+
+
+try:
+    in_tox_butnot_pyproject = tox_disables.difference(pyproject_disables)
+    assert not in_tox_butnot_pyproject
+except AssertionError:
+    raise Exception(
+        f"""
+    The following pylint messages seem to be disabled in `./tox.ini`, but not in
+    `./pyproject.toml`: {in_tox_butnot_pyproject}. Please make sure to add them to
+    `./pyproject.toml` such that pylint does not raise any warnings that are not
+    supposed to be fixed right now.
+
+    If you are not sure what exactly you are supposed to do, or if you think that this
+    message is wrong please feel free to ping @nathanaelbosch.
+    """
+    )
