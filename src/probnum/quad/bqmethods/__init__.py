244f33b19ad5066a4c8f1a8c90ab028eb6ae34bd
agessner <agessner@tue.mpg.de>
2020-10-15 18:41:45 +0200

    moved BQ methods into separate package

---
+++ src/probnum/quad/bqmethods/__init__.py
@@ -0,0 +1,1 @@
+from probnum.quad.bqmethods import *
---
+++ src/probnum/quad/bqmethods/bqmethods.py
@@ -0,0 +1,83 @@
+
+import abc
+from typing import Tuple, Dict
+
+
+class BayesianQuadrature(abc.ABC):
+    """
+    An abstract base class for Bayesian quadrature methods.
+
+    This class is designed to be subclassed by implementations of Bayesian quadrature
+    with an :meth:`integrate` method.
+    """
+
+    def integrate(self, fun, fun0, domain, nevals, **kwargs):
+        """
+        Integrate the function ``fun``.
+
+        Parameters
+        ----------
+        fun : function
+            Function to be integrated.
+        fun0 : RandomProcess
+            Stochastic process modelling function to be integrated.
+        domain : ndarray, shape=()
+            Domain to integrate over.
+        nevals : int
+            Number of function evaluations.
+        kwargs
+
+        Returns
+        -------
+
+        """
+        raise NotImplementedError
+
+class VanillaBayesianQuadrature(BayesianQuadrature):
+    """
+    Vanilla Bayesian quadrature.
+    """
+
+    def integrate(self, fun, fun0, domain, nevals, **kwargs) -> Tuple[RandomVariable, RandomProcess, Dict]:
+        """
+        Integrate the function ``fun``.
+
+        Parameters
+        ----------
+        fun : function
+            Function to be integrated.
+        fun0 : RandomProcess
+            Stochastic process modelling function to be integrated.
+        domain : ndarray, shape=()
+            Domain to integrate over.
+        nevals : int
+            Number of function evaluations.
+
+        Returns
+        -------
+        F : RandomVariable
+        The integral of ``func`` from ``a`` to ``b``.
+        fun0 : RandomProcess
+            Stochastic process modelling the function to be integrated after ``neval``
+            observations.
+        info : dict
+            Information on the performance of the method.
+        """
+
+        # Initialization
+        F = None
+
+        # Iteration
+        for _ in range(nevals):
+            # Predictive Distribution
+            # fun_pred = None
+
+            # Observation
+
+            # Update Distribution
+            self.fun0 = None
+
+        # Information on result
+        info = {"model_fit_diagnostic": None}
+
+        return F, self.fun0, info
