9224ca3a964132b9b793afd99269d8706d91c9e5
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-10-22 15:14:21 +0200

    added init files

--- src/probnum/filtsmooth/__init__.py
+++ src/probnum/filtsmooth/__init__.py
@@ -17,9 +17,9 @@ __all__ = [
     "SDE",
     "LinearSDE",
     "LTISDE",
-    "DiscreteGaussianModel",
-    "DiscreteGaussianLinearModel",
-    "DiscreteGaussianLTIModel",
+    "DiscreteGaussian",
+    "DiscreteLinearGaussian",
+    "DiscreteLTIGaussian",
     "FiltSmoothPosterior",
     "KalmanPosterior",
     "generate_cd",
@@ -28,9 +28,9 @@ __all__ = [
 
 # Set correct module paths (for superclasses).
 # Corrects links and module paths in documentation.
-Transition.__module__ = "probnum.filtsmooth"
-SDE.__module__ = "probnum.filtsmooth"
-LinearSDE.__module__ = "probnum.filtsmooth"
-DiscreteGaussianModel.__module__ = "probnum.filtsmooth"
-DiscreteGaussianLinearModel.__module__ = "probnum.filtsmooth"
+Transition.__module__ = "probnum.filtsmooth.statespace"
+SDE.__module__ = "probnum.filtsmooth.statespace"
+LinearSDE.__module__ = "probnum.filtsmooth.statespace"
+DiscreteGaussian.__module__ = "probnum.filtsmooth.statespace"
+DiscreteLinearGaussian.__module__ = "probnum.filtsmooth.statespace"
 KalmanPosterior.__module__ = "probnum.filtsmooth"
--- src/probnum/filtsmooth/statespace/__init__.py
+++ src/probnum/filtsmooth/statespace/__init__.py
@@ -1,3 +1,5 @@
-from .transition import *
-from .sde import *
+from .coordinate_change import *
 from .discrete_transition import *
+from .integrator import *
+from .sde import *
+from .transition import *
