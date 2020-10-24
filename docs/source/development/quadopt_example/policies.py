8682b71c0a09807b25bfea05c9f54e3c6fd84460
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-10-24 13:29:41 +0200

    Documentation: PN Method example added and development tutorial updated to compositional pattern (#181)
    * developer guide on PN methods updated to type hints and support for type hints in docs added
    
    * examples formatted with black
    
    * experimented with new readme
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * added example of a PN method
    
    * black and sphinx version
    
    * plotting defaults updated and intro to PN method introduction
    
    * more explanation of PN approach added to notebook
    
    * more flavor text
    
    * .
    
    * some formatting
    
    * policies added
    
    * adjusted matplotlib stylefile and simplified PN methmethod tutorial
    
    * updated PN example
    
    * restructured doc tutorials and changed readme text
    
    * sphinx galleries for tutorial notebooks
    
    * developer guide updated to include sphinx galleries
    
    * minor fixes to developer guide on example notebook
    
    * notes on simple 1D PN method added
    
    * more notes on tutorial
    
    * quickstart notebook adapted to PN method notebook
    
    * minor updates to notebook
    
    * added some files from rv refactor
    
    * typehints corrected after RV refactoring
    
    * updates to problem setup
    
    * added belief over function value and some css styling
    
    * updated contributing guide with pre-commit hooks
    
    * merged master
    
    * blacked
    
    * minor edits to pn method notebook'
    
    * .
    
    * legends added
    
    * bug in random objective fixed
    
    * animation of algorithm added
    
    * animation completed
    
    * first complete draft of notebook added
    
    * changed some absolute links to relative
    
    * blacked notebooks
    
    * reran notebooks
    
    * draft for generic PN class
    
    * .
    
    * draft for developer tutorial on writing tests
    
    * developer notebook on tests
    
    * .
    
    * thumbnails for developer guides
    
    * minor corrections to PN methods notebook
    
    * pn method example final touches
    
    * .
    
    * developer guide on documentation updated
    
    * testing signed commits
    
    * developer guide on unit testing added
    
    * .
    
    * PN method implementation dev tutorial
    
    * fixed minor rendering issues of dev notebooks
    
    * author attribution stratified
    
    * author formatting changed
    
    * updates to author attribution
    
    * section on argument and api types added to PN dev tutorial
    
    * updates to tutorial notebooks
    
    * parameter uncertainty added to tutorial plot
    
    * .
    
    * fixed some rendering of text after equations
    
    * samples added to parameter belief plot
    
    * correct computation of 3d credible region
    
    * fixed credible region and plotly experiment
    
    * plotly test v2
    
    * plotly test v3
    
    * plotly test notebook renderer
    
    * .
    
    * updated and reformatted animation for pnmethod tutorial
    
    * images of contributors are now shown
    
    * wording updated
    
    * played with algorithm parameters
    
    * policy updated, convergence criterion updated, exact problem added
    
    * prior as posterior
    
    * Lukas feedback for pn method tutorial
    
    * readded notebook thumbnails for ode tutorials
    
    * implemented feedback from PN Software Interest Group
    
    * updated style and pinned sphinx-rtd-theme version
    
    * blackd
    
    * class structure for ProbabilisticQuadraticOptimizer
    
    * probquadprog postprocess function updated
    
    * readded callback function
    
    * updated documentation of probsolve_qp and fixed some bugs
    
    * fixed pylint issues and added automatic prior determination
    
    * updated contributor order to alphabetical
    
    * flavor text for the pn method dev tutorial
    
    * added correct random number generator to interface
    
    * final draft of the text in the developer tutorial
    
    * broken link fixed
    
    * finalized pn method dev tutorial
    
    * fixed tex rendering issue
    
    * contributors updated
    
    * isort settings to match black
    
    * reverted black and isort order
    
    * Nico and Alex's feedback implemented

---
+++ docs/source/development/quadopt_example/policies.py
@@ -0,0 +1,60 @@
+"""
+Policies for 1D quadratic optimization.
+"""
+
+from typing import Callable, Optional
+
+import numpy as np
+
+import probnum as pn
+import probnum.random_variables as rvs
+from probnum.type import FloatArgType, RandomStateArgType
+
+
+def explore_exploit_policy(
+    fun: Callable[[FloatArgType], FloatArgType],
+    fun_params0: pn.RandomVariable,
+    random_state: RandomStateArgType = None,
+) -> float:
+    """
+    Policy exploring around the estimate of the minimum based on the certainty about the
+    parameters.
+
+    Parameters
+    ----------
+    fun :
+        One-dimensional objective function.
+    fun_params0 :
+        Belief over the parameters of the quadratic objective.
+    random_state :
+        Random state of the policy. If None (or np.random), the global np.random state
+        is used. If integer, it is used to seed the local
+        :class:`~numpy.random.RandomState` instance.
+    """
+    a0, b0, c0 = fun_params0
+    return (
+        -b0.mean / a0.mean
+        + rvs.Normal(0, np.trace(fun_params0.cov), random_state=random_state).sample()
+    )
+
+
+def stochastic_policy(
+    fun: Callable[[FloatArgType], FloatArgType],
+    fun_params0: pn.RandomVariable,
+    random_state: RandomStateArgType = None,
+) -> float:
+    """
+    Policy returning a random action.
+
+    Parameters
+    ----------
+    fun :
+        One-dimensional objective function.
+    fun_params0 :
+        Belief over the parameters of the quadratic objective.
+    random_state :
+        Random state of the policy. If None (or np.random), the global np.random state
+        is used. If integer, it is used to seed the local
+        :class:`~numpy.random.RandomState` instance.
+    """
+    return rvs.Normal(mean=0.0, cov=1.0, random_state=random_state).sample()
