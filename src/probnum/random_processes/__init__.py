b28cc67a70c53e2bccf5f997e3f2fd77a24aa897
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-11-04 11:17:23 +0100

    structure updated

--- src/probnum/random_processes/__init__.py
+++ src/probnum/random_processes/__init__.py
@@ -1,1 +1,11 @@
+from ._gaussian_process import GaussianProcess, GaussMarkovProcess
 from ._random_process import RandomProcess
+from ._utils import asrandproc
+
+# Public classes and functions. Order is reflected in documentation.
+__all__ = ["asrandproc", "RandomProcess", "GaussianProcess", "GaussMarkovProcess"]
+
+# Set correct module paths. Corrects links and module paths in documentation.
+RandomProcess.__module__ = "probnum.random_processes"
+GaussianProcess.__module__ = "probnum.random_processes"
+GaussMarkovProcess.__module__ = "probnum.random_processes"
