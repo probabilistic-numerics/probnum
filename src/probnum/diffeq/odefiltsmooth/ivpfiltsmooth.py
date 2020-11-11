d23437d8c0571b0aa2433afd9aa31268ab05368d
Nathanael Bosch <nathanael.bosch@tum.de>
2020-11-11 14:48:51 +0100

    Implemented the "correct" dynamic diffusion and error esimation
    This corresponds to the setting as described in [1].
    
    [1] Schober, M., Särkkä, S. and Hennig, P.. A probabilistic model for the
    numerical solution of initial value problems. Statistics and Computing, 2019.

--- src/probnum/diffeq/odefiltsmooth/ivpfiltsmooth.py
+++ src/probnum/diffeq/odefiltsmooth/ivpfiltsmooth.py
@@ -33,8 +33,7 @@ class GaussianIVPFilter(odesolver.ODESolver):
         if not issubclass(type(gaussfilt.dynamicmodel), ODEPrior):
             raise ValueError("Please initialise a Gaussian filter with an ODEPrior")
         self.gfilt = gaussfilt
-        self.sigma_squared_global = 0.0
-        self.sigma_squared_current = 0.0
+        self.sigma_squared_mle = 1.0
         self.with_smoothing = with_smoothing
         super().__init__(ivp)
 
@@ -43,27 +42,44 @@ class GaussianIVPFilter(odesolver.ODESolver):
 
     def step(self, t, t_new, current_rv, **kwargs):
         """Gaussian IVP filter step as nonlinear Kalman filtering with zero data."""
+        # 0. Obtain the diffusion matrix; required for calibration / error estimation
+        discrete_dynamics = self.gfilt.dynamod.discretise(t_new - t)
+        diffmat = discrete_dynamics.diffusionmatrix(t_new)
+
+        # 1. Predict
         pred_rv, _ = self.gfilt.predict(t, t_new, current_rv, **kwargs)
+
+        # 2. Measure
+        meas_rv, info = self.gfilt.measmod.transition_rv(pred_rv, t_new, **kwargs)
+
+        # 3. Estimate the diffusion (sigma squared)
+        self.sigma_squared_mle = self._estimate_diffusion(pred_rv, meas_rv)
+        # 3.1. Adjust the prediction covariance to include the diffusion
+        pred_rv = Normal(
+            pred_rv.mean, pred_rv.cov + (self.sigma_squared_mle - 1) * diffmat
+        )
+        # 3.2 Update the measurement covariance (measure again)
+        meas_rv, info = self.gfilt.measmod.transition_rv(pred_rv, t_new, **kwargs)
+
+        # 4. Update
         zero_data = 0.0
         filt_rv, meas_rv, info = self.gfilt.update(t_new, pred_rv, zero_data, **kwargs)
-        errorest, self.sigma_squared_current = self._estimate_error(
-            filt_rv, meas_rv, info
-        )
-        return filt_rv, errorest
 
-    def method_callback(self, time, current_guess, current_error):
-        """Update the sigma-squared (ssq) estimate."""
-        self.sigma_squared_global = (
-            self.sigma_squared_global
-            + (self.sigma_squared_current - self.sigma_squared_global) / self.num_steps
+        # 5. Error estimate
+        local_errors = self._estimate_local_error(
+            pred_rv, t_new, self.sigma_squared_mle * diffmat
         )
+        err = np.linalg.norm(local_errors)
+
+        return filt_rv, err
 
     def postprocess(self, times, rvs):
         """
         Rescale covariances with sigma square estimate,
         (if specified) smooth the estimate, return ODESolution.
         """
-        rvs = self._rescale(rvs)
+        if false:  # will become useful again for time-fixed diffusion models
+            rvs = self._rescale(rvs)
         odesol = super().postprocess(times, rvs)
         if self.with_smoothing is True:
             odesol = self._odesmooth(ode_solution=odesol)
@@ -71,7 +87,7 @@ class GaussianIVPFilter(odesolver.ODESolver):
 
     def _rescale(self, rvs):
         """Rescales covariances according to estimate sigma squared value."""
-        rvs = [Normal(rv.mean, self.sigma_squared_global * rv.cov) for rv in rvs]
+        rvs = [Normal(rv.mean, self.sigma_squared_mle * rv.cov) for rv in rvs]
         return rvs
 
     def _odesmooth(self, ode_solution, **kwargs):
@@ -107,40 +123,44 @@ class GaussianIVPFilter(odesolver.ODESolver):
         newrv = Normal(newmean, newcov)
         return newrv
 
-    def _estimate_error(self, filt_rv, meas_rv, info):
-        """
-        Error estimate.
+    def _estimate_local_error(self, pred_rv, t_new, calibrated_diffmat, **kwargs):
+        """Estimate the local errors
 
-        Estimates error as whitened residual using the
-        residual as weight vector.
+        This corresponds to the approach in [1], implemented such that it is compatible
+        with the EKF1 and UKF.
 
-        THIS IS NOT A PERFECT ERROR ESTIMATE, but this is a question of
-        research, not a question of implementation as of now.
+        References
+        ----------
+        .. [1] Schober, M., Särkkä, S. and Hennig, P..
+            A probabilistic model for the numerical solution of initial
+            value problems.
+            Statistics and Computing, 2019.
         """
-        # remove numerical issues w.r.t. symmetry and positive definiteness
-        std_like = np.linalg.cholesky(
-            0.5 * (meas_rv.cov + meas_rv.cov.T) + 1e-3 * np.eye(len(meas_rv.cov))
+        local_pred_rv = Normal(pred_rv.mean, calibrated_diffmat)
+        local_meas_rv, _ = self.gfilt.measmod.transition_rv(
+            local_pred_rv, t_new, **kwargs
         )
+        error = local_meas_rv.cov.diagonal()
+        return error
+
+    def _estimate_diffusion(self, pred_rv, meas_rv):
+        """Estimate the dynamic diffusion parameter sigma_squared
+
+        This corresponds to the approach in [1], implemented such that it is compatible
+        with the EKF1 and UKF.
+
+        References
+        ----------
+        .. [1] Schober, M., Särkkä, S. and Hennig, P..
+            A probabilistic model for the numerical solution of initial
+            value problems.
+            Statistics and Computing, 2019.
+        """
+        z, S = meas_rv.mean, meas_rv.cov
+        std_like = np.linalg.cholesky(meas_rv.cov)
         whitened_res = np.linalg.solve(std_like, meas_rv.mean)
-        ssq = whitened_res @ whitened_res / meas_rv.mean.size
-        abserrors = np.abs(whitened_res)
-        errorest = self._rel_and_abs_error(abserrors, filt_rv.mean)
-        return errorest, ssq
-
-    def _rel_and_abs_error(self, abserrors, currmn):
-        """
-        Returns maximum of absolute and relative error.
-        This way, both are guaranteed to be satisfied.
-        """
-        ordint, spatialdim = self.gfilt.dynamicmodel.ordint, self.ivp.dimension
-        h0_1d = np.eye(ordint + 1)[:, 0].reshape((1, ordint + 1))
-        projmat = np.kron(np.eye(spatialdim), h0_1d)
-        weights = np.ones(len(abserrors))
-        rel_error = (
-            (abserrors / np.abs(projmat @ currmn)) @ weights / np.linalg.norm(weights)
-        )
-        abs_error = abserrors @ weights / np.linalg.norm(weights)
-        return np.maximum(rel_error, abs_error)
+        ssq = whitened_res @ whitened_res / meas_rv.size
+        return ssq
 
     @property
     def prior(self):
