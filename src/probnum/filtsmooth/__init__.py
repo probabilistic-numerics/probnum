8376df9296fff27a097b386ce715e5082c4ddc58
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-11-16 11:34:31 +0100

    made statespace its own module

--- src/probnum/filtsmooth/__init__.py
+++ src/probnum/filtsmooth/__init__.py
@@ -3,43 +3,20 @@ Bayesian Filtering and Smoothing.
 """
 
 from .bayesfiltsmooth import *
-from .filtsmoothposterior import FiltSmoothPosterior
+from .filtsmoothposterior import *
 from .gaussfiltsmooth import *
-from .statespace import *
 
 # Public classes and functions. Order is reflected in documentation.
 __all__ = [
     "Kalman",
-    "IteratedKalman",
     "ContinuousEKFComponent",
-    "ContinuousUKFComponent",
     "DiscreteEKFComponent",
+    "ContinuousUKFComponent",
     "DiscreteUKFComponent",
     "UnscentedTransform",
-    "Transition",
-    "SDE",
-    "LinearSDE",
-    "LTISDE",
-    "DiscreteGaussian",
-    "DiscreteLinearGaussian",
-    "DiscreteLTIGaussian",
     "FiltSmoothPosterior",
     "KalmanPosterior",
-    "generate",
-    "matrix_fraction_decomposition",
-    "linear_sde_statistics",
+    "IteratedKalman",
     "StoppingCriterion",
     "FixedPointStopping",
-    "Preconditioner",
-    "TaylorCoordinates",
-    "Integrator",
 ]
-
-# Set correct module paths (for superclasses).
-# Corrects links and module paths in documentation.
-Transition.__module__ = "probnum.filtsmooth.statespace"
-SDE.__module__ = "probnum.filtsmooth.statespace"
-LinearSDE.__module__ = "probnum.filtsmooth.statespace"
-DiscreteGaussian.__module__ = "probnum.filtsmooth.statespace"
-DiscreteLinearGaussian.__module__ = "probnum.filtsmooth.statespace"
-KalmanPosterior.__module__ = "probnum.filtsmooth"
--- src/probnum/filtsmooth/statespace/__init__.py
+++ src/probnum/filtsmooth/statespace/__init__.py
@@ -3,3 +3,20 @@ from .integrator import *
 from .preconditioner import *
 from .sde import *
 from .transition import *
+
+
+__all__ = [
+    "Transition",
+    "SDE",
+    "LinearSDE",
+    "LTISDE",
+    "Integrator",
+    "IBM",
+    "IOUP",
+    "Matern",
+    "DiscreteGaussian",
+    "DiscreteLinearGaussian",
+    "DiscreteLTIGaussian",
+    "Preconditioner",
+    "TaylorCoordinates"
+]
