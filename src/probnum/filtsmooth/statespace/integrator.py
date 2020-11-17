ce74ae357d60ab22d44d04c5e39774cab4d96534
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-11-17 17:12:31 +0100

    docs

--- src/probnum/filtsmooth/statespace/integrator.py
+++ src/probnum/filtsmooth/statespace/integrator.py
@@ -20,7 +20,7 @@ from .preconditioner import TaylorCoordinates
 class Integrator:
     """
     An integrator is a special kind of SDE,
-    where the :math:`i`th coordinate models the :math:`i`th derivative.
+    where the :math:`i` th coordinate models the :math:`i` th derivative.
     """
 
     def __init__(self, ordint, spatialdim):
@@ -29,16 +29,16 @@ class Integrator:
 
     def proj2coord(self, coord: int) -> np.ndarray:
         """
-        Projection matrix to :math:`i`-th coordinates.
+        Projection matrix to :math:`i` th coordinates.
 
         Computes the matrix
 
         .. math:: H_i = \\left[ I_d \\otimes e_i \\right] P^{-1},
 
-        where :math:`e_i` is the :math:`i`-th unit vector,
-        that projects to the :math:`i`-th coordinate of a vector.
+        where :math:`e_i` is the :math:`i` th unit vector,
+        that projects to the :math:`i` th coordinate of a vector.
         If the ODE is multidimensional, it projects to **each** of the
-        :math:`i`-th coordinates of each ODE dimension.
+        :math:`i` th coordinates of each ODE dimension.
 
         Parameters
         ----------
