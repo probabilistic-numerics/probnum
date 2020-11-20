d2a7b13febb168714e52c053f34ff71caf019d2a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-11-20 18:49:10 +0100

    rbf kernel

--- src/probnum/kernels/_exponentiated_quadratic.py
+++ src/probnum/kernels/_exponentiated_quadratic.py
@@ -34,4 +34,16 @@ class ExpQuad(Kernel[_InputType]):
         return np.exp(-np.linalg.norm(x0 - x1) ** 2 / (2 * self.lengthscale ** 2))
 
     def __call__(self, x0: _InputType, x1: Optional[_InputType] = None) -> np.ndarray:
-        raise NotImplementedError
+        # Transform into 2d arrays
+        x0 = _utils.as_colvec(x0)
+        x1 = _utils.as_colvec(x1)
+
+        # Pre-compute norms with einsum for performance
+        x0_norm = np.einsum("ij,ij->i", x0, x0)
+        x1_norm = np.einsum("ij,ij->i", x1, x1)
+
+        # Kernel matrix
+        return np.exp(
+            -(x0_norm[:, None] + x1_norm[None, :] - 2 * np.dot(x0, x1.T))
+            / (2 * self.lengthscale ** 2)
+        )
