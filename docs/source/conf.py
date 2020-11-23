10cd36b14172dc95445d5453c1a7811b71efde07
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-11-23 08:49:15 +0100

    conf.py docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# probnum documentation build configuration file, created by
+# ProbNum documentation build configuration file, created by
 # sphinx-quickstart on Fri Nov  2 15:54:04 2019.
 #
 # This file is execfile()d with the current directory set to its
@@ -13,16 +13,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
 import os
 import sys
 from datetime import datetime
 
 from pkg_resources import get_distribution
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../../../probnum/src"))
 
 # -- General configuration ------------------------------------------------
