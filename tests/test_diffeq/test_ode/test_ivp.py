e25203334045348c355d0a9eb5d819a32597e6ff
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2020-11-24 18:31:01 +0100

    Adapted imports in IVP test

--- tests/test_diffeq/test_ode/test_ivp.py
+++ tests/test_diffeq/test_ode/test_ivp.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from probnum.diffeq.ode import ivp
+from probnum.diffeq.ode import ivp_examples
 from probnum.random_variables import Constant
 from tests.testing import NumpyAssertions
 
@@ -18,14 +19,14 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
     def test_logistic(self):
         """Test the logistic ODE convenience function."""
         rv = Constant(0.1)
-        lg1 = ivp.logistic(self.tspan, rv)
+        lg1 = ivp_examples.logistic(self.tspan, rv)
         self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.logistic(self.tspan, rv, params=(1.0, 1.0))
+        lg2 = ivp_examples.logistic(self.tspan, rv, params=(1.0, 1.0))
         self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
 
     def test_logistic_jacobian(self):
         rv = Constant(0.1)
-        lg1 = ivp.logistic(self.tspan, rv)
+        lg1 = ivp_examples.logistic(self.tspan, rv)
         random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
         random_point = 1 + np.random.rand(lg1.dimension)
         fd_approx = (
@@ -42,14 +43,14 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
 
     def test_fitzhughnagumo(self):
         rv = Constant(np.ones(2))
-        lg1 = ivp.fitzhughnagumo(self.tspan, rv)
+        lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
         self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.fitzhughnagumo(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+        lg2 = ivp_examples.fitzhughnagumo(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
         self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
 
     def test_fitzhughnagumo_jacobian(self):
         rv = Constant(np.ones(2))
-        lg1 = ivp.fitzhughnagumo(self.tspan, rv)
+        lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
         random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
         random_point = 1 + np.random.rand(lg1.dimension)
         fd_approx = (
@@ -66,13 +67,13 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
 
     def test_lotkavolterra(self):
         rv = Constant(np.ones(2))
-        lg1 = ivp.lotkavolterra(self.tspan, rv)
-        lg2 = ivp.lotkavolterra(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+        lg1 = ivp_examples.lotkavolterra(self.tspan, rv)
+        lg2 = ivp_examples.lotkavolterra(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
         self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
 
     def test_lotkavolterra_jacobian(self):
         rv = Constant(np.ones(2))
-        lg1 = ivp.lotkavolterra(self.tspan, rv)
+        lg1 = ivp_examples.lotkavolterra(self.tspan, rv)
         random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
         random_point = 1 + np.random.rand(lg1.dimension)
         fd_approx = (
@@ -92,9 +93,9 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         Test the SEIR ODE convenience function.
         """
         rv = Constant(np.array([1.0, 0.0, 0.0, 0.0]))
-        lg1 = ivp.seir(self.tspan, rv)
+        lg1 = ivp_examples.seir(self.tspan, rv)
         self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.seir(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+        lg2 = ivp_examples.seir(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
         self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
 
     def test_rigidbody(self):
@@ -102,12 +103,12 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         Test the rigidbody ODE convenience function.
         """
         rv = Constant(np.array([1.0, 1.0, 1.0]))
-        lg1 = ivp.rigidbody(self.tspan, rv)
+        lg1 = ivp_examples.rigidbody(self.tspan, rv)
         self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
 
     def test_rigidbody_jacobian(self):
         rv = Constant(np.array([1.0, 1.0, 1.0]))
-        lg1 = ivp.rigidbody(self.tspan, rv)
+        lg1 = ivp_examples.rigidbody(self.tspan, rv)
         random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
         random_point = 1 + np.random.rand(lg1.dimension)
         fd_approx = (
@@ -127,16 +128,16 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         Test the Van der Pol ODE convenience function.
         """
         rv = Constant(np.array([1.0, 1.0]))
-        lg1 = ivp.vanderpol(self.tspan, rv)
+        lg1 = ivp_examples.vanderpol(self.tspan, rv)
         self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.vanderpol(self.tspan, rv, params=(2.0,))
+        lg2 = ivp_examples.vanderpol(self.tspan, rv, params=(2.0,))
         self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-        lg3 = ivp.vanderpol(self.tspan, rv, params=2.0)
+        lg3 = ivp_examples.vanderpol(self.tspan, rv, params=2.0)
         self.assertEqual(issubclass(type(lg3), ivp.IVP), True)
 
     def test_vanderpol_jacobian(self):
         rv = Constant(np.array([1.0, 1.0]))
-        lg1 = ivp.vanderpol(self.tspan, rv)
+        lg1 = ivp_examples.vanderpol(self.tspan, rv)
         random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
         random_point = 1 + np.random.rand(lg1.dimension)
         fd_approx = (
