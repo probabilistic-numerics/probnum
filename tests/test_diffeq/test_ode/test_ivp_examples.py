6ad5309162e6db41b62e6ff060e864796077e854
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2020-12-01 18:40:11 +0100

    Adds IVP test cases, re-structures existing ones

--- tests/test_diffeq/test_ode/test_ivp_examples.py
+++ tests/test_diffeq/test_ode/test_ivp_examples.py
@@ -7,8 +7,8 @@ from probnum.random_variables import Constant
 from tests.testing import NumpyAssertions
 
 
-class TestExamples(unittest.TestCase, NumpyAssertions):
-    """Test cases for example IVPs: Lotka-Volterra, etc."""
+class TestConvenienceFunction(unittest.TestCase):
+    """Test case for correct object initialization"""
 
     def setUp(self):
         self.tspan = (0.0, 4.212)
@@ -17,9 +17,122 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         """Test the logistic ODE convenience function."""
         rv = Constant(0.1)
         lg1 = ivp_examples.logistic(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
         lg2 = ivp_examples.logistic(self.tspan, rv, params=(1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+
+    def test_fitzhughnagumo(self):
+        """Test the FHN IVP convenience function."""
+        rv = Constant(np.ones(2))
+        lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
+        lg2 = ivp_examples.fitzhughnagumo(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+
+    def test_lotkavolterra(self):
+        """Test the LV ODE convenience function."""
+        rv = Constant(np.ones(2))
+        lg1 = ivp_examples.lotkavolterra(self.tspan, rv)
+        lg2 = ivp_examples.lotkavolterra(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+
+    def test_seir(self):
+        """Test the SEIR ODE convenience function."""
+        rv = Constant(np.array([1.0, 0.0, 0.0, 0.0]))
+        lg1 = ivp_examples.seir(self.tspan, rv)
+        lg2 = ivp_examples.seir(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+
+    def test_rigidbody(self):
+        """Test the rigidbody ODE convenience function."""
+        rv = Constant(np.array([1.0, 1.0, 1.0]))
+        lg1 = ivp_examples.rigidbody(self.tspan, rv)
+
+        self.assertIsInstance(lg1, ivp.IVP)
+
+    def test_vanderpol(self):
+        """Test the Van der Pol ODE convenience function."""
+        rv = Constant(np.array([1.0, 1.0]))
+        lg1 = ivp_examples.vanderpol(self.tspan, rv)
+        lg2 = ivp_examples.vanderpol(self.tspan, rv, params=(2.0,))
+        lg3 = ivp_examples.vanderpol(self.tspan, rv, params=2.0)
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+        self.assertIsInstance(lg3, ivp.IVP)
+
+    def test_threebody(self):
+        """Test the Van der Pol ODE convenience function."""
+        rv = Constant(np.array([1.0, 1.0]))
+        lg1 = ivp_examples.threebody(self.tspan, rv)
+        lg2 = ivp_examples.threebody(self.tspan, rv, params=(0.012277471,))
+        lg3 = ivp_examples.threebody(self.tspan, rv, params=0.012277471)
+
+        self.assertIsInstance(lg1, ivp.IVP)
+        self.assertIsInstance(lg2, ivp.IVP)
+        self.assertIsInstance(lg3, ivp.IVP)
+
+
+class TestRHSEvaluation(unittest.TestCase, NumpyAssertions):
+    """Test cases that check the evaluation of IVP vector fields"""
+
+    def setUp(self):
+        self.tspan = (0.0, 4.212)
+
+    def test_logistic_rhs(self):
+        rv = Constant(0.1)
+        lg1 = ivp_examples.logistic(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_fitzhughnagumo_rhs(self):
+        rv = Constant(np.ones(2))
+        lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_lotkavolterra_rhs(self):
+        rv = Constant(np.ones(2))
+        lg1 = ivp_examples.lotkavolterra(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_seir_rhs(self):
+        rv = Constant(np.ones(4))
+        lg1 = ivp_examples.seir(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_rigidbody_rhs(self):
+        rv = Constant(np.ones(3))
+        lg1 = ivp_examples.rigidbody(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_vanderpol_rhs(self):
+        rv = Constant(np.ones(2))
+        lg1 = ivp_examples.vanderpol(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+    def test_threebody_rhs(self):
+        rv = Constant(np.ones(4))
+        lg1 = ivp_examples.threebody(self.tspan, rv)
+
+        self.assertEqual(lg1.rhs(0.1, rv).shape, rv.shape)
+
+
+class TestJacobianEvaluation(unittest.TestCase, NumpyAssertions):
+    """Test cases that check Jacobians of IVPs against finite differences"""
+
+    def setUp(self):
+        self.tspan = (0.0, 4.212)
 
     def test_logistic_jacobian(self):
         rv = Constant(0.1)
@@ -38,14 +151,6 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
 
-    def test_fitzhughnagumo(self):
-        """Test the FHN IVP convenience function."""
-        rv = Constant(np.ones(2))
-        lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp_examples.fitzhughnagumo(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-
     def test_fitzhughnagumo_jacobian(self):
         rv = Constant(np.ones(2))
         lg1 = ivp_examples.fitzhughnagumo(self.tspan, rv)
@@ -62,13 +167,6 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         self.assertAllClose(
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
-
-    def test_lotkavolterra(self):
-        """Test the LV ODE convenience function."""
-        rv = Constant(np.ones(2))
-        lg1 = ivp_examples.lotkavolterra(self.tspan, rv)
-        lg2 = ivp_examples.lotkavolterra(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
 
     def test_lotkavolterra_jacobian(self):
         rv = Constant(np.ones(2))
@@ -87,14 +185,6 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
 
-    def test_seir(self):
-        """Test the SEIR ODE convenience function."""
-        rv = Constant(np.array([1.0, 0.0, 0.0, 0.0]))
-        lg1 = ivp_examples.seir(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp_examples.seir(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-
     def test_seir_jacobian(self):
         rv = Constant(np.ones(4))
         lg1 = ivp_examples.seir(self.tspan, rv)
@@ -111,12 +201,6 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         self.assertAllClose(
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
-
-    def test_rigidbody(self):
-        """Test the rigidbody ODE convenience function."""
-        rv = Constant(np.array([1.0, 1.0, 1.0]))
-        lg1 = ivp_examples.rigidbody(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
 
     def test_rigidbody_jacobian(self):
         rv = Constant(np.array([1.0, 1.0, 1.0]))
@@ -135,16 +219,6 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
 
-    def test_vanderpol(self):
-        """Test the Van der Pol ODE convenience function."""
-        rv = Constant(np.array([1.0, 1.0]))
-        lg1 = ivp_examples.vanderpol(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp_examples.vanderpol(self.tspan, rv, params=(2.0,))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-        lg3 = ivp_examples.vanderpol(self.tspan, rv, params=2.0)
-        self.assertEqual(issubclass(type(lg3), ivp.IVP), True)
-
     def test_vanderpol_jacobian(self):
         rv = Constant(np.array([1.0, 1.0]))
         lg1 = ivp_examples.vanderpol(self.tspan, rv)
@@ -161,13 +235,3 @@ class TestExamples(unittest.TestCase, NumpyAssertions):
         self.assertAllClose(
             lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
         )
-
-    def test_threebody(self):
-        """Test the Van der Pol ODE convenience function."""
-        rv = Constant(np.array([1.0, 1.0]))
-        lg1 = ivp_examples.threebody(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp_examples.threebody(self.tspan, rv, params=(0.012277471,))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-        lg3 = ivp_examples.threebody(self.tspan, rv, params=0.012277471)
-        self.assertEqual(issubclass(type(lg3), ivp.IVP), True)
