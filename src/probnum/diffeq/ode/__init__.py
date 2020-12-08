6d54ff1e59643616c7a7e6b9342df7dd061b71b1
schmidtjonathan <jonathan.schmidt@student.uni-tuebingen.de>
2020-12-08 11:03:47 +0100

    More ODE examples (#262)
    * Adds SEIR model to example IVPs
    
    * Add simple rigid body dynamics to example IVPs
    
    * Made variable naming consistent
    
    * Fixed errors in documentation
    
    * Removed unnecessary parameter in rigidbody ivp
    
    * Added rigidbody jacobian
    
    * Made code structure consistent
    
    * Add Van der Pol equations to example IVPs
    
    * Removed tuple notation from vanderpol doc
    
    * Test for SEIR IVP
    
    * Tests for rigidbody and vanderpol IVPs
    
    * Fixed error in vanderpol jacobian
    
    * Adds three-body problem IVP to IVP examples
    
    * Moved IVP examples into separate file
    
    * Adapted imports in IVP test
    
    * Included shape information into IVP docs
    
    * Fixed isort problem
    
    * Fix pylint issues
    
    * Adapted shape hints in docstrings
    
    * Fix black issues in docstrings
    
    * Outsourced tests for ivp examples to separate file
    
    * Removed unnecessary pylint flag
    
    * Compute SEIR population from initial RV
    
    * Added Jacobian of SEIR model and test thereof
    
    * Complement docstrings for IVPs
    
    * Made variable naming in threebody more expressive
    
    * Keep __all__ statement at diffeq level only
    
    * Adds IVP test cases, re-structures existing ones
    
    * Added __all__ statement back to ode.__init__.py
    
    * Improved docstrings
    
    Co-authored-by: Jonathan Schmidt <jonathan_kjschmidt@gmx.de>

--- src/probnum/diffeq/ode/__init__.py
+++ src/probnum/diffeq/ode/__init__.py
@@ -1,4 +1,15 @@
 from .ivp import *
+from .ivp_examples import *
 from .ode import *
 
-__all__ = ["ODE", "IVP", "logistic", "fitzhughnagumo", "lotkavolterra"]
+__all__ = [
+    "ODE",
+    "IVP",
+    "logistic",
+    "fitzhughnagumo",
+    "seir",
+    "rigidbody",
+    "vanderpol",
+    "threebody",
+    "lotkavolterra",
+]
--- tests/test_diffeq/test_ode/test_ivp.py
+++ tests/test_diffeq/test_ode/test_ivp.py
@@ -4,88 +4,8 @@ import numpy as np
 
 from probnum.diffeq.ode import ivp
 from probnum.random_variables import Constant
-from tests.testing import NumpyAssertions
 
 TEST_NDIM = 3
-
-
-class TestExamples(unittest.TestCase, NumpyAssertions):
-    """Test cases for example IVPs: Lotka-Volterra, etc."""
-
-    def setUp(self):
-        self.tspan = (0.0, 4.212)
-
-    def test_logistic(self):
-        """Test the logistic ODE convenience function."""
-        rv = Constant(0.1)
-        lg1 = ivp.logistic(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.logistic(self.tspan, rv, params=(1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-
-    def test_logistic_jacobian(self):
-        rv = Constant(0.1)
-        lg1 = ivp.logistic(self.tspan, rv)
-        random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
-        random_point = 1 + np.random.rand(lg1.dimension)
-        fd_approx = (
-            0.5
-            * 1e11
-            * (
-                lg1(0.1, random_point + 1e-11 * random_direction)
-                - lg1(0.1, random_point - 1e-11 * random_direction)
-            )
-        )
-        self.assertAllClose(
-            lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
-        )
-
-    def test_fitzhughnagumo(self):
-        rv = Constant(np.ones(2))
-        lg1 = ivp.fitzhughnagumo(self.tspan, rv)
-        self.assertEqual(issubclass(type(lg1), ivp.IVP), True)
-        lg2 = ivp.fitzhughnagumo(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-
-    def test_fitzhughnagumo_jacobian(self):
-        rv = Constant(np.ones(2))
-        lg1 = ivp.fitzhughnagumo(self.tspan, rv)
-        random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
-        random_point = 1 + np.random.rand(lg1.dimension)
-        fd_approx = (
-            0.5
-            * 1e11
-            * (
-                lg1(0.1, random_point + 1e-11 * random_direction)
-                - lg1(0.1, random_point - 1e-11 * random_direction)
-            )
-        )
-        self.assertAllClose(
-            lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
-        )
-
-    def test_lotkavolterra(self):
-        rv = Constant(np.ones(2))
-        lg1 = ivp.lotkavolterra(self.tspan, rv)
-        lg2 = ivp.lotkavolterra(self.tspan, rv, params=(1.0, 1.0, 1.0, 1.0))
-        self.assertEqual(issubclass(type(lg2), ivp.IVP), True)
-
-    def test_lotkavolterra_jacobian(self):
-        rv = Constant(np.ones(2))
-        lg1 = ivp.lotkavolterra(self.tspan, rv)
-        random_direction = 1 + 0.1 * np.random.rand(lg1.dimension)
-        random_point = 1 + np.random.rand(lg1.dimension)
-        fd_approx = (
-            0.5
-            * 1e11
-            * (
-                lg1(0.1, random_point + 1e-11 * random_direction)
-                - lg1(0.1, random_point - 1e-11 * random_direction)
-            )
-        )
-        self.assertAllClose(
-            lg1.jacobian(0.1, random_point) @ random_direction, fd_approx, rtol=1e-2
-        )
 
 
 class TestIVP(unittest.TestCase):
