26abeac2a11a9d59f20f9939a91d2c419d5365d9
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-12-10 15:40:05 +0100

    documented parameters

--- src/probnum/problems/_problems.py
+++ src/probnum/problems/_problems.py
@@ -15,10 +15,29 @@ import probnum
 
 @dataclasses.dataclass
 class RegressionProblem:
-    r"""Regression problems.
+    r"""Regression problem.
 
     Fit a stochastic process to data, given a likelihood (realised by a :obj:`Transition`).
     Solved by Kalman filtering and smoothing in :mod:`probnum.filtsmooth`.
+
+    Parameters
+    ----------
+    observations
+        Observations of the latent process.
+    locations
+        Grid-points on which the observations were taken.
+    likelihood
+        Likelihood of the observations; that is, relation between the latent process and the observed values. Encodes for example noise.
+
+    Examples
+    --------
+    >>> obs = [11.4123, -15.5123]
+    >>> loc = [0.1, 0.2]
+    >>> rp = RegressionProblem(observations=obs, locations=loc)
+    >>> rp
+    RegressionProblem(observations=[11.4123, -15.5123], locations=[0.1, 0.2], likelihood=None)
+    >>> rp.observations
+    [11.4123, -15.5123]
     """
 
     observations: np.ndarray
@@ -28,20 +47,33 @@ class RegressionProblem:
 
 @dataclasses.dataclass
 class IVProblem:
-    r"""First order ODE initial value problems.
+    r"""First order ODE initial value problem.
 
     Compute a function :math:`y=y(t)` that solves
 
     .. math::
-        \dot y(t) = f(t, y(t)), \quad y(0) = y_0
+        \dot y(t) = f(t, y(t)), \quad y(t_0) = y_0
 
+    on time-interval :math:`[t_0, t_\text{max}]`.
     Solved by the ODE solvers in :mod:`probnum.diffeq`.
+
+
+    Parameters
+    ----------
+    f
+        ODE vector-field.
+    t0
+        Initial point in time.
+    tmax
+        Final point in time.
+    y0
+        Initial value of the solution.
 
     Examples
     --------
     >>> def f(t, x):
     ...     return x*(1-x)
-    >>> ivp = IVProblem(f, 0., 3., 0.1)
+    >>> ivp = IVProblem(f, t0=0., tmax=3., y0=0.1)
     >>> ivp.t0, ivp.tmax, ivp.y0
     (0.0, 3.0, 0.1)
     >>> np.round(ivp.f(ivp.t0, ivp.y0), 2)
@@ -51,7 +83,7 @@ class IVProblem:
     f: typing.Callable[[float, np.ndarray], np.ndarray]
     t0: float
     tmax: float  # Bold move: remove this? This is not really part of an IVP.
-    y0: np.ndarray
+    y0: typing.Union[probnum.type.FloatArgType, np.ndarray]
 
 
 @dataclasses.dataclass
@@ -61,8 +93,15 @@ class LinearSystemProblem:
     Compute :math:`x` from :math:`Ax=b`.
     Solved by the probabilistic linear solver in mod:`probnum.linalg`
 
-    Example
-    -------
+    Parameters
+    ----------
+    A
+        Square matrix or linear operator.
+    b
+        Right-hand side vector or matrix.
+
+    Examples
+    --------
     >>> A = np.eye(3)
     >>> b = np.arange(3)
     >>> lsp = LinearSystemProblem(A, b)
@@ -83,7 +122,7 @@ class LinearSystemProblem:
 
 @dataclasses.dataclass
 class QuadProblem:
-    r"""Numerically compute an integral.
+    r"""Numerical computation of an integral.
 
     Compute the integral
 
@@ -94,8 +133,15 @@ class QuadProblem:
     For the time being, :math:`\mu` is the Lebesgue measure.
     Solved by the quadrature rules in :mod:`probnum.quad`.
 
-    Example
-    -------
+    Parameters
+    ----------
+    integrand
+        Function to be integrated.
+    domain
+        Domain of the integral.
+
+    Examples
+    --------
     >>> def integrand(x):
     ...     return x**2
     >>> def domain(x):
