5f689ec92362298f76b6876808b4df56e5b212f8
Nicholas Krämer <nicokraemer@gmx.net>
2020-12-13 09:54:10 +0100

    no more star import

--- src/probnum/problems/__init__.py
+++ src/probnum/problems/__init__.py
@@ -1,5 +1,5 @@
 """Definitions and collection of problems solved by probabilistic numerical methods."""
 
-from ._problems import *
+from ._problems import IVProblem, LinearSystemProblem, QuadProblem, RegressionProblem
 
 __all__ = ["RegressionProblem", "IVProblem", "LinearSystemProblem", "QuadProblem"]
