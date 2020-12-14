407fcc58f012627f81cb75f784d01006777642c3
Nicholas Krämer <nicokraemer@gmx.net>
2020-12-14 10:15:37 +0100

    no more star imports in statespace

--- src/probnum/filtsmooth/statespace/__init__.py
+++ src/probnum/filtsmooth/statespace/__init__.py
@@ -5,11 +5,15 @@ space models, which are the basis for Bayesian filtering and smoothing,
 but also probabilistic ODE solvers.
 """
 
-from .discrete_transition import *
-from .integrator import *
-from .preconditioner import *
-from .sde import *
-from .transition import *
+from .discrete_transition import (
+    DiscreteGaussian,
+    DiscreteLinearGaussian,
+    DiscreteLTIGaussian,
+)
+from .integrator import IBM, IOUP, Integrator, Matern
+from .preconditioner import NordsieckLikeCoordinates
+from .sde import LTISDE, SDE, LinearSDE
+from .transition import Transition, generate
 
 __all__ = [
     "Transition",
@@ -25,4 +29,5 @@ __all__ = [
     "DiscreteLTIGaussian",
     "Preconditioner",
     "NordsieckLikeCoordinates",
+    "generate",
 ]
