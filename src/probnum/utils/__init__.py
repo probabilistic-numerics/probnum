3e54c2c47d79dc0e35099e47c10914a1389e0b17
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-12-14 20:06:39 +0100

    removed some star imports

--- src/probnum/utils/__init__.py
+++ src/probnum/utils/__init__.py
@@ -1,12 +1,13 @@
 """Utility Functions."""
 
-from .argutils import *
-from .arrayutils import *
-from .randomutils import *
+from .argutils import as_numpy_scalar, as_random_state, as_shape
+from .arrayutils import as_colvec, atleast_1d
+from .randomutils import derive_random_seed
 
 # Public classes and functions. Order is reflected in documentation.
 __all__ = [
     "as_colvec",
+    "atleast_1d",
     "as_numpy_scalar",
     "as_random_state",
     "as_shape",
