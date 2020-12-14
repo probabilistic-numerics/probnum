cf4e15b264e1edb320978aa7318f11319a7698af
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-12-14 14:49:09 +0100

    test files added

---
+++ tests/test_linalg/test_linearsolvers/test_policies.py
@@ -0,0 +1,9 @@
+"""Test cases for policies of probabilistic linear solvers."""
+
+import unittest
+
+from tests.testing import NumpyAssertions
+
+
+class LinearSolverPolicyTestCase(unittest.TestCase, NumpyAssertions):
+    """General test case for policies of probabilistic linear solvers."""
---
+++ tests/test_linalg/test_linearsolvers/test_probabilistic_linear_solver.py
@@ -0,0 +1,9 @@
+"""Tests for the implementation of a generic probabilistic linear solver."""
+
+import unittest
+
+from tests.testing import NumpyAssertions
+
+
+class ProbabilisticLinearSolverTestCase(unittest.TestCase, NumpyAssertions):
+    """General test case for probabilistic linear solvers."""
