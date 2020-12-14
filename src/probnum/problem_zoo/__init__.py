b72537ab2f26e06e16374c9952898311b6446ce4
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-12-14 11:56:11 +0100

    .

--- src/probnum/problem_zoo/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test problems for probabilistic linear solvers."""
--- src/probnum/problem_zoo/ode/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Test problems involving ordinary differential equations."""
-
-# Public classes and functions. Order is reflected in documentation.
-__all__ = []
