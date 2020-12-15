9f35e205f71f2b3e172aa82f0c080dfa1da16492
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-12-15 13:22:41 +0100

    forgot one...

--- src/probnum/filtsmooth/gaussfiltsmooth/__init__.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/__init__.py
@@ -2,6 +2,6 @@ from .extendedkalman import ContinuousEKFComponent, DiscreteEKFComponent
 from .iterated_kalman import IteratedKalman
 from .kalman import Kalman
 from .kalmanposterior import KalmanPosterior
-from .stoppingcriterion import StoppingCriterion
+from .stoppingcriterion import FixedPointStopping, StoppingCriterion
 from .unscentedkalman import ContinuousUKFComponent, DiscreteUKFComponent
 from .unscentedtransform import UnscentedTransform
