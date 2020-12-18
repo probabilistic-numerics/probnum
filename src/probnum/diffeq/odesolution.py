08e92ab609733958488a9f65f3e0cc9ba98eda81
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2020-12-18 12:07:21 +0100

    cleaned up ODESolution

--- src/probnum/diffeq/odesolution.py
+++ src/probnum/diffeq/odesolution.py
@@ -1,22 +1,38 @@
-"""ODESolution object, returned by `probsolve_ivp`
+"""ODESolution interface.
 
+This object is returned by ODESolver.solve().
+
+Provides dense output (by being callable), is sliceable, and collects the time-grid as well as the discrete-time solution.
 Contains the discrete time and function outputs. Provides dense output
 by being callable. Can function values can also be accessed by indexing.
 """
 import abc
 
-import numpy as np
-
+import probnum._randomvariablelist as pnrv_list
 import probnum.random_variable as pnrv
-from probnum import utils
-from probnum._randomvariablelist import _RandomVariableList
-from probnum.filtsmooth import KalmanPosterior
-from probnum.filtsmooth.filtsmoothposterior import FiltSmoothPosterior
 
 
 class ODESolution(abc.ABC):
     """ODE Solution interface."""
 
+    @property
+    @abc.abstractmethod
+    def t(self) -> float:
+        """Time points of the discrete-time solution."""
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def y(self) -> pnrv_list._RandomVariableList:
+        """:obj:`list` of :obj:`RandomVariable`: Probabilistic discrete-time solution
+
+        Probabilistic discrete-time solution at times :math:`t_1, ..., t_N`,
+        as a list of random variables.
+        To return means and covariances use ``y.mean`` and ``y.cov``.
+        """
+        raise NotImplementedError
+
+    # Not abstract, because providing interpolation could sometimes be tedious.
     def __call__(self, t: float) -> pnrv.RandomVariable:
         """Evaluate the time-continuous solution at time t.
 
@@ -27,30 +43,14 @@ class ODESolution(abc.ABC):
 
         Returns
         -------
-        :obj:`RandomVariable`
-            Probabilistic estimate of the continuous-time solution at time ``t``.
+        Probabilistic estimate of the continuous-time solution at time ``t``.
         """
         raise NotImplementedError
 
     def __len__(self) -> int:
         """Number of points in the discrete-time solution."""
-        raise NotImplementedError
+        return len(self.y)
 
     def __getitem__(self, idx: int) -> pnrv.RandomVariable:
         """Access the :math:`i`th element of the discrete-time solution."""
-        raise NotImplementedError
-
-    @property
-    def t(self) -> float:
-        """Time points of the discrete-time solution."""
-        raise NotImplementedError
-
-    @property
-    def y(self) -> _RandomVariableList:
-        """:obj:`list` of :obj:`RandomVariable`: Probabilistic discrete-time solution
-
-        Probabilistic discrete-time solution at times :math:`t_1, ..., t_N`,
-        as a list of random variables.
-        To return means and covariances use ``y.mean`` and ``y.cov``.
-        """
-        raise NotImplementedError
+        return self.y[idx]
