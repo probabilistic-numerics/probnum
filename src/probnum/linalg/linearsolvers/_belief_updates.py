f624b8d722c6210bf7390f98a79918c526021da3
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-12-21 12:18:41 +0100

    restructured linear gaussian belief update

--- src/probnum/linalg/linearsolvers/_belief_updates.py
+++ src/probnum/linalg/linearsolvers/_belief_updates.py
@@ -2,9 +2,11 @@
 
 import numpy as np
 
-import probnum  # pylint: disable="unused-import
+import probnum  # pylint: disable="unused-import"
 import probnum.linops as linops
 import probnum.random_variables as rvs
+
+# pylint: disable="invalid-name"
 
 
 class BeliefUpdate:
@@ -41,6 +43,10 @@ class BeliefUpdate:
         """
         return self._belief_update(solver_state)
 
+    # TODO: make update functions with kwargs part of this function and then
+    #  implement different variants of it depending on the type of solver (
+    #  matrixbased/solutionbased) etc.
+
 
 class LinearGaussianBeliefUpdate(BeliefUpdate):
     """Belief update assuming (symmetric) Gaussianity and linear observations."""
@@ -52,89 +58,101 @@ class LinearGaussianBeliefUpdate(BeliefUpdate):
         self, solver_state: "probnum.linalg.linearsolvers.LinearSolverState"
     ) -> "probnum.linalg.linearsolvers" ".LinearSolverState":
 
-        # TODO: refactor this method by splitting it up into a function for each of
-        #  the individual updates
-
-        # Setup
-        x_mean = solver_state.belief[0].mean
-        A_mean = solver_state.belief[1].mean
-        A_covfactor = solver_state.belief[1].cov.A
-        Ainv_mean = solver_state.belief[2].mean
-        Ainv_covfactor = solver_state.belief[2].cov.A
         action = solver_state.actions[-1]
         observation = solver_state.observations[-1]
-        residual = solver_state.residual
 
-        # Compute step size
+        # Compute step size and Rayleigh quotient
         sy = action.T @ observation
-        step_size = -np.squeeze((action.T @ residual) / sy)
+        step_size = -np.squeeze((action.T @ solver_state.residual) / sy)
+        # TODO log-Rayleigh quotient for calibration
 
-        # Step and residual update
-        x_mean = x_mean + step_size * action
-        solver_state.residual = residual + step_size * observation
+        # Solution and residual update
+        x = self._update_solution(
+            x=solver_state.belief[0], action=action, step_size=step_size
+        )
+        solver_state.residual = self._update_residual(
+            residual=solver_state.residual, step_size=step_size, observation=observation
+        )
 
-        # (Symmetric) mean and covariance updates
-        Vs = A_covfactor @ action
-        delta_A = observation - A_mean @ action
+        # System matrix and inverse updates
+        A = self._update_matrix(
+            A=solver_state.belief[1], action=action, observation=observation
+        )
+        Ainv = self._update_inverse(
+            Ainv=solver_state.belief[2], action=action, observation=observation
+        )
+
+        # Update right hand side b
+        b = self._update_rhs(b=solver_state.belief[3])
+
+        # Update solver state with new beliefs
+        solver_state.belief = (x, A, Ainv, b)
+
+        return solver_state
+
+    def _update_residual(
+        self, residual: np.ndarray, step_size: float, observation: np.ndarray
+    ) -> np.ndarray:
+        """Update the residual :math:`r_i = Ax_i - b`."""
+        raise NotImplementedError
+
+    def _update_solution(
+        self, x: rvs.RandomVariable, step_size: float, action: np.ndarray
+    ) -> rvs.RandomVariable:
+        """Update the solution :math:`x_i` to the linear system."""
+        return x + step_size * action
+
+    def _update_matrix(
+        self, A: rvs.RandomVariable, action: np.ndarray, observation: np.ndarray
+    ) -> rvs.Normal:
+        """Update the belief over the system matrix :math:`A`."""
+        Vs = A.cov.A @ action
+        delta_A = observation - A.mean @ action
         u_A = Vs / (action.T @ Vs)
         v_A = delta_A - 0.5 * (action.T @ delta_A) * u_A
 
-        Wy = Ainv_covfactor @ observation
-        delta_Ainv = action - Ainv_mean @ observation
-        yWy = np.squeeze(observation.T @ Wy)
-        u_Ainv = Wy / yWy
-        v_Ainv = delta_Ainv - 0.5 * (observation.T @ delta_Ainv) * u_Ainv
+        # Rank 2 mean update (+= uv' + vu')
+        A_mean = linops.aslinop(A.mean) + self._mean_update(u=u_A, v=v_A)
 
-        # Rank 2 mean updates (+= uv' + vu')
-        A_mean = linops.aslinop(A_mean) + self._mean_update(u=u_A, v=v_A)
-        Ainv_mean = linops.aslinop(Ainv_mean) + self._mean_update(u=u_Ainv, v=v_Ainv)
-
-        # Rank 1 covariance Kronecker factor update (-= u_A(Vs)' and -= u_Ainv(Wy)')
+        # Rank 1 covariance Kronecker factor update (-= u_A(Vs)')
         if solver_state.iteration == 0:
             _A_covfactor_update_term = self._covariance_factor_update(u=u_A, Ws=Vs)
-            _Ainv_covfactor_update_term = self._covariance_factor_update(
-                u=u_Ainv, Ws=Wy
-            )
         else:
             _A_covfactor_update_term = (
                 _A_covfactor_update_term + self._covariance_factor_update(u=u_A, Ws=Vs)
             )
+        A_covfactor = linops.aslinop(self.A_covfactor0) - _A_covfactor_update_term
+
+        return rvs.Normal(mean=A_mean, cov=linops.SymmetricKronecker(A_covfactor))
+
+    def _update_inverse(
+        self, Ainv: rvs.RandomVariable, action: np.ndarray, observation: np.ndarray
+    ) -> rvs.Normal:
+        """Update the belief over the inverse of the system matrix :math:`H=A^{-1}`."""
+        Wy = Ainv.cov.A @ observation
+        delta_Ainv = action - Ainv.mean @ observation
+        yWy = np.squeeze(observation.T @ Wy)
+        u_Ainv = Wy / yWy
+        v_Ainv = delta_Ainv - 0.5 * (observation.T @ delta_Ainv) * u_Ainv
+
+        # Rank 2 mean update (+= uv' + vu')
+        Ainv_mean = linops.aslinop(Ainv.mean) + self._mean_update(u=u_Ainv, v=v_Ainv)
+
+        # Rank 1 covariance Kronecker factor update (-= u_Ainv(Wy)')
+        if solver_state.iteration == 0:
+            _Ainv_covfactor_update_term = self._covariance_factor_update(
+                u=u_Ainv, Ws=Wy
+            )
+        else:
             _Ainv_covfactor_update_term = (
                 _Ainv_covfactor_update_term
                 + self._covariance_factor_update(u=u_Ainv, Ws=Wy)
             )
-        A_covfactor = linops.aslinop(self.A_covfactor0) - _A_covfactor_update_term
         Ainv_covfactor = (
             linops.aslinop(self.Ainv_covfactor0) - _Ainv_covfactor_update_term
         )
 
-        # Update solver state with new beliefs
-        solver_state.belief = (
-            rvs.Constant(x_mean),
-            rvs.Normal(A_mean, linops.SymmetricKronecker(A_covfactor)),
-            rvs.Normal(Ainv_mean, linops.SymmetricKronecker(Ainv_covfactor)),
-            solver_state.belief[3],
-        )
-
-        return solver_state
-
-    def _update_residual(self, step_size: float, observation: np.ndarray) -> np.ndarray:
-        """Update the residual :math:`r_i = Ax_i - b`."""
-        raise NotImplementedError
-
-    def _update_solution(self, step_size: float, action: np.ndarray) -> np.ndarray:
-        """Update the solution :math:`x_i` to the linear system."""
-        raise NotImplementedError
-
-    def _update_matrix(self, A_mean, A_covfactor, action, observation) -> rvs.Normal:
-        """Update the belief over the system matrix :math:`A`."""
-        raise NotImplementedError
-
-    def _update_inverse(
-        self, Ainv_mean, Ainv_covfactor, action, observation
-    ) -> rvs.Normal:
-        """Update the belief over the inverse of the system matrix :math:`H=A^{-1}`."""
-        raise NotImplementedError
+        return rvs.Normal(mean=Ainv_mean, cov=linops.SymmetricKronecker(Ainv_covfactor))
 
     def _mean_update(self, u, v):
         """Linear operator implementing the symmetric rank 2 mean update (+= uv' +
@@ -157,3 +175,7 @@ class LinearGaussianBeliefUpdate(BeliefUpdate):
         return linops.LinearOperator(
             shape=(u.shape[0], u.shape[0]), matvec=mv, matmat=mv
         )
+
+    def _update_rhs(self, b: rvs.RandomVariable) -> rvs.RandomVariable:
+        """Update the belief over the right hand side of the linear system."""
+        return b
