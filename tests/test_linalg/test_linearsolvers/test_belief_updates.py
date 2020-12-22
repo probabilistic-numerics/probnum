23347d59db34a9e574ba87dfdc521414c3015e52
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-12-22 19:26:15 +0100

    belief update tests fixed

--- tests/test_linalg/test_linearsolvers/test_belief_updates.py
+++ tests/test_linalg/test_linearsolvers/test_belief_updates.py
@@ -21,9 +21,6 @@ class BeliefUpdateTestCase(ProbabilisticLinearSolverTestCase, NumpyAssertions):
 
     def setUp(self) -> None:
         """Test resources for linear solver belief updates."""
-        # Action and observation
-        self.action = self.rng.normal(size=self.linsys.A.shape[1])
-        self.observation = self.rng.normal(size=self.linsys.A.shape[0])
 
         # Belief updates
         def custom_belief_update(
@@ -47,19 +44,26 @@ class BeliefUpdateTestCase(ProbabilisticLinearSolverTestCase, NumpyAssertions):
     def test_return_argument_types(self):
         """Test whether a belief update returns a linear system belief and solver
         state."""
+        # Action and observation
+        action = self.rng.normal(size=self.linsys.A.shape[1])
+        observation = self.rng.normal(size=self.linsys.A.shape[0])
+
         for belief_update in self.belief_updates:
             with self.subTest():
                 belief, solver_state = belief_update(
                     problem=self.linsys,
                     belief=self.prior,
-                    action=self.action,
-                    observation=self.observation,
+                    action=action,
+                    observation=observation,
                     solver_state=self.solver_state,
                 )
                 self.assertIsInstance(belief, LinearSystemBelief)
                 self.assertIsInstance(solver_state, LinearSolverState)
 
 
-class LinearGaussianBeliefUpdateTestCase(BeliefUpdateTestCase):
+class LinearSymmetricGaussianTestCase(BeliefUpdateTestCase):
+    """Test case for the linear symmetric Gaussian belief update."""
+
     def setUp(self) -> None:
         """Test resources for the linear Gaussian belief update."""
+        self.belief_updates = [LinearSymmetricGaussian()]
