5f737d87d26770142cb0241d3f2a292a99e0a21a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-01-05 22:35:34 +0100

    path to app data for windows systems added

--- src/probnum/problems/zoo/__init__.py
+++ src/probnum/problems/zoo/__init__.py
@@ -1,6 +1,11 @@
 """Collection of test problems for probabilistic numerical methods."""
 import os
+import sys
 from pathlib import Path
 
 # Directory containing data of the problem zoo
-PROBLEMZOO_DIR = os.path.join(Path.home(), ".probnum/problems/zoo/")
+PROBLEMZOO_DIR = ".probnum/problems/zoo/"
+if sys.platform == "win32":
+    PROBLEMZOO_DIR = os.path.join(os.environ["APPDATA"], PROBLEMZOO_DIR)
+else:
+    PROBLEMZOO_DIR = os.path.join(Path.home(), PROBLEMZOO_DIR)
