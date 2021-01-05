e55bdfa80530ffd4a05e68520485a2cc5ce489b2
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-01-05 23:11:41 +0100

    new platform independent fix attempt

--- src/probnum/problems/zoo/linalg/_suitesparse_matrix.py
+++ src/probnum/problems/zoo/linalg/_suitesparse_matrix.py
@@ -3,6 +3,7 @@
 This implementation is based on Sudarshan Raghunathan's SSGETPY package
 (https://github.com/drdarshan/ssgetpy).
 """
+import contextlib
 import csv
 import dataclasses
 import datetime
@@ -575,37 +576,32 @@ def getdtype(real, logical):
 
 def gen_rows(csvrows):
     """Creates a generator that returns a single row in the matrix database."""
-    reader = csv.reader(csvrows, newline="")
-    matid = 0
-    for line in reader:
-        matid += 1
-        group = line[0]
-        name = line[1]
-        rows = int(line[2])
-        cols = int(line[3])
-        nnz = int(line[4])
-        real = bool(int(line[5]))
-        logical = bool(int(line[6]))
-        is2d3d = bool(int(line[7]))
-        isspd = bool(int(line[8]))
-        psym = float(line[9])
-        nsym = float(line[10])
-        kind = line[11]
-        yield matid, group, name, rows, cols, nnz, getdtype(
-            real, logical
-        ), is2d3d, isspd, psym, nsym, kind
+    reader = csv.reader(csvrows)
 
 
 def csvindex_generate():
-    response = requests.get(SUITESPARSE_INDEX_URL)
-    lines = response.iter_lines()
-
-    # Read the number of entries
-    logger.info(f"Number of entries in the CSV file: {next(lines)}")
-    # Read the last modified date
-    logger.info(f"Last modified date: {next(lines)}")
-
-    return gen_rows(line.decode("utf-8") for line in lines)
+    with contextlib.closing(
+        requests.get(SUITESPARSE_INDEX_URL, stream=True)
+    ) as response:
+        reader = csv.reader(response.iter_lines(), delimiter=",", quotechar='"')
+        matid = 0
+        for line in reader:
+            matid += 1
+            group = line[0]
+            name = line[1]
+            rows = int(line[2])
+            cols = int(line[3])
+            nnz = int(line[4])
+            real = bool(int(line[5]))
+            logical = bool(int(line[6]))
+            is2d3d = bool(int(line[7]))
+            isspd = bool(int(line[8]))
+            psym = float(line[9])
+            nsym = float(line[10])
+            kind = line[11]
+            yield matid, group, name, rows, cols, nnz, getdtype(
+                real, logical
+            ), is2d3d, isspd, psym, nsym, kind
 
 
 # SuiteSparseMatrix Database Singleton
