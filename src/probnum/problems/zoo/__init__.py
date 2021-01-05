11f5cef95e44f92d742bc064fe2b4fa3d4e5385a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-01-05 22:45:15 +0100

    directory name windows

--- src/probnum/problems/zoo/__init__.py
+++ src/probnum/problems/zoo/__init__.py
@@ -4,8 +4,8 @@ import sys
 from pathlib import Path
 
 # Directory containing data of the problem zoo
-PROBLEMZOO_DIR = ".probnum/problems/zoo/"
-if sys.platform == "win32":
-    PROBLEMZOO_DIR = os.path.join(os.environ["APPDATA"], PROBLEMZOO_DIR)
-else:
-    PROBLEMZOO_DIR = os.path.join(Path.home(), PROBLEMZOO_DIR)
+PROBLEMZOO_DIR = "probnum/problems/zoo/"
+if sys.platform != "win32":
+    PROBLEMZOO_DIR = "." + PROBLEMZOO_DIR
+
+PROBLEMZOO_DIR = os.path.join(Path.home(), PROBLEMZOO_DIR)
