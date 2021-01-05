596eb2d8b11d27cfc76c64dd55c5fa47f83dd707
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-01-05 22:29:30 +0100

    data directory changed

--- src/probnum/problems/zoo/__init__.py
+++ src/probnum/problems/zoo/__init__.py
@@ -1,10 +1,6 @@
 """Collection of test problems for probabilistic numerical methods."""
 import os
-import sys
+from pathlib import Path
 
 # Directory containing data of the problem zoo
-PROBLEMZOO_DIR = "probnum/problems/zoo/"
-if sys.platform == "win32":
-    PROBLEMZOO_DIR = os.path.join(os.environ["APPDATA"], PROBLEMZOO_DIR)
-else:
-    PROBLEMZOO_DIR = os.path.join(os.environ["HOME"], "." + PROBLEMZOO_DIR)
+PROBLEMZOO_DIR = os.path.join(Path.home(), ".probnum/problems/zoo/")
