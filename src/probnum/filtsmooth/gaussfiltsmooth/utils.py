8690e0fea046eaa7e5e38ebf11682414183ae422
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-12 15:26:05 +0100

    basic test for cholesky update

--- src/probnum/filtsmooth/gaussfiltsmooth/utils.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/utils.py
@@ -1,5 +1,7 @@
 """Utility functions for Gaussian filtering and smoothing."""
 
+import numpy as np
+
 
 def cholesky_update(S1, S2=None):
     """Compute Cholesky factorization :math:`C C^\top = S_1 S_1^\top + S_2 S_2^\top`"""
--- tests/test_filtsmooth/test_gaussfiltsmooth/test_utils.py
+++ tests/test_filtsmooth/test_gaussfiltsmooth/test_utils.py
@@ -1,13 +1,27 @@
 import unittest
 
+import numpy as np
+
 import probnum.filtsmooth as pnfs
+from tests.testing import NumpyAssertions
+
+TEST_NDIM = 5
 
 
-class TestCholeskyUpdates(unittest.TestCase):
-    def test_cholesky_sum_choleskies(self):
+class TestCholeskyUpdate(unittest.TestCase, NumpyAssertions):
+    def setUp(self):
+        self.S1 = np.random.rand(TEST_NDIM)
+        self.X1 = self.S1 @ self.S1.T + np.eye(TEST_NDIM)
+        self.S2 = np.random.rand(TEST_NDIM, TEST_NDIM)
+        self.X2 = self.S2 @ self.S2.T + np.eye(TEST_NDIM)
+        self.L1 = np.linalg.cholesky(self.X1)
+        self.L2 = np.linalg.cholesky(self.X2)
+        self.H = np.random.rand(TEST_NDIM, TEST_NDIM)
 
-        print(pnfs.cholesky_update)
-        self.assertEqual(True, False)
+    def test_yields_cholesky_both(self):
+        received = pnfs.cholesky_update(self.H @ self.L1, self.L2)
+        expected = np.linalg.cholesky(self.H @ self.X1 @ self.H.T + self.X2)
+        self.assertAllClose(received, expected)
 
 
 if __name__ == "__main__":
