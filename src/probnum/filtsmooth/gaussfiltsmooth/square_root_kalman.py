0aaa145d8e557175ac1612b0d02185db2177ad48
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-12 16:46:21 +0100

    scaffolded some square root filtering

--- src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
@@ -1,10 +1,28 @@
 """Square-root Gaussian filtering and smoothing."""
 
 from .kalman import Kalman
+from .utils import cholesky_update
 
 
 class SquareRootKalman(Kalman):
     def __init__(self, dynamics_model, measurement_model, initrv):
         """Check that the initial distribution is Gaussian."""
 
+        # check if LTI or discreteLTI for dynamics and measurements
+        # or if at least EKF without diffusion
+
         super().__init__(dynamics_model, measurement_model, initrv)
+
+    def predict(self, start, stop, randvar, intermediate_step=None):
+        pass
+
+    def measure(self, time, randvar):
+        pass
+
+    def update(self, time, randvar, data):
+        pass
+
+    def smooth_step(
+        self, unsmoothed_rv, smoothed_rv, start, stop, intermediate_step=None
+    ):
+        pass
