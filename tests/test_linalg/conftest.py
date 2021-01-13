065097a49ade9b4ab39d41d352d484275ea214d1
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-01-13 18:58:36 +0100

    linear systems as test resources

--- tests/test_linalg/conftest.py
+++ tests/test_linalg/conftest.py
@@ -1,8 +1,13 @@
 """Test fixtures for the linalg subpackage."""
 
+import os
+
 import numpy as np
 import pytest
+import scipy.sparse
 
+import probnum.kernels as kernels
+from probnum.problems import LinearSystem
 from probnum.problems.zoo.linalg import random_sparse_spd_matrix, random_spd_matrix
 
 
@@ -24,6 +29,9 @@ def random_state(seed) -> np.random.RandomState:
     return np.random.RandomState(seed=seed.param)
 
 
+# Matrices
+
+
 @pytest.fixture()
 def A_spd(n: int, random_state: np.random.RandomState):
     """Random symmetric positive definite matrix of dimension :func:`n`, sampled from
@@ -41,4 +49,37 @@ def A_sparse_spd(density, n: int, random_state: np.random.RandomState):
     from :func:`random_state`."""
     return random_sparse_spd_matrix(
         dim=n, random_state=random_state, density=density.param
+    )
+
+
+# @pytest.fixture(
+#     params=[
+#         pytest.param(kernel, id=str(kernel))
+#         for kernel in [
+#             kernels.Linear,
+#             kernels.ExpQuad,
+#             kernels.RatQuad,
+#             kernels.Matern,
+#             kernels.Polynomial,
+#         ]
+#     ]
+# )
+# def kernel_mat(kernel, n: int, random_state: np.random.RandomState):
+#     """Kernel matrix evaluated on a randomly drawn data set."""
+#     data = np.random.uniform(-1.0, 1.0, (n, 1))
+
+
+# Linear systems
+
+
+@pytest.fixture()
+def linsys_poisson():
+    """Discretized Poisson equation with Dirichlet boundary conditions."""
+    fpath = os.path.join(os.path.dirname(__file__), "../resources")
+    A = scipy.sparse.load_npz(file=fpath + "/matrix_poisson.npz")
+    b = np.load(file=fpath + "/rhs_poisson.npy")
+    return LinearSystem(
+        A=A,
+        solution=scipy.sparse.linalg.spsolve(A=A, b=b),
+        b=b,
     )
