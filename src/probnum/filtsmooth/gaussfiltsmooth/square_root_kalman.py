5051176c04be4a5183b78fb43e8afb1625135427
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-13 10:10:42 +0100

    halfway through square-root updates

--- src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
@@ -1,5 +1,7 @@
 """Square-root Gaussian filtering and smoothing."""
 
+import numpy as np
+
 from .extendedkalman import DiscreteEKFComponent
 from .kalman import Kalman
 from .utils import cholesky_update
@@ -24,16 +26,64 @@ class SquareRootKalman(Kalman):
 
         super().__init__(dynamics_model, measurement_model, initrv)
 
-    def predict(self, start, stop, randvar, intermediate_step=None):
-        pass
+    def predict(self, start, stop, randvar, **kwargs):
+        if isinstance(self.dynamics_model, pnfss.LTISDE):
+            disc_model = self.dynamics_model.discretise(stop - start)
+            A = disc_model.driftmat
+            s = disc_model.forcevec
+            L_Q = disc_model.diffmat_cholesky
+        else:
+            A = self.dynamics_model.driftmatfun(start)
+            s = self.dynamics_model.forcevecfun(start)
+            L_Q = self.dynamics_model.diffmatfun_cholesky(start)
+
+        old_mean = randvar.mean
+        old_cov_cholesky = randvar.cov_cholesky
+
+        new_mean = A @ old_mean + s
+        new_cov_cholesky = cholesky_update(A @ old_cov_cholesky, L_Q)
+        new_cov = new_cov_cholesky @ new_cov_cholesky.T
+        return pnrv.Normal(new_mean, cov=new_cov, cov_cholesky=new_cov_cholesky)
 
     def measure(self, time, randvar):
-        pass
+        if isinstance(self.measurement_model, DiscreteEKFComponent):
+            self.measurement_model.linearize(at_this_rv=randvar)
+            disc_model = self.linearized_model
+        else:
+            disc_model = self.measurement_model
+
+        A = self.disc_model.driftmatfun(start)
+        s = self.disc_model.forcevecfun(start)
+        L_Q = self.disc_model.diffmatfun_cholesky(start)
+
+        old_mean = randvar.mean
+        old_cov_cholesky = randvar.cov_cholesky
+
+        new_mean = A @ old_mean + s
+        new_cov_cholesky = cholesky_update(A @ old_cov_cholesky, L_Q)
+        new_cov = new_cov_cholesky @ new_cov_cholesky.T
+        return pnrv.Normal(new_mean, cov=new_cov, cov_cholesky=new_cov_cholesky)
 
     def update(self, time, randvar, data):
+
         pass
 
     def smooth_step(
         self, unsmoothed_rv, smoothed_rv, start, stop, intermediate_step=None
     ):
         pass
+
+
+def square_root_update_matrices(L_R, H, L_C):
+
+    zeros = np.zeros(H.T.shape)
+    blockmat = np.block(((L_R, H @ L_C), (zeros, L_C))).T
+    big_R = np.linalg.qr(blockmat, mode="r")
+
+    size_small = len(H)
+
+    L_S = big_R[:size_small, :size_small].T
+    L_P = big_R[size_small:, size_small:]
+    K = np.linalg.solve(L_S, big_R[:size_small, size_small:]).T
+
+    return L_S, K, L_P
