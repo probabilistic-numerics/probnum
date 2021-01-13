cd475930fc94c380b232368232ee267f1eec1e2f
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-13 09:18:45 +0100

    changed inits

--- src/probnum/filtsmooth/gaussfiltsmooth/__init__.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/__init__.py
@@ -1,10 +1,10 @@
-from .extendedkalman import ContinuousEKFComponent, DiscreteEKFComponent
+from .extendedkalman import ContinuousEKFComponent, DiscreteEKFComponent, EKFComponent
 from .iterated_kalman import IteratedKalman
 from .kalman import Kalman
 from .kalmanposterior import KalmanPosterior
 from .linearizing_transition import LinearizingTransition
 from .square_root_kalman import SquareRootKalman
 from .stoppingcriterion import FixedPointStopping, StoppingCriterion
-from .unscentedkalman import ContinuousUKFComponent, DiscreteUKFComponent
+from .unscentedkalman import ContinuousUKFComponent, DiscreteUKFComponent, UKFComponent
 from .unscentedtransform import UnscentedTransform
 from .utils import cholesky_update
