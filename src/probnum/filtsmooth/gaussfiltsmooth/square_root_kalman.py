dbcd5dc81f69cdc0c9878d4f768df6775fe2adac
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-13 09:40:04 +0100

    init checks for square root kalman

--- src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/square_root_kalman.py
@@ -1,15 +1,26 @@
 """Square-root Gaussian filtering and smoothing."""
 
+from .extendedkalman import DiscreteEKFComponent
 from .kalman import Kalman
 from .utils import cholesky_update
 
 
+# This class re-implements predict and measure,
+# because e.g. update MUST be in here and therefore it seems more
+# didactic to do everything here.
 class SquareRootKalman(Kalman):
     def __init__(self, dynamics_model, measurement_model, initrv):
-        """Check that the initial distribution is Gaussian."""
+        """Check that the models are linear(ised)."""
 
-        # check if LTI or discreteLTI for dynamics and measurements
-        # or if at least EKF without diffusion
+        if not isinstance(dynamics_model, (pnfss.LTISDE, pnfss.DiscreteLinearGaussian)):
+            raise ValueError
+
+        # EKF is acceptable, because of the Taylor linearisation.
+        # UKF would need downdates, which is not supported at the moment.
+        if not isinstance(
+            measurement_model, (pnfss.DiscreteLinearGaussian, DiscreteEKFComponent)
+        ):
+            raise ValueError
 
         super().__init__(dynamics_model, measurement_model, initrv)
 
