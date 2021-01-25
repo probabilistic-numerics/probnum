noise model induced covariance for fixed rhs

src/probnum/linalg/solvers/belief_updates/_weak_mean_corr_linear_obs.py
@@ -1,25 +1,27 @@
 """Belief update for the weak mean correspondence belief given linear observations."""
-
-from functools import cached_property
-from typing import Optional, Tuple
+import dataclasses
+from typing import Optional, Tuple, Type
 
 import numpy as np
 
 import probnum
 import probnum.linops as linops
 import probnum.random_variables as rvs
+from probnum.linalg.solvers._state import LinearSolverState
 from probnum.linalg.solvers.belief_updates._symmetric_normal_linear_obs import (
     SymmetricNormalLinearObsBeliefUpdate,
     _InverseMatrixSymmetricNormalLinearObsBeliefUpdate,
+    _SymmetricNormalLinearObsCache,
     _SystemMatrixSymmetricNormalLinearObsBeliefUpdate,
 )
 from probnum.linalg.solvers.beliefs import (
     LinearSystemBelief,
     WeakMeanCorrespondenceBelief,
 )
-from probnum.linalg.solvers.hyperparams import (
-    LinearSolverHyperparams,
-    UncertaintyUnexploredSpace,
+from probnum.linalg.solvers.data import (
+    LinearSolverAction,
+    LinearSolverData,
+    LinearSolverObservation,
 )
 from probnum.problems import LinearSystem
 
@@ -27,50 +29,37 @@ from probnum.problems import LinearSystem
 __all__ = ["WeakMeanCorrLinearObsBeliefUpdate"]
 
 
+@dataclasses.dataclass
+class _WeakMeanCorrLinearObsCache(_SymmetricNormalLinearObsCache):
+    """Cached quantities assuming symmetric matrix-variate normal priors and linear
+    observations."""
+
+    # TODO use assumptions WS = Y and WY=H_0Y (Theorem 3, eqn. 1+2, Wenger2020)
+    # @cached_property
+    # def covfactorA_action(self) -> np.ndarray:
+    #     return self.qoi_belief.cov.A @ self.action
+    #
+    # @cached_property
+    # def action_covfactorA_action(self) -> float:
+    #     return self.action.T @ self.covfactor_action
+
+
 class _SystemMatrixWeakMeanCorrLinearObsBeliefUpdateState(
     _SystemMatrixSymmetricNormalLinearObsBeliefUpdate
 ):
     """Weak mean correspondence belief update for the system matrix."""
 
-    # TODO use assumptions WS = Y and WY=H_0Y (Theorem 3, eqn. 1+2, Wenger2020)
-    # @cached_property
-    # def covfactor_action(self) -> np.ndarray:
-    #     return self.qoi_belief.cov.A @ self.action
-    #
-    # @cached_property
-    # def action_covfactor_action(self) -> float:
-    #     return self.action.T @ self.covfactor_action
-
     def __call__(
         self,
-        belief: WeakMeanCorrespondenceBelief,
-        action: np.ndarray,
-        observation: np.ndarray,
-        hyperparams: Optional[UncertaintyUnexploredSpace] = None,
-        solver_state: Optional["probnum.linalg.solvers.LinearSolverState"] = None,
+        hyperparams: "probnum.linalg.solvers.hyperparams.UncertaintyUnexploredSpace",
+        solver_state: "probnum.linalg.solvers.LinearSolverState",
     ) -> rvs.Normal:
-        """Updated belief for the system matrix."""
 
-        # Action observation inner products
-        # TODO group all precomputed quantities in LinearSolverMiscQuantities
-        #   and move all belief update related functions back into belief update.
-        if self.solver_state is not None:
-            try:
-                action_obs_innerprod = self.solver_state.action_obs_innerprods[
-                    self.solver_state.iteration
-                ]
-            except IndexError:
-                action_obs_innerprod = self.action.T @ self.observation
-        else:
-            action_obs_innerprod = self.action.T @ self.observation
-        # TODO
-        covfactor_op = (
-            self.belief._cov_factor_matrix(action_obs_innerprods=action_obs_innerprod)
-            - self.covfactor_updates_batch[0]
+        mean = (
+            linops.aslinop(solver_state.prior.A.mean)
+            + solver_state.cache.meanA_update_batch
         )
-
-        mean = linops.aslinop(self.prior.A.mean) + self.mean_update_batch
-        cov = linops.SymmetricKronecker(covfactor_op)
+        cov = solver_state.prior.A.cov.A - solver_state.cache.covfactorA_update_batch[0]
         return rvs.Normal(mean=mean, cov=cov)
 
 
@@ -81,28 +70,26 @@ class _InverseMatrixWeakMeanCorrLinearObsBeliefUpdateState(
 
     def __call__(
         self,
-        belief: WeakMeanCorrespondenceBelief,
-        action: np.ndarray,
-        observation: np.ndarray,
-        hyperparams: Optional[UncertaintyUnexploredSpace] = None,
-        solver_state: Optional["probnum.linalg.solvers.LinearSolverState"] = None,
+        hyperparams: "probnum.linalg.solvers.hyperparams.UncertaintyUnexploredSpace",
+        solver_state: "probnum.linalg.solvers.LinearSolverState",
     ) -> rvs.Normal:
         """Updated belief for the inverse."""
-        # Empirical prior with scaled uncertainty in null space of observations
 
-        # TODO
         covfactor_op = (
-            self.belief._cov_factor_inverse(hyperparams=hyperparams)
-            - self.covfactor_updates_batch[0]
+            solver_state.prior.Ainv.cov.A
+            - solver_state.cache.covfactorH_update_batch[0]
         )
 
-        # Recursive trace update (See Section S4.3 of Wenger and Hennig, 2020)
+        # Recursive covariance trace update (Section S4.3 of Wenger and Hennig, 2020)
         covfactor_op.trace = (
-            self.belief.Ainv.cov.A.trace()
-            - self.sqnorm_covfactor_action / self.action_covfactor_action
+            solver_state.belief.Ainv.cov.A.trace()
+            - solver_state.cache.sqnorm_covfactorH_observation
+            / solver_state.cache.observation_covfactorH_observation
         )
-
-        mean = linops.aslinop(self.prior.Ainv.mean) + self.mean_update_batch
+        mean = (
+            linops.aslinop(solver_state.prior.Ainv.mean)
+            + solver_state.cache.meanH_update_batch
+        )
         cov = linops.SymmetricKronecker(covfactor_op)
         return rvs.Normal(mean=mean, cov=cov)
 
@@ -114,15 +101,16 @@ class WeakMeanCorrLinearObsBeliefUpdate(SymmetricNormalLinearObsBeliefUpdate):
         super().__init__(
             problem=problem,
             prior=prior,
+            cache_type=_WeakMeanCorrLinearObsCache,
             A_belief_update_type=_SystemMatrixWeakMeanCorrLinearObsBeliefUpdateState,
             Ainv_belief_update_type=_InverseMatrixWeakMeanCorrLinearObsBeliefUpdateState,
         )
 
-    def __init__(
+    def __call__(
         self,
         belief: LinearSystemBelief,
-        action: "probnum.linalg.solvers.data.LinearSolverAction",
-        observation: "probnum.linalg.solvers.data.LinearSolverObservation",
+        action: LinearSolverAction,
+        observation: LinearSolverObservation,
         hyperparams: Optional[
             "probnum.linalg.solvers.hyperparams.LinearSolverHyperparams"
         ] = None,
@@ -130,6 +118,64 @@ class WeakMeanCorrLinearObsBeliefUpdate(SymmetricNormalLinearObsBeliefUpdate):
     ) -> Tuple[
         LinearSystemBelief, Optional["probnum.linalg.solvers.LinearSolverState"]
     ]:
-        pass
-        # TODO modify empirical prior with actions and observations here in solver
-        #  state??
+        if solver_state is None:
+
+            solver_state = LinearSolverState(
+                problem=self._problem,
+                prior=self._prior,
+                belief=belief,
+                data=LinearSolverData(
+                    actions=[action],
+                    observations=[observation],
+                ),
+                cache=self._cache_type.from_new_data(
+                    action=action,
+                    observation=observation,
+                    prev_cache=self._cache_type(
+                        problem=self._problem,
+                        belief=self._prior,
+                    ),
+                ),
+            )
+
+        # Update empirical prior with new observations and uncertainty scale
+        # TODO what about non-conjugate actions??
+        self.prior = WeakMeanCorrespondenceBelief(
+            A0=solver_state.prior.A0,
+            Ainv0=solver_state.prior.Ainv0,
+            b=solver_state.problem.b,
+            uncertainty_scales=hyperparams,
+            actions=solver_state.data.actions,
+            observations=solver_state.data.observations,
+            action_obs_innerprods=np.array(
+                solver_state.cache.action_observation_innerprod_list
+            ),
+        )
+        solver_state.prior = self.prior
+
+        # Update belief (using optimized hyperparameters)
+        updated_belief = LinearSystemBelief(
+            x=self._x_belief_update(
+                hyperparams=hyperparams,
+                solver_state=solver_state,
+            ),
+            Ainv=self._Ainv_belief_update(
+                hyperparams=hyperparams,
+                solver_state=solver_state,
+            ),
+            A=self._A_belief_update(
+                hyperparams=hyperparams,
+                solver_state=solver_state,
+            ),
+            b=self._b_belief_update(
+                hyperparams=hyperparams,
+                solver_state=solver_state,
+            ),
+        )
+
+        # Create new solver state from updated belief
+        updated_solver_state = LinearSolverState.from_updated_belief(
+            updated_belief=updated_belief, prev_state=solver_state
+        )
+
+        return updated_belief, updated_solver_state

src/probnum/linalg/solvers/beliefs/_symmetric_normal_linear_system.py
@@ -201,17 +201,15 @@ class SymmetricNormalLinearSystemBelief(LinearSystemBelief):
         #  covariance is given by Prop S4 of Wenger and Hennig, 2020:
         #  \Sigma = 1/2 (W \otimes BWB + WB \boxtimes B'W)
         Wb = self.Ainv.cov.A @ self.b.mean
-        bWb = Wb.T @ self.b.mean
+        bWb = (Wb.T @ self.b.mean).item()
 
-        def _mv(v):
-            return 0.5 * (bWb.item() * self.Ainv.cov.A @ v + Wb @ (Wb.T @ v))
+        def _mv(vec):
+            return 0.5 * (bWb * self.Ainv.cov.A @ vec + Wb @ (Wb.T @ vec))
 
         x_cov = linops.LinearOperator(
             shape=self.Ainv.shape, dtype=float, matvec=_mv, matmat=_mv
         )
         # Efficient trace computation
-        x_cov.trace = lambda: 0.5 * (
-            self.Ainv.cov.A.trace() * np.trace(bWb) + np.trace(Wb.T @ Wb)
-        )
+        x_cov.trace = lambda: 0.5 * (self.Ainv.cov.A.trace() * bWb + (Wb.T @ Wb).item())
 
         return x_cov

src/probnum/linalg/solvers/beliefs/_weak_mean_correspondence.py
@@ -11,6 +11,7 @@ import probnum.random_variables as rvs
 from probnum.linalg.solvers.beliefs._symmetric_normal_linear_system import (
     SymmetricNormalLinearSystemBelief,
 )
+from probnum.linalg.solvers.data import LinearSolverData
 from probnum.linalg.solvers.hyperparams import UncertaintyUnexploredSpace
 from probnum.problems import LinearSystem
 
@@ -45,10 +46,8 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
     uncertainty_scales :
         Uncertainty scales of the system matrix and inverse model in the respective
         null spaces of the actions and observations.
-    actions :
-        Actions to probe the linear system with.
-    observations :
-        Observations of the linear system for the given actions.
+    data :
+        Actions and observations of the linear system.
     action_obs_innerprods :
         Inner product(s) :math:`(S^\top Y)_{ij} = s_i^\top y_j` of actions
         and observations. If a vector is passed, actions are assumed to be
@@ -102,26 +101,15 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
         uncertainty_scales: UncertaintyUnexploredSpace = UncertaintyUnexploredSpace(
             Phi=1.0, Psi=1.0
         ),
-        actions: Optional[Union[List, np.ndarray]] = None,
-        observations: Optional[Union[List, np.ndarray]] = None,
+        data: Optional[LinearSolverData] = None,
         action_obs_innerprods: Optional[np.ndarray] = None,
     ):
         self.A0 = A0
         self.Ainv0 = Ainv0
         self._A_covfactor_update_op = None
         self._Ainv_covfactor_update_op = None
-        if actions is None or observations is None:
-            self.actions = None
-            self.observations = None
-        else:
-            self.actions = (
-                actions if isinstance(actions, np.ndarray) else np.hstack(actions)
-            )
-            self.observations = (
-                observations
-                if isinstance(observations, np.ndarray)
-                else np.hstack(observations)
-            )
+        self.data = data
+
         cov_factor_A = self._cov_factor_matrix(
             uncertainty_scales=uncertainty_scales,
             action_obs_innerprods=action_obs_innerprods,
@@ -161,31 +149,35 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             and observations. If a vector is passed, actions and observations are
             assumed to be conjugate, i.e. :math:`s_i^\top y_j =0` for :math:`i \neq j`.
         """
-        if self.actions is None or self.observations is None:
+        if self.data is None:
             # For no actions taken, the uncertainty scales determine the overall
             # uncertainty, since :math:`{0}^\perp=\mathbb{R}^n`.
             return linops.ScalarMult(scalar=uncertainty_scales.Phi, shape=self.A0.shape)
         else:
             if action_obs_innerprods is None:
-                action_obs_innerprods = np.squeeze(self.actions.T @ self.observations)
+                action_obs_innerprods = np.squeeze(
+                    self.data.actions_arr.A.T @ self.data.observations_arr.A
+                )
 
-            action_proj = linops.OrthogonalProjection(subspace_basis=self.actions)
+            action_proj = linops.OrthogonalProjection(
+                subspace_basis=self.data.actions_arr.A
+            )
 
             if np.squeeze(action_obs_innerprods).ndim in (0, 1):
 
                 def _matvec(x):
                     """Conjugate actions implying :math:`S^{\top} Y` is a diagonal
                     matrix."""
-                    return (self.observations * action_obs_innerprods ** -1) @ (
-                        self.observations.T @ x
-                    )
+                    return (
+                        self.data.observations_arr.A * action_obs_innerprods ** -1
+                    ) @ (self.data.observations_arr.A.T @ x)
 
             else:
 
                 def _matvec(x):
-                    return self.observations @ np.linalg.solve(
+                    return self.data.observations_arr.A @ np.linalg.solve(
                         action_obs_innerprods,
-                        self.observations.T @ x,
+                        self.data.observations_arr.A.T @ x,
                     )
 
             action_space_op = linops.LinearOperator(
@@ -212,7 +204,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             Uncertainty scales of the system matrix and inverse model in the respective
             null spaces of the actions and observations.
         """
-        if self.actions is None or self.observations is None:
+        if self.data is None:
             # For no actions taken, the uncertainty scales determine the overall
             # uncertainty, since :math:`{0}^\perp=\mathbb{R}^n`.
             return linops.ScalarMult(
@@ -220,7 +212,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             )
         else:
             observation_proj = linops.OrthogonalProjection(
-                subspace_basis=self.observations
+                subspace_basis=self.data.observations_arr.A
             )
 
             if isinstance(self.Ainv0, linops.ScalarMult):
@@ -230,7 +222,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
                 def _matvec(x):
                     return self.Ainv0 @ (
                         linops.OrthogonalProjection(
-                            subspace_basis=self.observations,
+                            subspace_basis=self.data.observations_arr.A,
                             is_orthonormal=False,
                             innerprod_matrix=self.Ainv0,
                         )
@@ -274,8 +266,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
         cls,
         Ainv0: Union[np.ndarray, linops.LinearOperator, scipy.sparse.spmatrix],
         problem: LinearSystem,
-        actions: Optional[Union[np.ndarray, List]] = None,
-        observations: Optional[Union[np.ndarray, List]] = None,
+        data: Optional[LinearSolverData] = None,
     ) -> "WeakMeanCorrespondenceBelief":
         r"""Construct a belief about the linear system from an approximate inverse.
 
@@ -289,10 +280,8 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             Approximate inverse of the system matrix.
         problem :
             Linear system to solve.
-        actions :
-            Actions to probe the linear system with.
-        observations :
-            Observations of the linear system for the given actions.
+        data :
+            Actions and observations of the linear system.
         """
         try:
             A0 = Ainv0.inv()
@@ -307,8 +296,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             A0=A0,  # Ensure (weak) mean correspondence
             Ainv0=Ainv0,
             b=rvs.asrandvar(problem.b),
-            actions=actions,
-            observations=observations,
+            data=data,
         )
 
     @classmethod
@@ -316,8 +304,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
         cls,
         A0: Union[np.ndarray, linops.LinearOperator, scipy.sparse.spmatrix],
         problem: LinearSystem,
-        actions: Optional[Union[np.ndarray, List]] = None,
-        observations: Optional[Union[np.ndarray, List]] = None,
+        data: Optional[LinearSolverData] = None,
     ) -> "WeakMeanCorrespondenceBelief":
         r"""Construct a belief about the linear system from an approximate system matrix.
 
@@ -331,10 +318,8 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             Approximate system matrix.
         problem :
             Linear system to solve.
-        actions :
-            Actions to probe the linear system with.
-        observations :
-            Observations of the linear system for the given actions.
+        data :
+            Actions and observations of the linear system.
         """
         try:
             Ainv0 = A0.inv()
@@ -348,8 +333,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             A0=A0,
             Ainv0=Ainv0,  # Ensure (weak) mean correspondence
             b=rvs.asrandvar(problem.b),
-            actions=actions,
-            observations=observations,
+            data=data,
         )
 
     @classmethod
@@ -366,8 +350,7 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             scipy.sparse.spmatrix,
         ],
         problem: LinearSystem,
-        actions: Optional[Union[np.ndarray, List]] = None,
-        observations: Optional[Union[np.ndarray, List]] = None,
+        data: Optional[LinearSolverData] = None,
     ) -> "WeakMeanCorrespondenceBelief":
         r"""Construct a belief from an approximate system matrix and
         corresponding inverse.
@@ -384,18 +367,10 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             Approximate inverse of the system matrix.
         problem :
             Linear system to solve.
-        actions :
-            Actions to probe the linear system with.
-        observations :
-            Observations of the linear system for the given actions.
+        data :
+            Actions and observations of the linear system.
         """
-        return cls(
-            A0=A0,
-            Ainv0=Ainv0,
-            b=rvs.asrandvar(problem.b),
-            actions=actions,
-            observations=observations,
-        )
+        return cls(A0=A0, Ainv0=Ainv0, b=rvs.asrandvar(problem.b), data=data)
 
     @classmethod
     def from_scalar(
@@ -414,9 +389,6 @@ class WeakMeanCorrespondenceBelief(SymmetricNormalLinearSystemBelief):
             Scalar parameter defining prior mean(s) of matrix models.
         problem :
             Linear system to solve.
-        calibration_method :
-            Uncertainty calibration method to automatically set the uncertainty in the
-            null spaces given by :math:`\Phi` and :math:`\Psi`.
         """
         if scalar <= 0.0:
             raise ValueError(f"Scalar parameter alpha={scalar:.4f} must be positive.")

src/probnum/linalg/solvers/data.py
@@ -102,10 +102,14 @@ class LinearSolverData:
         """Array of performed actions."""
         ActionsArr = namedtuple("ActionsArr", ["A", "b"])
 
-        return ActionsArr(
-            np.hstack([action.A for action in self.actions]),
-            np.hstack([action.b for action in self.actions]),
-        )
+        arr_A = np.hstack([action.A for action in self.actions])
+        arr_b = np.hstack([action.b for action in self.actions])
+
+        if arr_A[0] is None:
+            arr_A = np.array([[None]])
+        if arr_b[0] is None:
+            arr_b = np.array([[None]])
+        return ActionsArr(A=arr_A, b=arr_b)
 
     @cached_property
     def observations_arr(self) -> Tuple[np.ndarray, np.ndarray]:
@@ -113,6 +117,27 @@ class LinearSolverData:
         side."""
         ObservationsArr = namedtuple("ObservationsArr", ["A", "b"])
         return ObservationsArr(
-            np.hstack([observation.A for observation in self.observations]),
-            np.hstack([observation.b for observation in self.observations]),
+            A=np.hstack([observation.A for observation in self.observations]),
+            b=np.hstack([observation.b for observation in self.observations]),
+        )
+
+    @classmethod
+    def from_arrays(
+        cls,
+        actions_arr: Tuple[np.ndarray, np.ndarray],
+        observations_arr: Tuple[np.ndarray, np.ndarray],
+    ):
+        """Create a linear solver data object from actions and observations given as
+        tuples of arrays."""
+        return cls(
+            actions=[
+                LinearSolverAction(A=actions_arr[0][:, i], b=actions_arr[1][:, i])
+                for i in range(actions_arr[0].shape[1])
+            ],
+            observations=[
+                LinearSolverObservation(
+                    A=observations_arr[0][:, i], b=observations_arr[1][:, i]
+                )
+                for i in range(observations_arr[0].shape[1])
+            ],
         )

tests/test_linalg/test_solvers/test_beliefs/test_weak_mean_correspondence.py
@@ -6,6 +6,7 @@ import scipy.linalg
 
 import probnum.linops as linops
 from probnum.linalg.solvers.beliefs import WeakMeanCorrespondenceBelief
+from probnum.linalg.solvers.data import LinearSolverData, LinearSolverObservation
 from probnum.linalg.solvers.hyperparams import UncertaintyUnexploredSpace
 from probnum.problems import LinearSystem
 
@@ -14,50 +15,47 @@ pytestmark = pytest.mark.filterwarnings("ignore::scipy.sparse.SparseEfficiencyWa
 
 def test_means_correspond_weakly(
     weakmeancorr_belief: WeakMeanCorrespondenceBelief,
-    actions: list,
-    matvec_observations: list,
+    solver_data: LinearSolverData,
     linsys_spd: LinearSystem,
 ):
     r"""Test whether :math:`\mathbb{E}[A]^{-1}y = \mathbb{E}[H]y` for all actions
     :math:`y`."""
     np.testing.assert_allclose(
         np.linalg.solve(
-            weakmeancorr_belief.A.mean.todense(), np.hstack(matvec_observations)
+            weakmeancorr_belief.A.mean.todense(), solver_data.observations_arr.A
         ),
-        weakmeancorr_belief.Ainv.mean @ np.hstack(matvec_observations),
+        weakmeancorr_belief.Ainv.mean @ solver_data.observations_arr.A,
     )
 
 
 def test_system_matrix_uncertainty_in_action_span(
     weakmeancorr_belief: WeakMeanCorrespondenceBelief,
-    actions: list,
-    matvec_observations: list,
+    solver_data: LinearSolverData,
     linsys_spd: LinearSystem,
     n: int,
 ):
     """Test whether the covariance factor W_0^A of the model for A acts like the
     true A in the span of the actions, i.e. if W_0^A S = Y."""
     np.testing.assert_allclose(
-        np.hstack(matvec_observations),
-        weakmeancorr_belief.A.cov.A @ np.hstack(actions),
+        solver_data.observations_arr.A,
+        weakmeancorr_belief.A.cov.A @ solver_data.actions_arr.A,
     )
 
 
 def test_inverse_uncertainty_in_observation_span(
     weakmeancorr_belief: WeakMeanCorrespondenceBelief,
-    actions: list,
-    matvec_observations: list,
+    solver_data: LinearSolverData,
     linsys_spd: LinearSystem,
     n: int,
 ):
     """Test whether the covariance factor W_0^H of the model for Ainv acts like its
     prior mean in the span of the observations, i.e. if W_0^H Y = H_0 Y."""
-    if n <= len(actions):
+    if n <= len(solver_data):
         pytest.skip("Action null space may be trivial.")
 
     np.testing.assert_allclose(
-        weakmeancorr_belief.Ainv.mean @ np.hstack(matvec_observations),
-        weakmeancorr_belief.Ainv.cov.A @ np.hstack(matvec_observations),
+        weakmeancorr_belief.Ainv.mean @ solver_data.observations_arr.A,
+        weakmeancorr_belief.Ainv.cov.A @ solver_data.observations_arr.A,
     )
 
 
@@ -65,12 +63,13 @@ def test_inverse_uncertainty_in_observation_span(
 def test_uncertainty_action_null_space_is_phi(
     phi: float,
     n: int,
-    actions: list,
+    num_iters: int,
+    solver_data: LinearSolverData,
     random_state: np.random.RandomState,
 ):
     r"""Test whether the uncertainty in the null space <S>^\perp is
     given by the uncertainty scale parameter phi for a scalar system matrix A."""
-    if n <= len(actions):
+    if n <= len(solver_data):
         pytest.skip("Action null space may be trivial.")
 
     scalar_linsys = LinearSystem.from_matrix(
@@ -83,15 +82,20 @@ def test_uncertainty_action_null_space_is_phi(
         uncertainty_scales=UncertaintyUnexploredSpace(
             Phi=phi, Psi=1 / phi if phi != 0.0 else 0.0
         ),
-        actions=actions,
-        observations=scalar_linsys.A @ np.hstack(actions),
+        data=LinearSolverData.from_arrays(
+            actions_arr=solver_data.actions_arr,
+            observations_arr=(
+                scalar_linsys.A @ solver_data.actions_arr.A,
+                np.repeat(scalar_linsys.b, num_iters, axis=1),
+            ),
+        ),
     )
 
-    action_null_space = scipy.linalg.null_space(np.hstack(actions).T)
+    action_null_space = scipy.linalg.null_space(solver_data.actions_arr.A.T)
 
     np.testing.assert_allclose(
         action_null_space.T @ (belief.A.cov.A @ action_null_space),
-        phi * np.eye(n - len(actions)),
+        phi * np.eye(n - len(solver_data)),
         atol=10 ** -15,
         rtol=10 ** -15,
     )
@@ -101,18 +105,18 @@ def test_uncertainty_action_null_space_is_phi(
 def test_uncertainty_observation_null_space_is_psi(
     psi: float,
     n: int,
-    actions: list,
+    solver_data: LinearSolverData,
     random_state: np.random.RandomState,
 ):
     r"""Test whether the uncertainty in the null space <Y>^\perp is
     given by the uncertainty scale parameter psi for a scalar prior mean."""
-    if n <= len(actions):
+    if n <= len(solver_data):
         pytest.skip("Observation null space may be trivial.")
 
     scalar_linsys = LinearSystem.from_matrix(
         A=linops.ScalarMult(scalar=2.5, shape=(n, n)), random_state=random_state
     )
-    observations = scalar_linsys.A @ np.hstack(actions)
+    observations = scalar_linsys.A @ solver_data.actions_arr.A
     belief = WeakMeanCorrespondenceBelief(
         A0=scalar_linsys.A,
         Ainv0=scalar_linsys.A.inv(),
@@ -120,15 +124,14 @@ def test_uncertainty_observation_null_space_is_psi(
         uncertainty_scales=UncertaintyUnexploredSpace(
             Phi=1 / psi if psi != 0.0 else 0.0, Psi=psi
         ),
-        actions=actions,
-        observations=observations,
+        data=solver_data,
     )
 
     observation_null_space = scipy.linalg.null_space(observations.T)
 
     np.testing.assert_allclose(
         observation_null_space.T @ (belief.Ainv.cov.A @ observation_null_space),
-        psi * np.eye(n - len(actions)),
+        psi * np.eye(n - len(solver_data)),
         atol=10 ** -15,
         rtol=10 ** -15,
     )
@@ -172,19 +175,18 @@ def test_no_data_prior(
 def test_inverse_nonscalar_prior_mean(
     n: int,
     weakmeancorr_belief: WeakMeanCorrespondenceBelief,
-    actions: list,
-    matvec_observations: list,
+    solver_data: LinearSolverData,
     linsys_spd: LinearSystem,
 ):
     """Test whether the covariance for the inverse model with a non-scalar prior mean
     matches a naively computed one."""
     W0_Ainv = weakmeancorr_belief.Ainv0 @ linops.OrthogonalProjection(
-        subspace_basis=np.hstack(matvec_observations),
+        subspace_basis=solver_data.observations_arr.A,
         innerprod_matrix=weakmeancorr_belief.Ainv0,
     ).todense() + (
         np.eye(n)
         - linops.OrthogonalProjection(
-            subspace_basis=np.hstack(matvec_observations)
+            subspace_basis=solver_data.observations_arr.A
         ).todense()
     )
 
@@ -204,13 +206,13 @@ def test_conjugate_actions_covariance(
     phi: float,
     psi: float,
     n: int,
-    actions: list,
+    solver_data: LinearSolverData,
     linsys_spd: LinearSystem,
 ):
     """Test whether the covariance for conjugate actions matches a naively computed
     one."""
     # Compute conjugate actions via Cholesky decomposition: S' = L^{-T}S
-    orth_actions = scipy.linalg.orth(np.hstack(actions))
+    orth_actions = scipy.linalg.orth(solver_data.actions_arr.A)
     chol = scipy.linalg.cholesky(linsys_spd.A, lower=False)
     conj_actions = scipy.linalg.solve_triangular(chol, orth_actions, lower=False)
     observations = linsys_spd.A @ conj_actions
@@ -236,8 +238,9 @@ def test_conjugate_actions_covariance(
         Ainv0=Ainv0,
         b=linsys_spd.b,
         uncertainty_scales=UncertaintyUnexploredSpace(Phi=phi, Psi=psi),
-        actions=conj_actions,
-        observations=observations,
+        data=LinearSolverData.from_arrays(
+            actions_arr=(conj_actions, None), observations_arr=(observations, None)
+        ),
         action_obs_innerprods=np.einsum("nk,nk->k", conj_actions, observations),
     )
 