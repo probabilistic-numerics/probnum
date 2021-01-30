e3d61d3277c1cd61fc823f620cb6e7164782dbd3
Nicholas Krämer <nicokraemer@gmx.net>
2021-01-30 12:46:58 +0100

    removed transition preconditioned entirely

--- src/probnum/filtsmooth/statespace/integrator.py
+++ src/probnum/filtsmooth/statespace/integrator.py
@@ -127,32 +127,25 @@ class IBM(Integrator, sde.LTISDE):
             raise TypeError(errormsg)
         step = stop - start
         rv = self.precon.inverse(step) @ rv
-        rv, info = self.transition_rv_preconditioned(rv, start, _diffusion=_diffusion)
-        info["crosscov"] = self.precon(step) @ info["crosscov"] @ self.precon(step).T
-        return self.precon(step) @ rv, info
-
-    def transition_rv_preconditioned(self, rv, start, _diffusion=1.0, **kwargs):
-        return self.equivalent_discretisation_preconditioned.transition_rv(
+        rv, info = self.equivalent_discretisation_preconditioned.transition_rv(
             rv, start, _diffusion=_diffusion
         )
+        info["crosscov"] = self.precon(step) @ info["crosscov"] @ self.precon(step).T
+        return self.precon(step) @ rv, info
 
     def transition_realization(self, real, start, stop, _diffusion=1.0, **kwargs):
         if not isinstance(real, np.ndarray):
             raise TypeError(f"Numpy array expected, {type(real)} received.")
         step = stop - start
         real = self.precon.inverse(step) @ real
-        real, info = self.transition_realization_preconditioned(
+        (
+            real,
+            info,
+        ) = self.equivalent_discretisation_preconditioned.transition_realization(
             real, start, _diffusion=_diffusion
         )
         info["crosscov"] = self.precon(step) @ info["crosscov"] @ self.precon(step).T
         return self.precon(step) @ real, info
-
-    def transition_realization_preconditioned(
-        self, real, start, _diffusion=1.0, **kwargs
-    ):
-        return self.equivalent_discretisation_preconditioned.transition_realization(
-            real, start, _diffusion=_diffusion
-        )
 
     def discretise(self, step):
         """Equivalent discretisation of the process.
