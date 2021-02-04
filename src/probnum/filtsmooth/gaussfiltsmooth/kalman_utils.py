d79b02f0182856b2489befbcbf3f2dc827bd143b
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-02-04 11:01:39 +0100

    square-root kalman update

--- src/probnum/filtsmooth/gaussfiltsmooth/kalman_utils.py
+++ src/probnum/filtsmooth/gaussfiltsmooth/kalman_utils.py
@@ -6,6 +6,7 @@ import numpy as np
 
 import probnum.random_variables as pnrv
 
+from .extendedkalman import DiscreteEKFComponent
 from .stoppingcriterion import StoppingCriterion
 
 ########################################################################################################################
@@ -86,6 +87,59 @@ def condition_state_on_measurement(pred_rv, meas_rv, crosscov, data):
     new_cov = pred_rv.cov - crosscov @ np.linalg.solve(meas_rv.cov, crosscov.T)
     updated_rv = pnrv.Normal(new_mean, new_cov)
     return updated_rv
+
+
+# Perhaps `SR` can be made optional, too...
+def sqrt_kalman_update(
+    measurement_model, rv, time, data, _linearise_at=None
+) -> (pnrv.RandomVariable, typing.Dict):
+    r"""Compute the Kalman update in square-root form.
+
+    Assumes a measurement model of the form
+
+        .. math::  x \mapsto N(H x, R)
+
+    and acts only on the square-root of the predicted covariance.
+
+    See Eq. 48 in
+    https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.68.1059&rep=rep1&type=pdf.
+    """
+    if isinstance(measurement_model, DiscreteEKFComponent):
+        compute_jacobian_at = _linearise_at if _linearise_at is not None else rv
+
+        measurement_model.linearize(compute_jacobian_at)
+        H = measurement_model.linearized_model.state_trans_mat_fun(time)
+        SR = measurement_model.linearized_model.proc_noise_cov_cholesky_fun(time)
+        shift = measurement_model.linearized_model.shift_vec_fun(time)
+    else:
+        H = measurement_model.state_trans_mat_fun(time)
+        SR = measurement_model.proc_noise_cov_cholesky_fun(time)
+        shift = measurement_model.shift_vec_fun(time)
+
+    SC = rv.cov_cholesky
+
+    zeros = np.zeros(H.T.shape)
+    blockmat = np.block([[SR, H @ SC], [zeros, SC]]).T
+    big_triu = np.linalg.qr(blockmat, mode="r")
+    ndim_measurements = len(H)
+
+    measured_triu = big_triu[:ndim_measurements, :ndim_measurements]
+    measured_cholesky = triu_to_positive_tril(measured_triu)
+
+    postcov_triu = big_triu[ndim_measurements:, ndim_measurements:]
+    postcov_cholesky = triu_to_positive_tril(postcov_triu)
+    kalman_gain = big_triu[:ndim_measurements, ndim_measurements:].T @ np.linalg.inv(
+        measured_triu.T
+    )
+
+    meas_mean = H @ rv.mean + shift
+    meas_cov = measured_cholesky @ measured_cholesky
+    meas_rv = pnrv.Normal(meas_mean, cov=meas_cov, cov_cholesky=measured_cholesky)
+
+    new_mean = rv.mean + kalman_gain @ (data - meas_mean)
+    new_cov = postcov_cholesky @ postcov_cholesky.T
+    new_rv = pnrv.Normal(new_mean, cov=new_cov, cov_cholesky=postcov_cholesky)
+    return new_rv, meas_rv, {}
 
 
 def iterate_update(update_fun, stopcrit=None):
@@ -218,3 +272,78 @@ def rts_smooth_step_classic(
         + smoothing_gain @ (smoothed_rv.cov - predicted_rv.cov) @ smoothing_gain.T
     )
     return pnrv.Normal(new_mean, new_cov), {}
+
+
+# used for predict() and measure(), but more general than that,
+# so it has a more general name than the functions below.
+def cholesky_update(
+    S1: np.ndarray, S2: typing.Optional[np.ndarray] = None
+) -> np.ndarray:
+    r"""Compute Cholesky update/factorization :math:`C C^\top = S_1 S_1^\top + S_2 S_2^\top`.
+
+    This can be used in various ways.
+    For example, :math:`S_1` and :math:`S_2` do not need to be Cholesky factors; any matrix square-root is sufficient.
+    As long as :math:`C C^\top = S_1 S_1^\top + S_2 S_2^\top` is well-defined (and admits a Cholesky-decomposition),
+    :math:`S_1` and :math:`S_2` do not even have to be square.
+    """
+    # doc might need a bit more explanation in the future
+    # perhaps some doctest or so?
+    if S2 is not None:
+        stacked_up = np.vstack((S1.T, S2.T))
+    else:
+        stacked_up = np.vstack((S1.T))
+    upper_sqrtm = np.linalg.qr(stacked_up, mode="r")
+    return triu_to_positive_tril(upper_sqrtm)
+
+
+def sqrt_smoothing_step(
+    SC_past: np.ndarray,
+    A: np.ndarray,
+    SQ: np.ndarray,
+    SC_futu: np.ndarray,
+    G: np.ndarray,
+) -> np.ndarray:
+    r"""Smoothing step in square-root form.
+
+    Assumes a prior dynamic model of the form
+
+        .. math:: x \\mapsto N(A x, Q).
+
+    For the mathematical justification of this step, see Eq. 45 in
+    https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.68.1059&rep=rep1&type=pdf.
+
+    Parameters
+    ----------
+    SC_past
+        Square root of the filtered (not yet smoothed) covariance at time :math:`t_n`.
+    A
+        Dynamics matrix :math:`A`.
+    SQ
+        Square root of the diffusion matrix :math:`Q`, `Q=SQ SQ.T`.
+    SC_futu
+        Square root of the smoothed covariance at time :math:`t_{n+1}`.
+    G
+        Smoothing gain.
+    """
+    dim = len(A)
+    zeros = np.zeros((dim, dim))
+    blockmat = np.block(
+        [
+            [SC_past.T @ A.T, SC_past.T],
+            [SQ.T, zeros],
+            [zeros, SC_futu.T @ G.T],
+        ]
+    )
+    big_triu = np.linalg.qr(blockmat, mode="r")
+    SC = big_triu[dim : 2 * dim, dim:]
+    return triu_to_positive_tril(SC)
+
+
+def triu_to_positive_tril(triu_mat: np.ndarray) -> np.ndarray:
+    r"""Change an upper triangular matrix into a valid lower Cholesky factor.
+
+    Transpose, and change the sign of the diagonals to '+' if necessary.
+    """
+    tril_mat = triu_mat.T
+    with_pos_diag = tril_mat @ np.diag(np.sign(np.diag(tril_mat)))
+    return with_pos_diag
