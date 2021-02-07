eb9795d4e6eda288b877ab39e7d431a73ce0392e
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-07 18:39:58 +0100

    backward functions for lti sde

--- src/probnum/filtsmooth/statespace/sde.py
+++ src/probnum/filtsmooth/statespace/sde.py
@@ -9,6 +9,7 @@ import probnum.random_variables as pnrv
 from probnum.type import FloatArgType
 
 from . import discrete_transition, transition
+from .discrete_transition_utils import backward_rv_classic, forward_rv_classic
 from .sde_utils import linear_sde_statistics, matrix_fraction_decomposition
 
 
@@ -33,11 +34,25 @@ class SDE(transition.Transition):
         super().__init__(input_dim=dimension, output_dim=dimension)
 
     def forward_realization(
-        self, real, t, dt=None, _compute_gain=False, _diffusion=1.0
+        self,
+        real,
+        t,
+        dt=None,
+        _compute_gain=False,
+        _diffusion=1.0,
+        **kwargs,
     ):
         raise NotImplementedError
 
-    def forward_rv(self, rv, t, dt=None, _compute_gain=False, _diffusion=1.0):
+    def forward_rv(
+        self,
+        rv,
+        t,
+        dt=None,
+        _compute_gain=False,
+        _diffusion=1.0,
+        **kwargs,
+    ):
         raise NotImplementedError
 
     def backward_realization(
@@ -49,6 +64,7 @@ class SDE(transition.Transition):
         t=None,
         dt=None,
         _diffusion=1.0,
+        **kwargs,
     ):
         raise NotImplementedError
 
@@ -61,6 +77,7 @@ class SDE(transition.Transition):
         t=None,
         dt=None,
         _diffusion=1.0,
+        **kwargs,
     ):
         raise NotImplementedError
 
@@ -94,6 +111,7 @@ class LinearSDE(SDE):
         forcevecfun: Callable[[FloatArgType], np.ndarray],
         dispmatfun: Callable[[FloatArgType], np.ndarray],
         dimension=None,
+        moment_equation_stepsize=np.nan,
     ):
         self.driftmatfun = driftmatfun
         self.forcevecfun = forcevecfun
@@ -103,13 +121,14 @@ class LinearSDE(SDE):
             jacobfun=(lambda t, x: driftmatfun(t)),
             dimension=dimension,
         )
+        self.moment_equation_stepsize = moment_equation_stepsize
 
     def forward_realization(
         self,
         real,
-        start,
-        stop,
-        step,
+        t,
+        dt=None,
+        _compute_gain=False,
         _diffusion=1.0,
         **kwargs,
     ):
@@ -126,14 +145,16 @@ class LinearSDE(SDE):
             _diffusion=_diffusion,
         )
 
-    def forward_rv(self, rv, start, stop, step, _diffusion=1.0, **kwargs):
+    def forward_rv(
+        self,
+        rv,
+        t,
+        dt=None,
+        _compute_gain=False,
+        _diffusion=1.0,
+        **kwargs,
+    ):
 
-        if not isinstance(rv, pnrv.Normal):
-            errormsg = (
-                "Closed form transitions in linear SDE models is only "
-                "available for Gaussian initial conditions."
-            )
-            raise TypeError(errormsg)
         return linear_sde_statistics(
             rv,
             start,
@@ -147,27 +168,29 @@ class LinearSDE(SDE):
 
     def backward_realization(
         self,
-        real,
-        rv_past,
-        start,
-        stop=None,
-        step=None,
+        real_obtained,
+        rv,
+        rv_forwarded=None,
+        gain=None,
+        t=None,
+        dt=None,
         _diffusion=1.0,
-        _linearise_at=None,
+        **kwargs,
     ):
-        raise NotImplementedError
+        raise NotImplementedError("Not available (yet).")
 
     def backward_rv(
         self,
-        rv_futu,
-        rv_past,
-        start,
-        stop=None,
-        step=None,
+        rv_obtained,
+        rv,
+        rv_forwarded=None,
+        gain=None,
+        t=None,
+        dt=None,
         _diffusion=1.0,
-        _linearise_at=None,
+        **kwargs,
     ):
-        raise NotImplementedError
+        raise NotImplementedError("Not available (yet).")
 
 
 class LTISDE(LinearSDE):
@@ -192,7 +215,14 @@ class LTISDE(LinearSDE):
         This is L. It is the dispersion matrix of the SDE.
     """
 
-    def __init__(self, driftmat: np.ndarray, forcevec: np.ndarray, dispmat: np.ndarray):
+    def __init__(
+        self,
+        driftmat: np.ndarray,
+        forcevec: np.ndarray,
+        dispmat: np.ndarray,
+        use_forward_rv=forward_rv_classic,
+        use_backward_rv=backward_rv_classic,
+    ):
         _check_initial_state_dimensions(driftmat, forcevec, dispmat)
         dimension = len(driftmat)
         super().__init__(
@@ -205,64 +235,89 @@ class LTISDE(LinearSDE):
         self.forcevec = forcevec
         self.dispmat = dispmat
 
+        self.use_forward_rv = use_forward_rv
+        self.use_backward_rv = use_backward_rv
+
     def forward_realization(
         self,
         real,
-        start,
-        stop,
+        t,
+        dt=None,
+        _compute_gain=False,
         _diffusion=1.0,
         **kwargs,
     ):
+        zero_cov = np.zeros((len(real), len(real)))
+        real_as_rv = pnrv.Normal(mean=real, cov=zero_cov, cov_cholesky=zero_cov)
 
-        if not isinstance(real, np.ndarray):
-            raise TypeError(f"Numpy array expected, {type(real)} received.")
-        discretised_model = self.discretise(step=stop - start)
-        return discretised_model.forward_realization(real, start, _diffusion=_diffusion)
+        return self.forward_rv(
+            rv=real_as_rv,
+            t=t,
+            dt=dt,
+            _compute_gain=_compute_gain,
+            _diffusion=_diffusion,
+        )
 
     def forward_rv(
         self,
         rv,
-        start,
-        stop,
+        t,
+        dt=None,
+        _compute_gain=False,
         _diffusion=1.0,
         **kwargs,
     ):
-
-        if not isinstance(rv, pnrv.Normal):
-            errormsg = (
-                "Closed form transitions in LTI SDE models is only "
-                "available for Gaussian initial conditions."
-            )
-            raise TypeError(errormsg)
-        discretised_model = self.discretise(step=stop - start)
-        return discretised_model.forward_rv(rv, start, _diffusion=_diffusion)
+        discretised_model = self.discretise(dt=dt)
+        return discretised_model.forward_rv(rv, t, _diffusion=_diffusion)
 
     def backward_realization(
         self,
-        real,
-        rv_past,
-        start,
-        stop=None,
-        step=None,
+        real_obtained,
+        rv,
+        rv_forwarded=None,
+        gain=None,
+        t=None,
+        dt=None,
         _diffusion=1.0,
-        _linearise_at=None,
+        **kwargs,
     ):
-        raise NotImplementedError
+        zero_cov = np.zeros((len(real), len(real)))
+        real_as_rv = pnrv.Normal(mean=real, cov=zero_cov, cov_cholesky=zero_cov)
+
+        return self.backward_rv(
+            rv_obtained=real_as_rv,
+            rv=rv,
+            rv_forwarded=rv_forwarded,
+            gain=gain,
+            t=t,
+            dt=dt,
+            _diffusion=_diffusion,
+        )
 
     def backward_rv(
         self,
-        rv_futu,
-        rv_past,
-        start,
-        stop=None,
-        step=None,
+        rv_obtained,
+        rv,
+        rv_forwarded=None,
+        gain=None,
+        t=None,
+        dt=None,
         _diffusion=1.0,
-        _linearise_at=None,
+        **kwargs,
     ):
-        raise NotImplementedError
+        discretised_model = self.discretise(dt=dt)
+        return discretised_model.backward_rv(
+            rv_obtained=rv_obtained,
+            rv=rv,
+            rv_forwarded=rv_forwarded,
+            gain=gain,
+            t=t,
+            _diffusion=_diffusion,
+        )
 
-    def discretise(self, step):
-        """Returns a discrete transition model (i.e. mild solution to SDE) using matrix
+    @functools.lru_cache(maxsize=None)
+    def discretise(self, dt):
+        """Return a discrete transition model (i.e. mild solution to SDE) using matrix
         fraction decomposition.
 
         That is, matrices A(h) and Q(h) and vector s(h) such
@@ -274,9 +329,15 @@ class LTISDE(LinearSDE):
         """
         if np.linalg.norm(self.forcevec) > 0:
             raise NotImplementedError("MFD does not work for force>0 (yet).")
-        ah, qh, _ = matrix_fraction_decomposition(self.driftmat, self.dispmat, step)
+        ah, qh, _ = matrix_fraction_decomposition(self.driftmat, self.dispmat, dt)
         sh = np.zeros(len(ah))
-        return discrete_transition.DiscreteLTIGaussian(ah, sh, qh)
+        return discrete_transition.DiscreteLTIGaussian(
+            ah,
+            sh,
+            qh,
+            use_forward_rv=self.use_forward_rv,
+            use_backward_rv=self.use_backward_rv,
+        )
 
 
 def _check_initial_state_dimensions(driftmat, forcevec, dispmat):
