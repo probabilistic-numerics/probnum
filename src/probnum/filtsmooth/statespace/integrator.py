300fc045be9050256055e13311d22e43eb85c8bd
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-02-12 14:49:02 +0100

    matern tests

--- src/probnum/filtsmooth/statespace/integrator.py
+++ src/probnum/filtsmooth/statespace/integrator.py
@@ -340,6 +340,8 @@ class Matern(Integrator, sde.LTISDE):
         ordint: int,
         spatialdim: int,
         lengthscale: float,
+        forward_implementation="classic",
+        backward_implementation="classic",
     ):
 
         self.lengthscale = lengthscale
@@ -350,6 +352,8 @@ class Matern(Integrator, sde.LTISDE):
             driftmat=self._driftmat,
             forcevec=self._forcevec,
             dispmat=self._dispmat,
+            forward_implementation=forward_implementation,
+            backward_implementation=backward_implementation,
         )
 
     @property
@@ -372,3 +376,84 @@ class Matern(Integrator, sde.LTISDE):
         dispmat_1d = np.zeros(self.ordint + 1)
         dispmat_1d[-1] = 1.0  # Unit diffusion
         return np.kron(np.eye(self.spatialdim), dispmat_1d).T
+
+    def forward_rv(
+        self,
+        rv,
+        t,
+        dt=None,
+        compute_gain=False,
+        _diffusion=1.0,
+        **kwargs,
+    ):
+
+        # Fetch things into preconditioned space
+        rv = self.precon.inverse(dt) @ rv
+
+        # Apply preconditioning to system matrices
+        self.driftmat = self.precon.inverse(dt) @ self.driftmat @ self.precon(dt)
+        self.forcevec = self.precon.inverse(dt) @ self.forcevec
+        self.dispmat = self.precon.inverse(dt) @ self.dispmat
+
+        # Discretise and propagate
+        discretised_model = self.discretise(dt=dt)
+        rv, info = discretised_model.forward_rv(
+            rv, t, compute_gain=compute_gain, _diffusion=_diffusion
+        )
+
+        # Undo preconditioning and return
+        rv = self.precon(dt) @ rv
+        info["crosscov"] = self.precon(dt) @ info["crosscov"] @ self.precon(dt).T
+        warnings.warn("What happens to the gain here???")
+
+        self.driftmat = self.precon(dt) @ self.driftmat @ self.precon.inverse(dt)
+        self.forcevec = self.precon(dt) @ self.forcevec
+        self.dispmat = self.precon(dt) @ self.dispmat
+
+        return rv, info
+
+    def backward_rv(
+        self,
+        rv_obtained,
+        rv,
+        rv_forwarded=None,
+        gain=None,
+        t=None,
+        dt=None,
+        _diffusion=1.0,
+        **kwargs,
+    ):
+        # Fetch things into preconditioned space
+        rv_obtained = self.precon.inverse(dt) @ rv_obtained
+        rv = self.precon.inverse(dt) @ rv
+        rv_forwarded = (
+            self.precon.inverse(dt) @ rv_forwarded if rv_forwarded is not None else None
+        )
+        gain = (
+            self.precon.inverse(dt) @ gain @ self.precon.inverse(dt).T
+            if gain is not None
+            else None
+        )
+
+        # Apply preconditioning to system matrices
+        self.driftmat = self.precon.inverse(dt) @ self.driftmat @ self.precon(dt)
+        self.forcevec = self.precon.inverse(dt) @ self.forcevec
+        self.dispmat = self.precon.inverse(dt) @ self.dispmat
+
+        # Discretise and propagate
+        discretised_model = self.discretise(dt=dt)
+        rv, info = discretised_model.backward_rv(
+            rv_obtained=rv_obtained,
+            rv=rv,
+            rv_forwarded=rv_forwarded,
+            gain=gain,
+            t=t,
+            _diffusion=_diffusion,
+        )
+
+        # Undo preconditioning and return
+        rv = self.precon(dt) @ rv
+        self.driftmat = self.precon(dt) @ self.driftmat @ self.precon.inverse(dt)
+        self.forcevec = self.precon(dt) @ self.forcevec
+        self.dispmat = self.precon(dt) @ self.dispmat
+        return rv, info
--- tests/test_filtsmooth/test_statespace/test_integrator.py
+++ tests/test_filtsmooth/test_statespace/test_integrator.py
@@ -110,6 +110,39 @@ class TestIOUP(TestLTISDE, TestIntegrator):
         return self.transition
 
 
+class TestMatern(TestLTISDE, TestIntegrator):
+
+    # Replacement for an __init__ in the pytest language. See:
+    # https://stackoverflow.com/questions/21430900/py-test-skips-test-class-if-constructor-is-defined
+    @pytest.fixture(autouse=True)
+    def _setup(
+        self,
+        some_ordint,
+        forw_impl_string_linear_gauss,
+        backw_impl_string_linear_gauss,
+    ):
+        self.some_ordint = some_ordint
+        spatialdim = 1  # make tests compatible with some_normal_rv1, etc.
+        self.transition = pnfss.Matern(
+            ordint=self.some_ordint,
+            spatialdim=spatialdim,
+            lengthscale=1.2345,
+            forward_implementation=forw_impl_string_linear_gauss,
+            backward_implementation=backw_impl_string_linear_gauss,
+        )
+
+        self.G = lambda t: self.transition.driftmat
+        self.v = lambda t: self.transition.forcevec
+        self.L = lambda t: self.transition.dispmat
+
+        self.g = lambda t, x: self.G(t) @ x + self.v(t)
+        self.dg = lambda t, x: self.G(t)
+
+    @property
+    def integrator(self):
+        return self.transition
+
+
 @pytest.fixture
 def dt():
     return 0.1
