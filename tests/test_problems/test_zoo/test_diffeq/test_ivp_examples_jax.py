1cfee9f252a67fc554afec8a672d48f2ce19be44
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-13 13:29:42 +0100

    .

--- tests/test_problems/test_zoo/test_diffeq/test_ivp_examples_jax.py
+++ tests/test_problems/test_zoo/test_diffeq/test_ivp_examples_jax.py
@@ -4,6 +4,7 @@ import probnum.diffeq as pnd
 import probnum.problems as pnprob
 import probnum.problems.zoo.diffeq as diffeq_zoo
 
+# Jax dependency handling
 try:
     import jax
     import jax.numpy as jnp
@@ -16,8 +17,10 @@ try:
     IVPs = [diffeq_zoo.threebody_jax(), diffeq_zoo.vanderpol_jax()]
 except ImportError:
     JAX_AVAILABLE = False
-    IVPs = None
+    IVPs = []
 
+
+# Pytest decorators to select tests for each case
 only_if_jax_available = pytest.mark.skipif(not JAX_AVAILABLE, reason="requires jax")
 only_if_jax_is_not_available = pytest.mark.skipif(JAX_AVAILABLE)
 
@@ -25,9 +28,9 @@ only_if_jax_is_not_available = pytest.mark.skipif(JAX_AVAILABLE)
 # Tests for when JAX is available
 
 
-@only_if_jax_available
 @pytest.mark.parametrize("ivp_jax", IVPs)
 @pytest.mark.parametrize("order", [0, 1, 2])
+@only_if_jax_available
 def test_compute_all_derivatives_terminates_successfully(ivp_jax, order):
     """Test asserts that the examples in diffeq-zoo are compatible with
     `compute_all_derivatives`, which happens if they are implemented in jax, and jax is
@@ -37,20 +40,20 @@ def test_compute_all_derivatives_terminates_successfully(ivp_jax, order):
     assert isinstance(ivp, pnprob.InitialValueProblem)
 
 
-@only_if_jax_available
 @pytest.mark.parametrize("ivp_jax", IVPs)
+@only_if_jax_available
 def test_f(ivp_jax):
     ivp_jax.f(ivp_jax.t0, ivp_jax.y0)
 
 
-@only_if_jax_available
 @pytest.mark.parametrize("ivp_jax", IVPs)
+@only_if_jax_available
 def test_df(ivp_jax):
     ivp_jax.df(ivp_jax.t0, ivp_jax.y0)
 
 
-@only_if_jax_available
 @pytest.mark.parametrize("ivp_jax", IVPs)
+@only_if_jax_available
 def test_ddf(ivp_jax):
     ivp_jax.ddf(ivp_jax.t0, ivp_jax.y0)
 
