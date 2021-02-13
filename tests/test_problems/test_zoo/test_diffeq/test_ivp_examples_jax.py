c5d9971f787136dbe68d4f7533d72c921c0e4bd9
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-13 12:31:48 +0100

    increased test coverage

--- tests/test_problems/test_zoo/test_diffeq/test_ivp_examples_jax.py
+++ tests/test_problems/test_zoo/test_diffeq/test_ivp_examples_jax.py
@@ -8,7 +8,8 @@ import probnum.problems.zoo.diffeq as diffeq_zoo
 @pytest.mark.parametrize(
     "ivp_jax", [diffeq_zoo.threebody_jax(), diffeq_zoo.vanderpol_jax()]
 )
-def test_compute_all_derivatives_terminates_successfully(ivp_jax):
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_compute_all_derivatives_terminates_successfully(ivp_jax, order):
     """Test asserts that the examples in diffeq-zoo are compatible with
     `compute_all_derivatives`, which happens if they are implemented in jax, and jax is
     available in the current environment."""
