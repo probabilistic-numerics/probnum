9fc2ccf71a1d23dc5afd58685a852198759adffc
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-13 11:50:51 +0100

    ivp example renamde to ivp examples jax

--- src/probnum/problems/zoo/diffeq/__init__.py
+++ src/probnum/problems/zoo/diffeq/__init__.py
@@ -1,7 +1,7 @@
 """Test problems involving ordinary differential equations."""
 
 
-from ._ivp_examples import threebody_jax, vanderpol_jax
+from ._ivp_examples_jax import threebody_jax, vanderpol_jax
 
 # Public classes and functions. Order is reflected in documentation.
 __all__ = ["threebody_jax", "vanderpol_jax"]
