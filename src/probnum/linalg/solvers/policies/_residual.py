6b9a69aa977917c89b6771789b6d35ccc23b1fe3
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-02-21 19:54:22 +0100

    .

--- src/probnum/linalg/solvers/policies/_residual.py
+++ src/probnum/linalg/solvers/policies/_residual.py
@@ -13,7 +13,7 @@ __all__ = ["Residual"]
 class Residual(Policy):
     r"""Policy returning the negative residual of the linear system.
 
-    Returns an action given by the residual :math:`s_k = -r_{k-1} = A x_{k-1} - b`.
+    Returns an action given by the residual :math:`s_k = r_{k-1} = A x_{k-1} - b`.
     This can also be interpreted as the gradient of
     :math:`f(x)=\frac{1}{2}x^\top A x - b^\top x + c` assuming symmetric positive
     definite :math:`A`.
@@ -32,6 +32,6 @@ class Residual(Policy):
         if solver_state is None:
             solver_state = LinearSolverState(problem=problem, belief=belief)
 
-        action = -solver_state.cache.residual
+        action = solver_state.cache.residual
 
         return LinearSolverAction(actA=action)
