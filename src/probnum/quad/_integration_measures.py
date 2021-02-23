7327a551b871706f46b18bc9d68b1c0253ee372e
agessner <agessner@tue.mpg.de>
2021-02-23 11:06:08 +0100

    renamings

--- src/probnum/quad/_integration_measures.py
+++ src/probnum/quad/_integration_measures.py
@@ -8,11 +8,11 @@ from typing import Tuple
 import numpy as np
 
 
-
 class IntegrationMeasure(ABC):
     """
     An abstract class for a measure against which a target function is integrated.
     """
+
     def __init__(self, domain: Tuple[np.ndarray, np.ndarray], name: str):
         self.domain = domain
         self._name = name
@@ -22,6 +22,7 @@ class LebesgueMeasure(IntegrationMeasure):
     """
     A Lebesgue measure.
     """
+
     def __init__(self, domain: Tuple[np.ndarray, np.ndarray]):
         super().__init__(domain=domain, name="Lebesgue measure")
 
@@ -30,7 +31,13 @@ class GaussianMeasure(IntegrationMeasure):
     """
     A Gaussian measure.
     """
-    def __init__(self, domain: Tuple[np.ndarray, np.ndarray], mean: np.ndarray, covariance: np.ndarray):
+
+    def __init__(
+        self,
+        domain: Tuple[np.ndarray, np.ndarray],
+        mean: np.ndarray,
+        covariance: np.ndarray,
+    ):
         super().__init__(domain=domain, name="Gaussian measure")
         self.mean = mean
         self.cov = covariance
