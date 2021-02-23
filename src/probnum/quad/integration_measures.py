d3c8ff43fd78b80ee35505102f299607cdd818c1
agessner <agessner@tue.mpg.de>
2021-02-23 09:33:16 +0100

    structure

---
+++ src/probnum/quad/integration_measures.py
@@ -0,0 +1,36 @@
+"""
+Contains integration measures
+"""
+
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import numpy as np
+
+
+
+class IntegrationMeasure(ABC):
+    """
+    An abstract class for a measure against which a target function is integrated.
+    """
+    def __init__(self, domain: Tuple[np.ndarray, np.ndarray], name: str):
+        self.domain = domain
+        self._name = name
+
+
+class LebesgueMeasure(IntegrationMeasure):
+    """
+    A Lebesgue measure.
+    """
+    def __init__(self, domain: Tuple[np.ndarray, np.ndarray]):
+        super().__init__(domain=domain, name="Lebesgue measure")
+
+
+class GaussianMeasure(IntegrationMeasure):
+    """
+    A Gaussian measure.
+    """
+    def __init__(self, domain: Tuple[np.ndarray, np.ndarray], mean: np.ndarray, covariance: np.ndarray):
+        super().__init__(domain=domain, name="Gaussian measure")
+        self.mean = mean
+        self.cov = covariance
---
+++ src/probnum/quad/kernel_embeddings.py
@@ -0,0 +1,4 @@
+"""
+Contains the kernel embeddings, i.e., integrals over kernels.
+"""
+
