893527de16efd0ec331b47cf671f98c072b96af6
Nicholas Krämer <nicokraemer@gmx.net>
2021-02-24 11:21:16 +0100

    References issues in _project_rv and _precon_rv

--- src/probnum/diffeq/odefiltsmooth/kalman_odesolution.py
+++ src/probnum/diffeq/odefiltsmooth/kalman_odesolution.py
@@ -141,6 +141,13 @@ class KalmanODESolution(ODESolution):
 
 
 def _project_rv(projmat, rv):
+    # There is no way of checking whether `rv` has its Cholesky factor computed already or not.
+    # Therefore, since we need to update the Cholesky factor for square-root filtering,
+    # we also update the Cholesky factor for non-square-root algorithms here,
+    # which implies additional cost.
+    # See Issues #319 and #329.
+    # When they are resolved, this function here will hopefully be superfluous.
+
     new_mean = projmat @ rv.mean
     new_cov = projmat @ rv.cov @ projmat.T
     new_cov_cholesky = cholesky_update(projmat @ rv.cov_cholesky)
