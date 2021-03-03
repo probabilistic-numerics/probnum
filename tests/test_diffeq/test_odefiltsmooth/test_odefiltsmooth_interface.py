222623d19a511ff76a180376556eb94d2e91a7b7
Nicholas Krämer <nicokraemer@gmx.net>
2021-03-03 09:21:23 +0100

    speed up tests a bit

--- tests/test_diffeq/test_odefiltsmooth/test_odefiltsmooth_interface.py
+++ tests/test_diffeq/test_odefiltsmooth/test_odefiltsmooth_interface.py
@@ -9,7 +9,7 @@ from probnum.random_variables import Constant
 @pytest.fixture
 def ivp():
     initrv = Constant(20.0 * np.ones(2))
-    return ode.lotkavolterra([0.0, 0.5], initrv)
+    return ode.lotkavolterra([0.0, 0.25], initrv)
 
 
 @pytest.mark.parametrize("method", ["ek0", "ek1"])
@@ -30,12 +30,18 @@ def ivp():
         "matern92",
     ],
 )
-@pytest.mark.parametrize("dense_output", ["True", "False"])
-def test_adaptive_solver_successfull(ivp, method, which_prior, dense_output):
+@pytest.mark.parametrize("dense_output", [True, False])
+@pytest.mark.parametrize("step", [0.01, None])
+def test_adaptive_solver_successful(ivp, method, which_prior, dense_output, step):
+    """The solver terminates successfully for all sorts of parametrizations."""
     f = ivp.rhs
     df = ivp.jacobian
     t0, tmax = ivp.timespan
     y0 = ivp.initrv.mean
+
+    # Values that seem okay for this setup from experience.
+    lengthscale = 10.0
+    driftspeed = 1.0
     probsolve_ivp(
         f,
         t0,
@@ -47,4 +53,7 @@ def test_adaptive_solver_successfull(ivp, method, which_prior, dense_output):
         which_prior=which_prior,
         method=method,
         dense_output=dense_output,
+        step=step,
+        lengthscale=lengthscale,
+        driftspeed=driftspeed,
     )
