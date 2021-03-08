e14b6c7878a4328d79f8c3ab3b036bf8eaa0579d
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-03-08 11:31:09 +0100

    removed dy0_all and dy0_error

--- src/probnum/problems/_problems.py
+++ src/probnum/problems/_problems.py
@@ -106,8 +106,8 @@ class InitialValueProblem:
     df: typing.Optional[typing.Callable[[float, np.ndarray], np.ndarray]] = None
     ddf: typing.Optional[typing.Callable[[float, np.ndarray], np.ndarray]] = None
 
-    dy0_all: typing.Optional[np.ndarray] = None
-    dy0_errors: typing.Optional[np.ndarray] = None
+    # dy0_all: typing.Optional[np.ndarray] = None
+    # dy0_errors: typing.Optional[np.ndarray] = None
 
     # For testing and benchmarking
     solution: typing.Optional[typing.Callable[[float, np.ndarray], np.ndarray]] = None
