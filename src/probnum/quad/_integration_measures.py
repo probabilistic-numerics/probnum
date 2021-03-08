1ef3947a91b067dda5332d67681ba30377dd033e
Toni Karvonen <tskarvon@iki.fi>
2021-03-08 12:30:30 +0200

    Rewrite GaussianMeasure using random_variable._normal

--- src/probnum/quad/_integration_measures.py
+++ src/probnum/quad/_integration_measures.py
@@ -5,6 +5,7 @@ Contains integration measures
 import abc
 from typing import Tuple, Optional, Union
 from probnum.type import IntArgType, FloatArgType
+from probnum.random_variables._normal import Normal
 
 import numpy as np
 
@@ -15,14 +16,14 @@ class IntegrationMeasure(abc.ABC):
     The integration measure is assumed normalized.
     """
 
-    def __init__(self,
-                 ndim: IntArgType,
-                 domain: Tuple[Union[np.ndarray, FloatArgType], Union[np.ndarray,
-                                                                      FloatArgType]],
-                 name: str
-                 ):
+    def __init__(
+        self,
+        dim: IntArgType,
+        domain: Tuple[Union[np.ndarray, FloatArgType], Union[np.ndarray, FloatArgType]],
+        name: str = "Custom measure",
+    ):
 
-        self._set_dimension_domain(ndim, domain)
+        self._set_dimension_domain(dim, domain)
         self._name = name
 
     def sample(self, n_sample):
@@ -31,37 +32,35 @@ class IntegrationMeasure(abc.ABC):
         """
         raise NotImplementedError
 
-    def _set_dimension_domain(self, ndim, domain):
+    def _set_dimension_domain(self, dim, domain):
         """
         Sets the integration domain and dimension. Error is thrown if the given
         dimension and domain limits do not match.
 
         TODO: check that dimensions match and the domain is not empty
         """
-        if ndim >= 1:
-            self.ndim = ndim
+        if dim >= 1:
+            self.dim = dim
         else:
-            # Error if the dimension is stupid
-            pass
+            raise ValueError(f"Domain dimension dim={dim} must be positive.")
         if np.isscalar(domain[0]):
             # Use same domain limit in all dimensions if only one limit is given
-            domain_a = np.full((self.ndim, 1), domain[0])
+            domain_a = np.full((self.dim, 1), domain[0])
         else:
             domain_a = domain[0]
         if np.isscalar(domain[1]):
-            domain_b = np.full((self.ndim, 1), domain[1])
+            domain_b = np.full((self.dim, 1), domain[1])
         else:
             domain_b = domain[1]
         self.domain = (domain_a, domain_b)
+
 
 class LebesgueMeasure(IntegrationMeasure):
     """
     A Lebesgue measure.
     """
 
-    def __init__(self,
-                 domain: Tuple[np.ndarray, np.ndarray]
-                 ):
+    def __init__(self, domain: Tuple[np.ndarray, np.ndarray]):
 
         super().__init__(domain=domain, name="Lebesgue measure")
 
@@ -71,47 +70,33 @@ class GaussianMeasure(IntegrationMeasure):
     A Gaussian measure.
     """
 
-    def __init__(self,
-                 ndim: Optional[IntArgType],
-                 mean: Optional[Union[np.ndarray, FloatArgType]] = 0.,
-                 covariance: Optional[Union[np.ndarray, FloatArgType]] = 1.,
-                 ):
+    def __init__(
+        self,
+        mean: Union[float, np.floating, np.ndarray],
+        cov: Union[float, np.floating, np.ndarray],
+    ):
+        # Exploit random variables to carry out mean and covariance checks
+        self.random_variable = Normal(mean, cov)
+        self.mean = self.random_variable.mean
+        self.cov = self.random_variable.cov
+        self.diagonal_covariance = not bool(
+            np.count_nonzero(self.cov - np.diag(np.diagonal(self.cov)))
+        )
 
-        super().__init__(ndim=ndim, domain=(-np.Inf, np.Inf), name="Gaussian measure")
-        self._set_mean_covariance(mean, covariance)
+        # Use the mean to set the dimension
+        if len(self.mean.shape) == 0:
+            self.dim = 1
+        else:
+            self.dim = len(self.mean)
+
+        super().__init__(
+            dim=self.dim,
+            domain=(np.full((self.dim,), -np.Inf), np.full((self.dim,), np.Inf)),
+            name="Gaussian measure",
+        )
 
     def sample(self, n_sample):
-        """
-        Sample from Gaussian measure.
-        """
-        raise NotImplementedError
-
-    def _set_mean_covariance(self, mean, covariance):
-        """
-        Sets the mean and covariance of the Gaussian integration measure. Throws
-        error if their dimensions do not match with ndim.
-
-        TODO: dimension or covariance positivity checks have not been implemented
-        """
-        if np.isscalar(mean):
-            self.mean = np.full((self.ndim, 1), mean)
+        if self.dim == 1:
+            return self.random_variable._univariate_sample(size=(n_sample, 1))
         else:
-            self.mean = mean
-
-        if np.isscalar(covariance):
-            if covariance <= 0:
-                # TODO: raise error
-                pass
-            self.covariance = covariance*np.eye(self.ndim)
-            self.diagonal_covariance = True
-        elif covariance.ndim == 1:
-            if not all(covariance > 0):
-                # TODO: raise error
-                pass
-            self.covariance = np.diag(covariance)
-            self.diagonal_covariance = True
-        else:
-            # TODO: raise error if covariance matrix is not positive-definite
-            self.covariance = covariance
-            self.diagonal_covariance = False
-
+            return self.random_variable._dense_sample(size=n_sample)
