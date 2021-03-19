9cd089579bec9da6770990669f52788cda1296ca
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-03-19 11:46:34 +0100

    whitespaces

--- src/probnum/statespace/integrator.py
+++ src/probnum/statespace/integrator.py
@@ -1,4 +1,5 @@
 """Integrated processes such as the integrated Wiener process or the Matern process."""
+
 try:
     # cached_property is only available in Python >=3.8
     from functools import cached_property
@@ -25,18 +26,24 @@ class Integrator:
         self.precon = NordsieckLikeCoordinates.from_order(self.ordint, self.spatialdim)
 
     def proj2coord(self, coord: int) -> np.ndarray:
-        """Projection matrix to :math:`i` th coordinates. Computes the matrix.
+        """Projection matrix to :math:`i` th coordinates.
+
+        Computes the matrix
 
         .. math:: H_i = \\left[ I_d \\otimes e_i \\right] P^{-1},
-        where :math:`e_i` is the :math:`i` th unit vector,
+
+        where :math:`e_i` is the :math:`i` th unit vector
         that projects to the :math:`i` th coordinate of a vector.
+
         If the ODE is multidimensional, it projects to **each** of the
         :math:`i` th coordinates of each ODE dimension.
+
         Parameters
         ----------
         coord : int
             Coordinate index :math:`i` which to project to.
             Expected to be in range :math:`0 \\leq i \\leq q + 1`.
+
         Returns
         -------
         np.ndarray, shape=(d, d*(q+1))
@@ -50,11 +57,17 @@ class Integrator:
     @property
     def _derivwise2coordwise_projmat(self) -> np.ndarray:
         r"""Projection matrix to change the ordering of the state representation in an :class:`Integrator` from coordinate-wise to derivative-wise representation.
+
         A coordinate-wise ordering is
+
         .. math:: (y_1, \dot y_1, \ddot y_1, y_2, \dot y_2, ..., y_d^{(\nu)})
+
         and a derivative-wise ordering is
+
         .. math:: (y_1, y_2, ..., y_d, \dot y_1, \dot y_2, ..., \dot y_d, \ddot y_1, ..., y_d^{(\nu)}).
+
         Default representation in an :class:`Integrator` is coordinate-wise ordering, but sometimes, derivative-wise ordering is more convenient.
+
         See Also
         --------
         :attr:`Integrator._convert_coordwise_to_derivwise`
@@ -73,11 +86,17 @@ class Integrator:
     @property
     def _coordwise2derivwise_projmat(self) -> np.ndarray:
         r"""Projection matrix to change the ordering of the state representation in an :class:`Integrator` from derivative-wise to coordinate-wise representation.
+
         A coordinate-wise ordering is
+
         .. math:: (y_1, \dot y_1, \ddot y_1, y_2, \dot y_2, ..., y_d^{(\nu)})
+
         and a derivative-wise ordering is
+
         .. math:: (y_1, y_2, ..., y_d, \dot y_1, \dot y_2, ..., \dot y_d, \ddot y_1, ..., y_d^{(\nu)}).
+
         Default representation in an :class:`Integrator` is coordinate-wise ordering, but sometimes, derivative-wise ordering is more convenient.
+
         See Also
         --------
         :attr:`Integrator._convert_coordwise_to_derivwise`
@@ -90,7 +109,9 @@ class Integrator:
         state: np.ndarray, ordint: pntype.IntArgType, spatialdim: pntype.IntArgType
     ) -> np.ndarray:
         """Convert coordinate-wise representation to derivative-wise representation.
+
         Lightweight call to the respective property in :class:`Integrator`.
+
         Parameters
         ----------
         state:
@@ -99,6 +120,7 @@ class Integrator:
             Order of the integrator-state. Usually, this is the order of the highest derivative in the state.
         spatialdim:
             Spatial dimension of the integrator. Usually, this is the number of states associated with each derivative.
+
         See Also
         --------
         :attr:`Integrator._coordwise2derivwise_projmat`
@@ -113,6 +135,7 @@ class Integrator:
     ) -> np.ndarray:
         """Convert coordinate-wise representation to derivative-wise representation.
         Lightweight call to the respective property in :class:`Integrator`.
+
         Parameters
         ----------
         state:
@@ -121,6 +144,7 @@ class Integrator:
             Order of the integrator-state. Usually, this is the order of the highest derivative in the state.
         spatialdim:
             Spatial dimension of the integrator. Usually, this is the number of states associated with each derivative.
+
         See Also
         --------
         :attr:`Integrator._coordwise2derivwise_projmat`
