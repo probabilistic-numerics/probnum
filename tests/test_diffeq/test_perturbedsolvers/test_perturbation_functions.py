6562f98d47f5ac80b2eb31850e6267ee91285540
NinaEffenberger <nina-sophie.effenberger@student.uni-tuebingen.de>
2021-04-14 15:53:25 +0200

    changed variable name

--- tests/test_diffeq/test_perturbedsolvers/test_perturbation_functions.py
+++ tests/test_diffeq/test_perturbedsolvers/test_perturbation_functions.py
@@ -8,18 +8,24 @@ random_state = np.random.mtrand.RandomState(seed=1234)
 def test_perturb_uniform():
     num_samples = 100
     all_steps = 0
+    unperturbed_step = 0.2
     for _ in range(num_samples):
-        stp = _perturbation_functions.perturb_uniform(0.2, 4, 1)
+        stp = _perturbation_functions.perturb_uniform(unperturbed_step, 4, 1)
         all_steps += stp
     mean_suggested_step = all_steps / num_samples
-    np.testing.assert_allclose(0.2, mean_suggested_step, atol=1e-4, rtol=1e-4)
+    np.testing.assert_allclose(
+        mean_suggested_step, unperturbed_step, atol=1e-4, rtol=1e-4
+    )
 
 
 def test_perturb_lognormal():
     num_samples = 100
     all_steps = 0
+    unperturbed_step = 0.2
     for _ in range(num_samples):
-        stp = _perturbation_functions.perturb_lognormal(0.2, 4, 1)
+        stp = _perturbation_functions.perturb_lognormal(unperturbed_step, 4, 1)
         all_steps += stp
     mean_suggested_step = all_steps / num_samples
-    np.testing.assert_allclose(mean_suggested_step, 0.2, atol=1e-4, rtol=1e-4)
+    np.testing.assert_allclose(
+        mean_suggested_step, unperturbed_step, atol=1e-4, rtol=1e-4
+    )
