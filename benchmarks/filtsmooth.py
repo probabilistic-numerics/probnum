186f5f760e30644f3e10f8a6d7e3e9076263333f
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-04-20 18:02:58 +0200

    Benchmark sqrt and classic implementations

--- benchmarks/filtsmooth.py
+++ benchmarks/filtsmooth.py
@@ -1,4 +1,6 @@
 """Benchmarks for Gaussian filtering."""
+import functools
+
 import numpy as np
 
 from probnum import filtsmooth, problems, randvars, statespace
@@ -50,13 +52,18 @@ def load_pendulum():
 class Filtering:
     """Benchmark Kalman filter for different linearization techniques."""
 
-    param_names = ["linearization"]
-    params = [["ekf", "ukf"]]
+    param_names = ["linearization_implementation"]
+    params = [[("ekf", "classic"), ("ekf", "sqrt"), ("ukf", "classic")]]
 
-    def setup(self, linearization):
+    def setup(self, linearization_implementation):
         dynmod, measmod, initrv, info = load_pendulum()
+        linearization, implementation = linearization_implementation
         _lin_method = {
-            "ekf": filtsmooth.DiscreteEKFComponent,
+            "ekf": functools.partial(
+                filtsmooth.DiscreteEKFComponent,
+                forward_implementation=implementation,
+                backward_implementation=implementation,
+            ),
             "ukf": filtsmooth.DiscreteUKFComponent,
         }[linearization]
 
@@ -77,23 +84,28 @@ class Filtering:
             initrv=initrv,
         )
 
-    def time_filter(self, linearization):
+    def time_filter(self, linearization_implementation):
         self.kalman_filter.filter(self.regression_problem)
 
-    def peakmem_filter(self, linearization):
+    def peakmem_filter(self, linearization_implementation):
         self.kalman_filter.filter(self.regression_problem)
 
 
 class Smoothing:
     """Benchmark Kalman smoother for different linearization techniques."""
 
-    param_names = ["linearization"]
-    params = [["ekf", "ukf"]]
+    param_names = ["linearization_implementation"]
+    params = [[("ekf", "classic"), ("ekf", "sqrt"), ("ukf", "classic")]]
 
-    def setup(self, linearization):
+    def setup(self, linearization_implementation):
         dynmod, measmod, initrv, info = load_pendulum()
+        linearization, implementation = linearization_implementation
         _lin_method = {
-            "ekf": filtsmooth.DiscreteEKFComponent,
+            "ekf": functools.partial(
+                filtsmooth.DiscreteEKFComponent,
+                forward_implementation=implementation,
+                backward_implementation=implementation,
+            ),
             "ukf": filtsmooth.DiscreteUKFComponent,
         }[linearization]
 
@@ -115,10 +127,10 @@ class Smoothing:
         )
         self.filtering_posterior = self.kalman_filter.filter(self.regression_problem)
 
-    def time_smooth(self, linearization):
+    def time_smooth(self, linearization_implementation):
         self.kalman_filter.smooth(filter_posterior=self.filtering_posterior)
 
-    def peakmem_smooth(self, linearization):
+    def peakmem_smooth(self, linearization_implementation):
         self.kalman_filter.smooth(filter_posterior=self.filtering_posterior)
 
 
@@ -131,13 +143,18 @@ class DenseGridOperations:
         * Drawing samples from the smoothing posterior
     """
 
-    param_names = ["linearization", "num_samples"]
-    params = [["ekf", "ukf"], [1, 10]]
+    param_names = ["linearization_implementation", "num_samples"]
+    params = [[("ekf", "classic"), ("ekf", "sqrt"), ("ukf", "classic")], [1, 10]]
 
-    def setup(self, linearization, num_samples):
+    def setup(self, linearization_implementation, num_samples):
         dynmod, measmod, initrv, info = load_pendulum()
+        linearization, implementation = linearization_implementation
         _lin_method = {
-            "ekf": filtsmooth.DiscreteEKFComponent,
+            "ekf": functools.partial(
+                filtsmooth.DiscreteEKFComponent,
+                forward_implementation=implementation,
+                backward_implementation=implementation,
+            ),
             "ukf": filtsmooth.DiscreteUKFComponent,
         }[linearization]
 
@@ -172,20 +189,20 @@ class DenseGridOperations:
             filter_posterior=self.filtering_posterior
         )
 
-    def time_sample(self, linearization, num_samples):
+    def time_sample(self, linearization_implementation, num_samples):
         self.smoothing_posterior.sample(t=self.dense_locations, size=num_samples)
 
-    def peakmem_sample(self, linearization, num_samples):
+    def peakmem_sample(self, linearization_implementation, num_samples):
         self.smoothing_posterior.sample(t=self.dense_locations, size=num_samples)
 
-    def time_dense_filter(self, linearization, num_samples):
+    def time_dense_filter(self, linearization_implementation, num_samples):
         self.filtering_posterior(self.dense_locations)
 
-    def peakmem_dense_filter(self, linearization, num_samples):
+    def peakmem_dense_filter(self, linearization_implementation, num_samples):
         self.filtering_posterior(self.dense_locations)
 
-    def time_dense_smoother(self, linearization, num_samples):
+    def time_dense_smoother(self, linearization_implementation, num_samples):
         self.smoothing_posterior(self.dense_locations)
 
-    def peakmem_dense_smoother(self, linearization, num_samples):
+    def peakmem_dense_smoother(self, linearization_implementation, num_samples):
         self.smoothing_posterior(self.dense_locations)
