3d408edb4ab05077c5e461dc9b042ff7a175c546
NinaEffenberger <nina-sophie.effenberger@student.uni-tuebingen.de>
2021-04-20 09:33:15 +0200

    adaptations as proposed by Nico, parametrized tests

--- src/probnum/diffeq/perturbedsolvers/_perturbation_functions.py
+++ src/probnum/diffeq/perturbedsolvers/_perturbation_functions.py
@@ -1,4 +1,4 @@
-"""perturbation functions to perturb the stepsize.
+"""Perturbation functions to perturb the stepsize.
 
 References
 ----------
@@ -10,6 +10,7 @@ import scipy
 
 def perturb_uniform(step, solver_order, noise_scale, random_state=None, size=()):
     """Perturb the step with uniformly distributed noise scaled by noise-scale [1]_.
+
     proposed by Abdulle and Garegnani(2020)
 
     Parameters
@@ -20,19 +21,20 @@ def perturb_uniform(step, solver_order, noise_scale, random_state=None, size=())
         order of the solver
     noise_scale : float
         scales the perturbation
-    seed : int
+    seed : RandomStateArgType
         seed for pseudo-random number generator.
     """
-    scipy.stats.uniform.rvs(random_state=random_state)
-    try:
-        noisy_step = np.random.uniform(
-            step - noise_scale * step ** (solver_order + 0.5),
-            step + noise_scale * step ** (solver_order + 0.5),
-            size,
+    if step >= 1.0:
+        raise ValueError("Stepsize too large (>= 1)")
+    else:
+        uniform_rv_samples = scipy.stats.uniform.rvs(
+            random_state=random_state, size=size
         )
-    except ValueError:
-        print("ValueError: Stepsize is too large (>=1)")
-    return noisy_step
+        shift = noise_scale * step ** (solver_order + 0.5)
+        left_boundary = step - shift
+        right_boundary = step + shift
+        samples = left_boundary + (right_boundary - left_boundary) * uniform_rv_samples
+    return samples
 
 
 def perturb_lognormal(step, solver_order, noise_scale, random_state=None, size=()):
@@ -46,13 +48,15 @@ def perturb_lognormal(step, solver_order, noise_scale, random_state=None, size=(
         order of the solver
     noise_scale : float
         scales the perturbation
-    seed : int
+    seed : RandomStateArgType
         seed for pseudo-random number generator.
     """
-    scipy.stats.uniform.rvs(random_state=random_state)
-    mean = np.log(step) - np.log(
-        np.sqrt(1 + noise_scale * (step ** (2 * solver_order)))
+    shift = 0.5 * np.log(1 + noise_scale * (step ** (2 * solver_order)))
+    mean = np.log(step) - shift
+    cov = 2 * shift
+    samples = np.exp(
+        scipy.stats.multivariate_normal.rvs(
+            mean=mean, cov=cov, size=size, random_state=random_state
+        )
     )
-    cov = np.log(1 + noise_scale * (step ** (2 * solver_order)))
-    noisy_step = np.exp(np.random.normal(mean, cov, size))
-    return noisy_step
+    return samples
--- tests/test_diffeq/test_perturbedsolvers/test_perturbation_functions.py
+++ tests/test_diffeq/test_perturbedsolvers/test_perturbation_functions.py
@@ -26,35 +26,32 @@ def num_samples():
     return 100
 
 
-def test_perturb_uniform_mean(step, solver_order, noise_scale, num_samples):
-    suggested_steps = _perturbation_functions.perturb_uniform(
+@pytest.mark.parametrize(
+    "perturb_fct",
+    [
+        _perturbation_functions.perturb_uniform,
+        _perturbation_functions.perturb_lognormal,
+    ],
+)
+def test_mean(perturb_fct, step, solver_order, noise_scale, num_samples):
+    suggested_steps = perturb_fct(
         step, solver_order, noise_scale, random_state=1, size=num_samples
     )
     mean_suggested_step = np.sum(suggested_steps) / num_samples
     np.testing.assert_allclose(mean_suggested_step, step, atol=1e-4, rtol=1e-4)
 
 
-def test_perturb_lognormal_mean(step, solver_order, noise_scale, num_samples):
-    suggested_steps = _perturbation_functions.perturb_lognormal(
-        step, solver_order, noise_scale, random_state=1, size=num_samples
-    )
-    mean_suggested_step = np.sum(suggested_steps) / num_samples
-    np.testing.assert_allclose(mean_suggested_step, step, atol=1e-6, rtol=1e-6)
-
-
-def test_perturb_uniform_var(step, solver_order, noise_scale, num_samples):
-    expected_var = (1 / 3) * step ** (2 * solver_order + 1)
-    suggested_steps = _perturbation_functions.perturb_uniform(
+@pytest.mark.parametrize(
+    "perturb_fct",
+    [
+        _perturbation_functions.perturb_uniform,
+        _perturbation_functions.perturb_lognormal,
+    ],
+)
+def test_var(perturb_fct, step, solver_order, noise_scale, num_samples):
+    expected_var = step ** (2 * solver_order + 1)
+    suggested_steps = perturb_fct(
         step, solver_order, noise_scale, random_state=1, size=num_samples
     )
     var = ((suggested_steps - step) ** 2) / num_samples
     np.testing.assert_allclose(expected_var, var, atol=1e-4, rtol=1e-4)
-
-
-def test_perturb_lognormal_var(step, solver_order, noise_scale, num_samples):
-    expected_var = (1 / 3) * step ** (2 * solver_order + 1)
-    suggested_steps = _perturbation_functions.perturb_uniform(
-        step, solver_order, noise_scale, random_state=1, size=num_samples
-    )
-    var = ((suggested_steps - step) ** 2) / num_samples
-    np.testing.assert_allclose(expected_var, var, atol=1e-6, rtol=1e-6)
