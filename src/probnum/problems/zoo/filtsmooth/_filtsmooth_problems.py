333de00cadaacc4a9fa566d5ce349fe2fbb3ca23
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-04-23 15:50:07 +0200

    Refactoring

--- src/probnum/problems/zoo/filtsmooth/_filtsmooth_problems.py
+++ src/probnum/problems/zoo/filtsmooth/_filtsmooth_problems.py
@@ -1,11 +1,19 @@
+from typing import Optional, Tuple
+
 import numpy as np
 
 from probnum import diffeq, filtsmooth, problems, randvars, statespace
+from probnum.type import FloatArgType
 
 __all__ = ["car_tracking", "logistic_ode", "ornstein_uhlenbeck", "pendulum"]
 
 
-def car_tracking(measurement_variance=0.5, times=None, initrv=None):
+def car_tracking(
+    measurement_variance: FloatArgType = 0.5,
+    process_diffusion: FloatArgType = 1.0,
+    times: Optional[np.ndarray] = None,
+    initrv: Optional[randvars.RandomVariable] = None,
+):
     r"""Filtering/smoothing setup for a simple car-tracking scenario.
 
     A linear, time-invariant Gaussian state space model for car-tracking, based on
@@ -37,23 +45,24 @@ def car_tracking(measurement_variance=0.5, times=None, initrv=None):
 
     Parameters
     ----------
-    delta_t : float
-        The step size of the discretized time grid on which the model is considered.
-    measurement_variance : float
-        The marginal measurement variance.
-    t_max : float
-        The time limit of the grid.
+    measurement_variance
+        Marginal measurement variance.
+    process_diffusion
+        Diffusion constant for the dynamics.
+    times
+        Time grid for the filtering/smoothing problem.
+    initrv
+        Initial random variable.
 
     Returns
     -------
-    dynmod
-        The `DiscreteLTIGaussian` dynamics model.
-    measmod
-        The `DiscreteLTIGaussian` measurement model.
-    initrv
-        A `Normal` random variable for the Gaussian initial conditions.
     regression_problem
-        A `RegressionProblem` with time points and noisy observations.
+        `RegressionProblem` object with time points and noisy observations.
+    statespace_components
+        Dictionary containing
+        - dynamics model
+        - measurement model
+        - initial random variable
 
     References
     ----------
@@ -62,11 +71,12 @@ def car_tracking(measurement_variance=0.5, times=None, initrv=None):
 
     """
 
-    dynmod = statespace.IBM(ordint=1, spatialdim=2)
+    dynamics_model = statespace.IBM(ordint=1, spatialdim=2)
+    dynamics_model.dispmat *= process_diffusion
 
     measurement_matrix = np.eye(2, 4)
     measurement_cov = measurement_variance * np.eye(2)
-    measmod = statespace.DiscreteLTIGaussian(
+    measurement_model = statespace.DiscreteLTIGaussian(
         state_trans_mat=measurement_matrix,
         shift_vec=np.zeros(2),
         proc_noise_cov_mat=measurement_cov,
@@ -75,20 +85,31 @@ def car_tracking(measurement_variance=0.5, times=None, initrv=None):
     if initrv is None:
         initrv = randvars.Normal(np.zeros(4), 0.5 * measurement_variance * np.eye(4))
 
-    # Generate data
+    # Set up regression problem
     if times is None:
         times = np.arange(0.0, 20.0, step=0.2)
     states, obs = statespace.generate_samples(
-        dynmod=dynmod, measmod=measmod, initrv=initrv, times=times
+        dynmod=dynamics_model, measmod=measurement_model, initrv=initrv, times=times
     )
     regression_problem = problems.RegressionProblem(
         observations=obs, locations=times, solution=states
     )
-    statespace_components = dict(dynmod=dynmod, measmod=measmod, initrv=initrv)
+
+    statespace_components = dict(
+        dynamics_model=dynamics_model,
+        measurement_model=measurement_model,
+        initrv=initrv,
+    )
     return regression_problem, statespace_components
 
 
-def ornstein_uhlenbeck(delta_t=0.2, lam=0.21, dynvar=0.5, measvar=0.1, t_max=20.0):
+def ornstein_uhlenbeck(
+    measurement_variance: FloatArgType = 0.1,
+    driftspeed: FloatArgType = 0.21,
+    process_diffusion: FloatArgType = 0.5,
+    times: Optional[np.ndarray] = None,
+    initrv: Optional[randvars.RandomVariable] = None,
+):
     r"""Filtering/smoothing setup based on an Ornstein Uhlenbeck process.
 
     A linear, time-invariant state space model for the dynamics of a time-invariant
@@ -107,27 +128,27 @@ def ornstein_uhlenbeck(delta_t=0.2, lam=0.21, dynvar=0.5, measvar=0.1, t_max=20.
 
     Parameters
     ----------
-    delta_t : float
-        The step size of the discretized time grid on which the model is considered.
-    lam : float
-        The drift coefficient :math:`\lambda` in the Ornstein-Uhlenbeck dynamics.
-    dynvar : float
-        The process noise variance.
-    measvar: float
-        The measurement noise variance.
-    t_max : float
-        The time limit of the grid.
+    measurement_variance
+        Marginal measurement variance.
+    driftspeed
+        Drift parameter of the Ornstein-Uhlenbeck process.
+    process_diffusion
+        Diffusion constant for the dynamics
+    times
+        Time grid for the filtering/smoothing problem.
+    initrv
+        Initial random variable.
+
 
     Returns
     -------
-    dynmod
-        The `LTISDE` dynamics model.
-    measmod
-        The `DiscreteLTIGaussian` measurement model.
-    initrv
-        A `Normal` random variable for the Gaussian initial conditions.
     regression_problem
-        A `RegressionProblem` with time points and noisy observations.
+        `RegressionProblem` object with time points and noisy observations.
+    statespace_components
+        Dictionary containing
+        - dynamics model
+        - measurement model
+        - initial random variable
 
 
     References
@@ -136,33 +157,42 @@ def ornstein_uhlenbeck(delta_t=0.2, lam=0.21, dynvar=0.5, measvar=0.1, t_max=20.
         Cambridge University Press, 2019
     """
 
-    drift = -lam * np.eye(1)
-    force = np.zeros(1)
-    disp = np.sqrt(dynvar) * np.eye(1)
-    dynmod = statespace.LTISDE(
-        driftmat=drift,
-        forcevec=force,
-        dispmat=disp,
-    )
-    measmod = statespace.DiscreteLTIGaussian(
+    dynamics_model = statespace.IOUP(ordint=0, spatialdim=1, driftspeed=driftspeed)
+    dynamics_model.dispmat *= process_diffusion
+
+    measurement_model = statespace.DiscreteLTIGaussian(
         state_trans_mat=np.eye(1),
         shift_vec=np.zeros(1),
-        proc_noise_cov_mat=measvar * np.eye(1),
+        proc_noise_cov_mat=measurement_variance * np.eye(1),
     )
-    initrv = randvars.Normal(10 * np.ones(1), np.eye(1))
 
-    # Generate data
-    times = np.arange(0.0, t_max, step=delta_t)
+    if initrv is None:
+        initrv = randvars.Normal(10.0 * np.ones(1), np.eye(1))
+
+    # Set up regression problem
+    if times is None:
+        times = np.arange(0.0, 20.0, step=0.2)
     states, obs = statespace.generate_samples(
-        dynmod=dynmod, measmod=measmod, initrv=initrv, times=times
+        dynmod=dynamics_model, measmod=measurement_model, initrv=initrv, times=times
     )
     regression_problem = problems.RegressionProblem(
         observations=obs, locations=times, solution=states
     )
-    return dynmod, measmod, initrv, regression_problem
+
+    statespace_components = dict(
+        dynamics_model=dynamics_model,
+        measurement_model=measurement_model,
+        initrv=initrv,
+    )
+    return regression_problem, statespace_components
 
 
-def pendulum(delta_t=0.0075, measvar=0.1024, t_max=4.0):
+def pendulum(
+    measurement_variance: FloatArgType = 0.1024,
+    delta_t: FloatArgType = 0.0075,
+    t_max: FloatArgType = 4.0,
+    initrv: Optional[randvars.RandomVariable] = None,
+):
     r"""Filtering/smoothing setup for a (noisy) pendulum.
 
     A non-linear state space model for a pendulum with unknown forces acting on the
@@ -188,23 +218,24 @@ def pendulum(delta_t=0.0075, measvar=0.1024, t_max=4.0):
 
     Parameters
     ----------
-    delta_t : float
+    measurement_variance
+        Marginal measurement variance.
+    delta_t
         The step size of the discretized time grid on which the model is considered.
-    measvar: float
-        The measurement noise variance.
-    t_max : float
+    t_max
         The time limit of the grid.
+    initrv
+        Initial random variable.
 
     Returns
     -------
-    dynmod
-        The `DiscreteGaussian` dynamics model.
-    measmod
-        The `DiscreteGaussian` measurement model.
-    initrv
-        A `Normal` random variable for the Gaussian initial conditions.
     regression_problem
-        A `RegressionProblem` with time points and noisy observations.
+        `RegressionProblem` object with time points and noisy observations.
+    statespace_components
+        Dictionary containing
+        - dynamics model
+        - measurement model
+        - initial random variable
 
 
     References
@@ -217,6 +248,7 @@ def pendulum(delta_t=0.0075, measvar=0.1024, t_max=4.0):
     # Graviational constant
     g = 9.81
 
+    # Define non-linear dynamics and measurements
     def f(t, x):
         x1, x2 = x
         y1 = x1 + x2 * delta_t
@@ -237,42 +269,53 @@ def pendulum(delta_t=0.0075, measvar=0.1024, t_max=4.0):
         x1, x2 = x
         return np.array([[np.cos(x1), 0.0]])
 
-    q = 1.0 * (
+    process_noise_cov = (
         np.diag(np.array([delta_t ** 3 / 3, delta_t]))
         + np.diag(np.array([delta_t ** 2 / 2]), 1)
         + np.diag(np.array([delta_t ** 2 / 2]), -1)
     )
-    r = measvar * np.eye(1)
-    initmean = np.ones(2)
-    initcov = measvar * np.eye(2)
-    dynmod = statespace.DiscreteGaussian(
+
+    dynamics_model = statespace.DiscreteGaussian(
         input_dim=2,
         output_dim=2,
         state_trans_fun=f,
-        proc_noise_cov_mat_fun=lambda t: q,
+        proc_noise_cov_mat_fun=lambda t: process_noise_cov,
         jacob_state_trans_fun=df,
     )
-    measmod = statespace.DiscreteGaussian(
+
+    measurement_model = statespace.DiscreteGaussian(
         input_dim=2,
         output_dim=1,
         state_trans_fun=h,
-        proc_noise_cov_mat_fun=lambda t: r,
+        proc_noise_cov_mat_fun=lambda t: measurement_variance * np.eye(1),
         jacob_state_trans_fun=dh,
     )
-    initrv = randvars.Normal(initmean, initcov)
+
+    if initrv is None:
+        initrv = randvars.Normal(np.ones(2), measurement_variance * np.eye(2))
 
     # Generate data
     times = np.arange(0.0, t_max, step=delta_t)
     states, obs = statespace.generate_samples(
-        dynmod=dynmod, measmod=measmod, initrv=initrv, times=times
+        dynmod=dynamics_model, measmod=measurement_model, initrv=initrv, times=times
     )
     regression_problem = problems.RegressionProblem(
         observations=obs, locations=times, solution=states
     )
-    return dynmod, measmod, initrv, regression_problem
+    statespace_components = dict(
+        dynamics_model=dynamics_model,
+        measurement_model=measurement_model,
+        initrv=initrv,
+    )
+    return regression_problem, statespace_components
 
 
-def logistic_ode(delta_t=0.2, t_max=2.0):
+def logistic_ode(
+    ivp_initrv: Optional[randvars.RandomVariable] = None,
+    solver_initrv: Optional[randvars.RandomVariable] = None,
+    timespan: Optional[Tuple[float, float]] = None,
+    params: Optional[Tuple[float, float]] = None,
+):
     r"""Filtering/smoothing setup for a probabilistic ODE solver based on the logistic ODE.
 
     This state space model puts an integrated Brownian motion prior on the dynamics
@@ -288,18 +331,13 @@ def logistic_ode(delta_t=0.2, t_max=2.0):
 
     Returns
     -------
-    dynmod
-        The `DiscreteGaussian` dynamics model.
-    measmod
-        The `DiscreteGaussian` measurement model.
-    initrv
-        A `Normal` random variable for the Gaussian initial conditions.
-    info
-        A dictionary containing
-
-            * ``dt`` the step size of the grid,
-            * ``tmax`` the limit of the time grid,
-            * ``ode`` the right-hand-side of the logistic ODE.
+    logistic_ivp
+        The initial value problem based on the logistic ODE.
+    statespace_components
+        Dictionary containing
+        - dynamics model
+        - measurement model
+        - initial random variable
 
     See Also
     --------
@@ -307,16 +345,29 @@ def logistic_ode(delta_t=0.2, t_max=2.0):
 
     """
 
-    logistic = diffeq.logistic(
-        (0, t_max), initrv=randvars.Constant(np.array([0.1])), params=(6, 1)
-    )
-    dynamod = statespace.IBM(ordint=3, spatialdim=1)
-    measmod = filtsmooth.DiscreteEKFComponent.from_ode(
-        logistic, dynamod, np.zeros((1, 1)), ek0_or_ek1=1
+    if timespan is None:
+        timespan = (0.0, 2.0)
+
+    if ivp_initrv is None:
+        ivp_initrv = randvars.Constant(np.array([0.1]))
+
+    if params is None:
+        params = (6.0, 1.0)
+
+    logistic_ivp = diffeq.logistic(timespan=timespan, initrv=ivp_initrv, params=params)
+    dynamics_model = statespace.IBM(ordint=3, spatialdim=1)
+    measurement_model = filtsmooth.DiscreteEKFComponent.from_ode(
+        logistic_ivp, prior=dynamics_model, evlvar=np.zeros((1, 1)), ek0_or_ek1=1
     )
 
-    initmean = np.array([0.1, 0, 0.0, 0.0])
-    initcov = np.diag([0.0, 1.0, 1.0, 1.0])
-    initrv = randvars.Normal(initmean, initcov)
+    if solver_initrv is None:
+        initmean = np.array([0.1, 0, 0.0, 0.0])
+        initcov = np.diag([0.0, 1.0, 1.0, 1.0])
+        solver_initrv = randvars.Normal(initmean, initcov)
 
-    return dynamod, measmod, initrv, {"dt": delta_t, "tmax": t_max, "ode": logistic}
+    statespace_components = dict(
+        dynamics_model=dynamics_model,
+        measurement_model=measurement_model,
+        initrv=solver_initrv,
+    )
+    return logistic_ivp, statespace_components
