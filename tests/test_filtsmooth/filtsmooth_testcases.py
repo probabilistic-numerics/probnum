dccb70a0f2c83b0414fe7d1c8ac99f7208ee2a9a
Nicholas Krämer <nicokraemer@gmx.net>
2021-05-07 07:28:48 +0200

    removed comment

--- tests/test_filtsmooth/filtsmooth_testcases.py
+++ tests/test_filtsmooth/filtsmooth_testcases.py
@@ -63,7 +63,6 @@ class LinearisedDiscreteTransitionTestCase(unittest.TestCase, NumpyAssertions):
     def test_filtsmooth_pendulum(self):
         # pylint: disable=not-callable
         # Set up test problem
-        # regression_problem, statespace_components = filtsmooth_zoo.pendulum(measurement_variance=0.001)
         regression_problem, statespace_components = filtsmooth_zoo.pendulum(
             measurement_variance=0.0001
         )
