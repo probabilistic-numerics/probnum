d2e3e72443144b748dabf71509cccb332e407854
Nicholas Krämer <nicokraemer@gmx.net>
2021-06-09 16:15:56 +0200

    tidies up initialise code

--- src/probnum/diffeq/odefiltsmooth/initialize.py
+++ src/probnum/diffeq/odefiltsmooth/initialize.py
@@ -87,18 +87,6 @@ def initialize_odefilter_with_rk(
     ode_dim = y0.shape[0] if y0.ndim > 0 else 1
     order = prior_process.transition.ordint
 
-    proj_to_y = prior_process.transition.proj2coord(coord=0)
-    zeros_shift = np.zeros(ode_dim)
-    zeros_cov = np.zeros((ode_dim, ode_dim))
-    measmod_scipy = statespace.DiscreteLTIGaussian(
-        proj_to_y,
-        zeros_shift,
-        zeros_cov,
-        proc_noise_cov_cholesky=zeros_cov,
-        forward_implementation="sqrt",
-        backward_implementation="sqrt",
-    )
-
     # order + 1 would suffice in theory, 2*order + 1 is for good measure
     # (the "+1" is a safety factor for order=1)
     num_steps = 2 * order + 1
@@ -113,43 +101,51 @@ def initialize_odefilter_with_rk(
         method=method,
     )
 
-    ts = sol.t[:num_steps]
-    ys = list(sol.y[:, :num_steps].T)
+    # Measurement model for SciPy observations
+    proj_to_y = prior_process.transition.proj2coord(coord=0)
+    zeros_shift = np.zeros(ode_dim)
+    zeros_cov = np.zeros((ode_dim, ode_dim))
+    measmod_scipy = statespace.DiscreteLTIGaussian(
+        proj_to_y,
+        zeros_shift,
+        zeros_cov,
+        proc_noise_cov_cholesky=zeros_cov,
+        forward_implementation="sqrt",
+        backward_implementation="sqrt",
+    )
 
+    # Measurement model for initial condition observations
     proj_to_dy = prior_process.transition.proj2coord(coord=1)
     if df is not None and order > 1:
         proj_to_ddy = prior_process.transition.proj2coord(coord=2)
-        zeros_shift = np.zeros(3 * ode_dim)
-        zeros_cov = np.zeros((3 * ode_dim, 3 * ode_dim))
-        measmod_initcond = statespace.DiscreteLTIGaussian(
-            np.vstack((proj_to_y, proj_to_dy, proj_to_ddy)),
-            zeros_shift,
-            zeros_cov,
-            proc_noise_cov_cholesky=zeros_cov,
-            forward_implementation="sqrt",
-            backward_implementation="sqrt",
-        )
-        ys[0] = np.hstack((y0, f(t0, y0), df(t0, y0) @ f(t0, y0)))
-
+        projmat_initial_conditions = np.vstack((proj_to_y, proj_to_dy, proj_to_ddy))
+        initial_data = np.hstack((y0, f(t0, y0), df(t0, y0) @ f(t0, y0)))
     else:
-        zeros_shift = np.zeros(2 * ode_dim)
-        zeros_cov = np.zeros((2 * ode_dim, 2 * ode_dim))
-        measmod_initcond = statespace.DiscreteLTIGaussian(
-            np.vstack((proj_to_y, proj_to_dy)),
-            zeros_shift,
-            zeros_cov,
-            proc_noise_cov_cholesky=zeros_cov,
-            forward_implementation="sqrt",
-            backward_implementation="sqrt",
-        )
-        ys[0] = np.hstack((y0, f(t0, y0)))
+        projmat_initial_conditions = np.vstack((proj_to_y, proj_to_dy))
+        initial_data = np.hstack((y0, f(t0, y0)))
+    zeros_shift = np.zeros(len(projmat_initial_conditions))
+    zeros_cov = np.zeros(
+        (len(projmat_initial_conditions), len(projmat_initial_conditions))
+    )
+    measmod_initcond = statespace.DiscreteLTIGaussian(
+        projmat_initial_conditions,
+        zeros_shift,
+        zeros_cov,
+        proc_noise_cov_cholesky=zeros_cov,
+        forward_implementation="sqrt",
+        backward_implementation="sqrt",
+    )
 
+    # Create regression problem and measurement model list
+    ts = sol.t[:num_steps]
+    ys = list(sol.y[:, :num_steps].T)
+    ys[0] = initial_data
     measmod_list = [measmod_initcond] + [measmod_scipy] * (len(ts) - 1)
     regression_problem = problems.RegressionProblem(observations=ys, locations=ts)
 
+    # Infer the solution
     kalman = filtsmooth.Kalman(prior_process)
     out, _ = kalman.filtsmooth(regression_problem, measmod_list)
-
     estimated_initrv = out.states[0]
     return estimated_initrv
 
