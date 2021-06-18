dc59faefcf7447723b8a780a4fbbe46b9d663c00
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-06-18 22:05:35 +0200

    code style switched to solarized dark

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,7 +105,7 @@ language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = "solarized-dark"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
