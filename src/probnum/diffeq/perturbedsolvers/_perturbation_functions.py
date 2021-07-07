a7b74b852f67af9172231979abde07f3f286fc97
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-07 10:31:41 +0200

    rng in perturbation functions

--- src/probnum/diffeq/perturbedsolvers/_perturbation_functions.py
+++ src/probnum/diffeq/perturbedsolvers/_perturbation_functions.py
@@ -8,28 +8,26 @@ import numpy as np
 import scipy
 
 
-def perturb_uniform(step, solver_order, noise_scale, random_state=None, size=()):
+def perturb_uniform(rng, step, solver_order, noise_scale, size=()):
     """Perturb the step with uniformly distributed noise scaled by noise-scale [1]_.
 
     Proposed by Abdulle and Garegnani(2020)
 
     Parameters
     ----------
+    rng
+        Random number generator
     step : float
         unperturbed step propesed by the steprule
     solver_order : int
         order of the solver
     noise_scale : float
         scales the perturbation
-    seed : RandomStateArgType
-        seed for pseudo-random number generator.
     """
     if step >= 1.0:
         raise ValueError("Stepsize too large (>= 1)")
     else:
-        uniform_rv_samples = scipy.stats.uniform.rvs(
-            random_state=random_state, size=size
-        )
+        uniform_rv_samples = scipy.stats.uniform.rvs(random_state=rng, size=size)
         shift = noise_scale * step ** (solver_order + 0.5)
         left_boundary = step - shift
         right_boundary = step + shift
@@ -37,26 +35,26 @@ def perturb_uniform(step, solver_order, noise_scale, random_state=None, size=())
     return samples
 
 
-def perturb_lognormal(step, solver_order, noise_scale, random_state=None, size=()):
+def perturb_lognormal(rng, step, solver_order, noise_scale, size=()):
     """Perturb the step with lognormally distributed noise scaled by noise-scale [1]_.
 
     Parameters
     ----------
+    rng
+        Random number generator
     step : float
         unperturbed step propesed by the steprule
     solver_order : int
         order of the solver
     noise_scale : float
         scales the perturbation
-    seed : RandomStateArgType
-        seed for pseudo-random number generator.
     """
     shift = 0.5 * np.log(1 + noise_scale * (step ** (2 * solver_order)))
     mean = np.log(step) - shift
     cov = 2 * shift
     samples = np.exp(
         scipy.stats.multivariate_normal.rvs(
-            mean=mean, cov=cov, size=size, random_state=random_state
+            mean=mean, cov=cov, size=size, random_state=rng
         )
     )
     return samples
