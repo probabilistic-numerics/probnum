40f920840cbf25b518af6830ffee9dc3a183c1f7
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-07-07 16:29:07 +0200

    conjugacy test

--- src/probnum/linalg/solvers/policies/_conjugate_gradient.py
+++ src/probnum/linalg/solvers/policies/_conjugate_gradient.py
@@ -13,44 +13,21 @@ class ConjugateGradientPolicy(_linear_solver_policy.LinearSolverPolicy):
     r"""Policy returning :math:`A`-conjugate actions.
 
     Selects the negative gradient / residual as an initial action :math:`s_0 = A x_0 - b` and then successively generates :math:`A`-conjugate actions, i.e. the actions satisfy :math:`s_i^\top A s_j = 0` iff :math:`i \neq j`.
-
-    Parameters
-    ----------
-    reorthogonalization_fn :
-        Callable which reorthogonalizes a vector with respect to a set of vectors with respect to an inner product defined by a linear operator.
     """
-
-    def __init__(
-        self,
-        reorthogonalization_fn: Optional[
-            Callable[
-                [np.ndarray, Iterable[np.ndarray], linops.LinearOperator], np.ndarray
-            ]
-        ] = None,
-    ) -> None:
-        self._reorthogonalization_fn = reorthogonalization_fn
 
     def __call__(
         self, solver_state: "probnum.linalg.solvers.LinearSolverState"
     ) -> np.ndarray:
 
-        action = solver_state.residual.copy()
+        action = -solver_state.residual.copy()
 
         if solver_state.step > 0:
-            # A-conjugate action (in exact arithmetic)
+            # A-conjugacy correction (in exact arithmetic)
             beta = (
-                solver_state.residual_norm_squared
-                / solver_state.prev_residual_norm_squared
-            )
+                np.linalg.norm(solver_state.residual)
+                / np.linalg.norm(solver_state.residuals[solver_state.step - 1])
+            ) ** 2
 
-            action += beta * solver_state.actions[-1]
-
-            # (Optional) Reorthogonalization
-            if self._reorthogonalization_fn is not None:
-                action = self._reorthogonalization_fn(
-                    action,
-                    solver_state.actions,
-                    solver_state.problem.A,
-                )
+            action += beta * solver_state.actions[solver_state.step - 1]
 
         return action
