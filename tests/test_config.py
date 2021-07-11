cdda8e9e25318f5322ca0420cb9f212e6cae27da
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-07-11 12:35:44 +0200

    test

---
+++ tests/test_config.py
@@ -0,0 +1,18 @@
+import pytest
+
+import probnum
+
+
+def test_register():
+    probnum.config.register("dummy", 3.14, None)
+    assert probnum.config.dummy == 3.14
+
+    with pytest.raises(KeyError):
+        probnum.config.register("dummy", 4.2, None)
+
+    with probnum.config(dummy=9.9):
+        assert probnum.config.dummy == 9.9
+
+    with pytest.raises(KeyError):
+        with probnum.config(unknown_config=False):
+            pass
