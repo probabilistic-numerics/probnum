559e0c00a7833f9f3fdc4652e88addf7849aaf25
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-12 10:51:00 +0200

    migrated remaining ODE problems

--- src/probnum/problems/zoo/diffeq/_ivp_examples.py
+++ src/probnum/problems/zoo/diffeq/_ivp_examples.py
@@ -281,7 +281,7 @@ def fitzhughnagumo(t0=0.0, tmax=20.0, y0=None, params=(0.2, 0.2, 3.0)):
     Returns
     -------
     InitialValueProblem
-        InitialValueProblem object describing the rigid body dynamics IVP with the prescribed
+        InitialValueProblem object describing the FitzHugh-Nagumo model with the prescribed
         configuration.
     """
     if y0 is None:
@@ -300,7 +300,7 @@ def fitzhughnagumo(t0=0.0, tmax=20.0, y0=None, params=(0.2, 0.2, 3.0)):
     return InitialValueProblem(f=rhs, t0=t0, tmax=tmax, y0=y0, df=jac)
 
 
-def lotkavolterra(timespan, initrv, params=(0.5, 0.05, 0.5, 0.05)):
+def lotkavolterra(t0=0.0, tmax=20.0, y0=None, params=(0.5, 0.05, 0.5, 0.05)):
     r"""Initial value problem (IVP) based on the Lotka-Volterra model.
 
     The Lotka-Volterra (LV) model is defined through
@@ -319,50 +319,38 @@ def lotkavolterra(timespan, initrv, params=(0.5, 0.05, 0.5, 0.05)):
 
     Parameters
     ----------
-    timespan : (float, float)
-        Time span of IVP.
-    initrv : RandomVariable,
-        *(shape=(2, ))* -- Vector-valued RandomVariable that describes the belief
-        over the initial value. Usually it is a Constant (noise-free) or Normal (noisy)
-        Random Variable with :math:`2`-dimensional mean vector and
-        :math:`2 \times 2`-dimensional covariance matrix.
-        To replicate "classical" initial values use the Constant distribution.
-    params : (float, float, float, float), optional
-        Parameters :math:`(a, b, c, d)` for the Lotka-Volterra IVP.
-        Default is :math:`(a, b, c, d)=(0.5, 0.05, 0.5, 0.05)`.
+    t0
+        Initial time. Default is 0.0
+    tmax
+        Final time. Default is 20.0
+    y0
+        *(shape=(2, ))* -- Initial value. Default is ``[1., -1.]``.
+    params
+        Parameter of the Lotka-Volterra model. Default is ``(0.2, 0.2, 3.0)``.
 
     Returns
     -------
-    IVP
-        IVP object describing the Lotka-Volterra
-        IVP with the prescribed
+    InitialValueProblem
+        InitialValueProblem object describing the Lotka-Volterra system with the prescribed
         configuration.
     """
+    if y0 is None:
+        y0 = np.array([20.0, 20.0])
 
-    def rhs(t, y):
-        return lv_rhs(t, y, params)
+    def rhs(t, y, params=params):
+        a, b, c, d = params
+        y1, y2 = y
+        return np.array([a * y1 - b * y1 * y2, -c * y2 + d * y1 * y2])
 
-    def jac(t, y):
-        return lv_jac(t, y, params)
+    def jac(t, y, params=params):
+        a, b, c, d = params
+        y1, y2 = y
+        return np.array([[a - b * y2, -b * y1], [d * y2, -c + d * y1]])
 
-    return IVP(timespan, initrv, rhs, jac)
-
-
-def lv_rhs(t, y, params):
-    """RHS for Lotka-Volterra."""
-    a, b, c, d = params
-    y1, y2 = y
-    return np.array([a * y1 - b * y1 * y2, -c * y2 + d * y1 * y2])
-
-
-def lv_jac(t, y, params):
-    """Jacobian for Lotka-Volterra."""
-    a, b, c, d = params
-    y1, y2 = y
-    return np.array([[a - b * y2, -b * y1], [d * y2, -c + d * y1]])
+    return InitialValueProblem(f=rhs, t0=t0, tmax=tmax, y0=y0, df=jac)
 
 
-def seir(timespan, initrv, params=(0.3, 0.3, 0.1)):
+def seir(t0=0.0, tmax=200.0, y0=None, params=(0.3, 0.3, 0.1)):
     r"""Initial value problem (IVP) based on the SEIR model.
 
     The SEIR model with no vital dynamics is defined through
@@ -393,66 +381,51 @@ def seir(timespan, initrv, params=(0.3, 0.3, 0.1)):
 
     Parameters
     ----------
-    timespan : (float, float)
-        Time span of IVP.
-    initrv : RandomVariable,
-        *(shape=(4, ))* -- Vector-valued RandomVariable that describes the belief
-        over the initial value. Usually it is a Constant (noise-free) or Normal (noisy)
-        Random Variable with :math:`4`-dimensional mean vector and
-        :math:`4 \times 4`-dimensional covariance matrix.
-        To replicate "classical" initial values use the Constant distribution.
-    params : (float, float, float), optional
-        Parameters :math:`(\alpha, \beta, \gamma)` for the SEIR model IVP.
-        Default is :math:`(\alpha, \beta, \gamma)=(0.3, 0.3, 0.1)`.
+    t0
+        Initial time. Default is 0.0
+    tmax
+        Final time. Default is 200.0
+    y0
+        *(shape=(4, ))* -- Initial value. Default is ``[998, 1, 1, 0]``.
+    params
+        Parameter of the SEIR model. Default is ``(0.3, 0.3, 0.1)``.
 
     Returns
     -------
-    IVP
-        IVP object describing the SEIR model IVP with the prescribed
+    InitialValueProblem
+        InitialValueProblem object describing the SEIR model with the prescribed
         configuration.
     """
+    if y0 is None:
+        y0 = np.array([998, 1, 1, 0])
 
-    population_count = np.sum(initrv.mean)
-    params_and_population_count = (*params, population_count)
+    def rhs(t, y, params=params):
+        alpha, beta, gamma, population_count = params
+        y1, y2, y3, y4 = y
+        y1_next = -beta * y1 * y3 / population_count
+        y2_next = beta * y1 * y3 / population_count - alpha * y2
+        y3_next = alpha * y2 - gamma * y3
+        y4_next = gamma * y3
+        return np.array([y1_next, y2_next, y3_next, y4_next])
 
-    def rhs(t, y):
-        return seir_rhs(t, y, params_and_population_count)
+    def jac(t, y, params=params):
+        alpha, beta, gamma, population_count = params
+        y1, y2, y3, y4 = y
+        d_dy1 = np.array(
+            [-beta * y3 / population_count, 0.0, -beta * y1 / population_count, 0.0]
+        )
+        d_dy2 = np.array(
+            [beta * y3 / population_count, -alpha, beta * y1 / population_count, 0.0]
+        )
+        d_dy3 = np.array([0.0, alpha, -gamma, 0.0])
+        d_dy4 = np.array([0.0, 0.0, gamma, 0.0])
+        jac_matrix = np.array([d_dy1, d_dy2, d_dy3, d_dy4])
+        return jac_matrix
 
-    def jac(t, y):
-        return seir_jac(t, y, params_and_population_count)
-
-    return IVP(timespan, initrv, rhs, jac=jac)
-
-
-def seir_rhs(t, y, params):
-    """RHS for SEIR model."""
-    alpha, beta, gamma, population_count = params
-    y1, y2, y3, y4 = y
-    y1_next = -beta * y1 * y3 / population_count
-    y2_next = beta * y1 * y3 / population_count - alpha * y2
-    y3_next = alpha * y2 - gamma * y3
-    y4_next = gamma * y3
-
-    return np.array([y1_next, y2_next, y3_next, y4_next])
-
-
-def seir_jac(t, y, params):
-    """Jacobian for SEIR model."""
-    alpha, beta, gamma, population_count = params
-    y1, y2, y3, y4 = y
-    d_dy1 = np.array(
-        [-beta * y3 / population_count, 0.0, -beta * y1 / population_count, 0.0]
-    )
-    d_dy2 = np.array(
-        [beta * y3 / population_count, -alpha, beta * y1 / population_count, 0.0]
-    )
-    d_dy3 = np.array([0.0, alpha, -gamma, 0.0])
-    d_dy4 = np.array([0.0, 0.0, gamma, 0.0])
-    jac_matrix = np.array([d_dy1, d_dy2, d_dy3, d_dy4])
-    return jac_matrix
+    return InitialValueProblem(f=rhs, t0=t0, tmax=tmax, y0=y0, df=jac)
 
 
-def lorenz(timespan, initrv, params=(10.0, 28.0, 8.0 / 3.0)):
+def lorenz(t0=0.0, tmax=20.0, y0=None, params=(10.0, 28.0, 8.0 / 3.0)):
     r"""Initial value problem (IVP) based on the Lorenz system.
 
     The Lorenz system is defined through
@@ -472,43 +445,32 @@ def lorenz(timespan, initrv, params=(10.0, 28.0, 8.0 / 3.0)):
 
     Parameters
     ----------
-    timespan : (float, float)
-        Time span of IVP.
-    initrv : RandomVariable,
-        *(shape=(3, ))* -- Vector-valued RandomVariable that describes the belief
-        over the initial value. Usually it is a Constant (noise-free) or Normal (noisy)
-        Random Variable with :math:`3`-dimensional mean vector and
-        :math:`3 \times 3`-dimensional covariance matrix.
-        To replicate "classical" initial values use the Constant distribution.
-    params : (float, float, float, float), optional
-        Parameters :math:`(a, b, c)` for the Lorenz system.
-        Default is :math:`(a, b, c)=(10, 28, 2.667)`.
+    t0
+        Initial time. Default is 0.0
+    tmax
+        Final time. Default is 20.0
+    y0
+        *(shape=(3, ))* -- Initial value. Default is ``[0., 1., 1.05]``.
+    params
+        Parameter of the Lotka-Volterra model. Default is ``(0.2, 0.2, 3.0)``.
 
     Returns
     -------
-    IVP
-        IVP object describing the Lorenz system IVP with the prescribed
+    InitialValueProblem
+        InitialValueProblem object describing the Lorenz system with the prescribed
         configuration.
     """
+    if y0 is None:
+        y0 = np.array([0.0, 1.0, 1.05])
 
-    def rhs(t, y):
-        return lor_rhs(t, y, params)
+    def rhs(t, y, params=params):
+        a, b, c = params
+        y1, y2, y3 = y
+        return np.array([a * (y2 - y1), y1 * (b - y3) - y2, y1 * y2 - c * y3])
 
-    def jac(t, y):
-        return lor_jac(t, y, params)
+    def jac(t, y, params=params):
+        a, b, c = params
+        y1, y2, y3 = y
+        return np.array([[-a, a, 0], [b - y3, -1, -y1], [y2, y1, -c]])
 
-    return IVP(timespan, initrv, rhs, jac)
-
-
-def lor_rhs(t, y, params):
-    """RHS for Lorenz system."""
-    a, b, c = params
-    y1, y2, y3 = y
-    return np.array([a * (y2 - y1), y1 * (b - y3) - y2, y1 * y2 - c * y3])
-
-
-def lor_jac(t, y, params):
-    """Jacobian for Lorenz system."""
-    a, b, c = params
-    y1, y2, y3 = y
-    return np.array([[-a, a, 0], [b - y3, -1, -y1], [y2, y1, -c]])
+    return InitialValueProblem(f=rhs, t0=t0, tmax=tmax, y0=y0, df=jac)
