fa5c26874b3ee53f4160c3b785b99cdbb5adca16
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-14 09:39:02 +0200

    docs for kalman_filter

--- src/probnum/filtsmooth/_kalman_filter.py
+++ src/probnum/filtsmooth/_kalman_filter.py
@@ -1,11 +1,76 @@
 """Convenience functions for filtering and smoothing."""
 
+import numpy as np
+
 from probnum import problems, randprocs, statespace
 
 __all__ = ["kalman_filter", "rauch_tung_striebel_smoother"]
 
 
-def kalman_filter(observations, locations, F, L, H, R, m0, C0, prior_model="discrete"):
+def kalman_filter(
+    observations, locations, F, L, H, R, m0, C0, prior_model="continuous"
+):
+    r"""Estimate an unknown, hidden trajectory from a set of observations with a Kalman filter.
+
+    A Kalman filter estimates the unknown trajectory :math:`X` from a set of observations `Y`.
+    There is a continuous-discrete and a discrete-discrete version (describing whether the prior model and measurement model are continuous/discrete).
+
+    In a continuous-discrete model, the prior distribution is described by the SDE
+
+    .. math:: \text{d}X(t) = F X(t) \text{d}t + L \text{d}W(t)
+
+    driven by Wiener process :math:`W` and subject to initial condition
+
+    .. math:: X(t_0) \sim N(m_0, C_0).
+
+    By default, :math:`t_0` is set to the location of the first observation.
+
+    In a discrete-discrete model, the prior distribution is described by the transition
+
+    .. math:: X_{n+1} \,|\, X_n \sim N(F X_n, L)
+
+    subject to the same initial condition.
+
+    In both cases, the measurement model is (write :math:`X(t_n)=X_n` in the continuous case)
+
+    .. math:: Y_n \,|\, X_n \sim N(H X_n, R)
+
+    and the Kalman filter estimates :math:`X` given `Y_n=y_n`, `Y=[y_1, ..., y_N]`.
+
+    Parameters
+    ----------
+    observations
+        A list of noisy observations of the hidden trajectory.
+    locations
+        Time-locations of the observations.
+    F
+        State transition matrix. Either the drift matrix in an SDE model,
+        or the transition matrix in a discrete model (depending on the value of `prior_model`).
+    L
+        Diffusion/dispersion matrix. Either the dispersion matrix in an SDE model,
+        or the diffusion matrix in a discrete model (depending on the value of `prior_model`).
+    H
+        Transition matrix of the (discrete) observation model.
+    R
+        Covariance matrix of the observation noise.
+    m0
+        Initial mean of the prior model.
+    C0
+        Initial covariance of the prior model.
+    prior_model
+        Either discrete (``discrete'') or continuous (``continuous''). This affects the role of `F' and `L'.
+        Optional. Default is `continuous'.
+
+    Raises
+    ------
+    ValueError
+        If `prior_model' is neither ``discrete'' nor ``continuous''.
+
+    Returns
+    -------
+    gaussian.FilteringPosterior
+        Filtering distribution as returned by the Kalman filter.
+    """
     regression_problem = _setup_regression_problem(
         H=H, R=R, observations=observations, locations=locations
     )
@@ -17,8 +82,68 @@ def kalman_filter(observations, locations, F, L, H, R, m0, C0, prior_model="disc
 
 
 def rauch_tung_striebel_smoother(
-    observations, locations, F, L, H, R, prior_model="discrete"
+    observations, locations, F, L, H, R, m0, C0, prior_model="continuous"
 ):
+    r"""Estimate an unknown, hidden trajectory from a set of observations with a Rauch-Tung-Striebel smoother.
+
+    A Rauch-Tung-Striebel smoother estimates the unknown trajectory :math:`X` from a set of observations `Y`.
+    There is a continuous-discrete and a discrete-discrete version (describing whether the prior model and measurement model are continuous/discrete).
+
+    In a continuous-discrete model, the prior distribution is described by the SDE
+
+    .. math:: \text{d}X(t) = F X(t) \text{d}t + L \text{d}W(t)
+
+    driven by Wiener process :math:`W` and subject to initial condition
+
+    .. math:: X(t_0) \sim N(m_0, C_0).
+
+    By default, :math:`t_0` is set to the location of the first observation.
+
+    In a discrete-discrete model, the prior distribution is described by the transition
+
+    .. math:: X_{n+1} \,|\, X_n \sim N(F X_n, L)
+
+    subject to the same initial condition.
+
+    In both cases, the measurement model is (write :math:`X(t_n)=X_n` in the continuous case)
+
+    .. math:: Y_n \,|\, X_n \sim N(H X_n, R)
+
+    and the Rauch-Tung-Striebel smoother estimates :math:`X` given `Y_n=y_n`, `Y=[y_1, ..., y_N]`.
+
+    Parameters
+    ----------
+    observations
+        A list of noisy observations of the hidden trajectory.
+    locations
+        Time-locations of the observations.
+    F
+        State transition matrix. Either the drift matrix in an SDE model,
+        or the transition matrix in a discrete model (depending on the value of `prior_model`).
+    L
+        Diffusion/dispersion matrix. Either the dispersion matrix in an SDE model,
+        or the diffusion matrix in a discrete model (depending on the value of `prior_model`).
+    H
+        Transition matrix of the (discrete) observation model.
+    R
+        Covariance matrix of the observation noise.
+    m0
+        Initial mean of the prior model.
+    C0
+        Initial covariance of the prior model.
+    prior_model
+        Either discrete (``discrete'') or continuous (``continuous''). This affects the role of `F' and `L'.
+
+    Raises
+    ------
+    ValueError
+        If `prior_model' is neither ``discrete'' nor ``continuous''.
+
+    Returns
+    -------
+    gaussian.SmoothingPosterior
+        Smoothing distribution as returned by the Rauch-Tung-Striebel smoother.
+    """
     regression_problem = _setup_regression_problem(
         H=H, R=R, observations=observations, locations=locations
     )
@@ -29,7 +154,7 @@ def rauch_tung_striebel_smoother(
     return kalman.filtsmooth(regression_problem)
 
 
-def _setup_prior_process(F, L, m0, c0, prior_model):
+def _setup_prior_process(F, L, m0, C0, prior_model):
     zero_shift_prior = np.zeros(F.shape[1])
     if prior_model == "discrete":
         prior = statespace.DiscreteLTIGaussian(
