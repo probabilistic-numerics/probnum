9c0df77d8bd6da53324f116f59680cfca14322c6
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-17 14:07:40 +0200

    TM is class now

--- src/probnum/diffeq/odefiltsmooth/initialize/_initialize_with_taylormode.py
+++ src/probnum/diffeq/odefiltsmooth/initialize/_initialize_with_taylormode.py
@@ -4,12 +4,91 @@
 
 import numpy as np
 
-from probnum import randvars, statespace
+from probnum import problems, randprocs, randvars, statespace
 
 # In the initialisation-via-RK function below, this value is added to the marginal stds of the initial derivatives that are known.
 # If we put in zero, there are linalg errors (because a zero-cov RV is conditioned on a dirac likelihood).
 # This value is chosen such that its square-root is a really small damping factor).
 SMALL_VALUE = 1e-28
+
+
+from probnum.diffeq.odefiltsmooth.initialize import _initialize
+
+
+class TaylorModeInitialization(_initialize.InitializationRoutine):
+    """Initialize an ODE solver with Taylor-mode automatic differentiation."""
+
+    def __call__(
+        self, ivp: problems.InitialValueProblem, prior_process: randprocs.MarkovProcess
+    ) -> randvars.RandomVariable:
+
+        f, y0, t0, tmax = ivp.f, ivp.y0, ivp.t0, ivp.tmax
+
+        try:
+            import jax.numpy as jnp
+            from jax.config import config
+            from jax.experimental.jet import jet
+
+            config.update("jax_enable_x64", True)
+        except ImportError as err:
+            raise ImportError(
+                "Cannot perform Taylor-mode initialisation without optional "
+                "dependencies jax and jaxlib. Try installing them via `pip install jax jaxlib`."
+            ) from err
+
+        order = prior_process.transition.ordint
+
+        def total_derivative(z_t):
+            """Total derivative."""
+            z, t = jnp.reshape(z_t[:-1], z_shape), z_t[-1]
+            dz = jnp.ravel(f(t, z))
+            dt = jnp.array([1.0])
+            dz_t = jnp.concatenate((dz, dt))
+            return dz_t
+
+        z_shape = y0.shape
+        z_t = jnp.concatenate((jnp.ravel(y0), jnp.array([t0])))
+
+        derivs = []
+
+        derivs.extend(y0)
+        if order == 0:
+            all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
+                np.asarray(jnp.array(derivs)), ordint=0, spatialdim=len(y0)
+            )
+
+            return randvars.Normal(
+                np.asarray(all_derivs),
+                cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+                cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+            )
+
+        (dy0, [*yns]) = jet(total_derivative, (z_t,), ((jnp.ones_like(z_t),),))
+        derivs.extend(dy0[:-1])
+        if order == 1:
+            all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
+                np.asarray(jnp.array(derivs)), ordint=1, spatialdim=len(y0)
+            )
+
+            return randvars.Normal(
+                np.asarray(all_derivs),
+                cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+                cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+            )
+
+        for _ in range(1, order):
+            (dy0, [*yns]) = jet(total_derivative, (z_t,), ((dy0, *yns),))
+            derivs.extend(yns[-2][:-1])
+
+        all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
+            jnp.array(derivs), ordint=order, spatialdim=len(y0)
+        )
+
+        return randvars.Normal(
+            np.asarray(all_derivs),
+            cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+            cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
+        )
 
 
 def initialize_odefilter_with_taylormode(f, y0, t0, prior_process):
@@ -90,69 +169,6 @@ def initialize_odefilter_with_taylormode(f, y0, t0, prior_process):
     >>> print(improved_initrv.std)
     [0. 0. 0. 0. 0. 0. 0. 0.]
     """
-
-    try:
-        import jax.numpy as jnp
-        from jax.config import config
-        from jax.experimental.jet import jet
-
-        config.update("jax_enable_x64", True)
-    except ImportError as err:
-        raise ImportError(
-            "Cannot perform Taylor-mode initialisation without optional "
-            "dependencies jax and jaxlib. Try installing them via `pip install jax jaxlib`."
-        ) from err
-
-    order = prior_process.transition.ordint
-
-    def total_derivative(z_t):
-        """Total derivative."""
-        z, t = jnp.reshape(z_t[:-1], z_shape), z_t[-1]
-        dz = jnp.ravel(f(t, z))
-        dt = jnp.array([1.0])
-        dz_t = jnp.concatenate((dz, dt))
-        return dz_t
-
-    z_shape = y0.shape
-    z_t = jnp.concatenate((jnp.ravel(y0), jnp.array([t0])))
-
-    derivs = []
-
-    derivs.extend(y0)
-    if order == 0:
-        all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
-            np.asarray(jnp.array(derivs)), ordint=0, spatialdim=len(y0)
-        )
-
-        return randvars.Normal(
-            np.asarray(all_derivs),
-            cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-            cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-        )
-
-    (dy0, [*yns]) = jet(total_derivative, (z_t,), ((jnp.ones_like(z_t),),))
-    derivs.extend(dy0[:-1])
-    if order == 1:
-        all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
-            np.asarray(jnp.array(derivs)), ordint=1, spatialdim=len(y0)
-        )
-
-        return randvars.Normal(
-            np.asarray(all_derivs),
-            cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-            cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-        )
-
-    for _ in range(1, order):
-        (dy0, [*yns]) = jet(total_derivative, (z_t,), ((dy0, *yns),))
-        derivs.extend(yns[-2][:-1])
-
-    all_derivs = statespace.Integrator._convert_derivwise_to_coordwise(
-        jnp.array(derivs), ordint=order, spatialdim=len(y0)
-    )
-
-    return randvars.Normal(
-        np.asarray(all_derivs),
-        cov=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-        cov_cholesky=np.asarray(jnp.diag(jnp.zeros(len(derivs)))),
-    )
+    taylor_init = TaylorModeInitialization()
+    ivp = problems.InitialValueProblem(f=f, y0=y0, t0=t0, tmax=np.inf)
+    return taylor_init(ivp, prior_process=prior_process)
