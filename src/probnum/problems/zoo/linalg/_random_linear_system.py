d536f6a8c9b3b73fbbbedd8e53a23c761dfdb11c
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-07-18 08:07:38 +0200

    random linear system

--- src/probnum/problems/zoo/linalg/_random_linear_system.py
+++ src/probnum/problems/zoo/linalg/_random_linear_system.py
@@ -1,7 +1,9 @@
 """Generate random linear systems as test problems."""
 
+import numpy as np
 
-def random_linear_system():
+
+def random_linear_system(assume_A: str, rng: np.random.Generator):
     """Random linear system.
 
     Generate a random linear system by sampling a system matrix with the given properties. The solution is chosen to be a realization from the provided random variable. If None the solution is drawn from a standard normal distribution.
