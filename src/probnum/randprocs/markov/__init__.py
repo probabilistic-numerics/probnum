f0b4f16e505aa810e5716e01071fe034d9ecd1a7
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-18 06:56:24 +0200

    moved statespace to randprocs.markov

---
+++ src/probnum/randprocs/markov/__init__.py
@@ -0,0 +1,19 @@
+"""Probabilistic State Space Models.
+
+This package implements continuous-discrete and discrete-discrete state
+space models, which are the basis for Bayesian filtering and smoothing,
+but also probabilistic ODE solvers.
+"""
+
+from ._markov_process import MarkovProcess
+from ._transition import Transition
+
+# Public classes and functions. Order is reflected in documentation.
+__all__ = [
+    "MarkovProcess",
+    "Transition",
+]
+
+# Set correct module paths. Corrects links and module paths in documentation.
+MarkovProcess.__module__ = "probnum.randprocs.markov"
+Transition.__module__ = "probnum.randprocs.markov"
