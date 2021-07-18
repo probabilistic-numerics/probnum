a29b9f44123796829ca206d7f7fce18b147f20a1
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-07-18 08:02:45 +0200

    random linear system

---
+++ src/probnum/problems/zoo/linalg/_random_linear_system.py
@@ -0,0 +1,13 @@
+"""Generate random linear systems as test problems."""
+
+
+def random_linear_system():
+    """Random linear system.
+
+    Generate a random linear system by sampling a system matrix with the given properties. The solution is chosen to be a realization from the provided random variable. If None the solution is drawn from a standard normal distribution.
+
+
+    Parameters
+    ----------
+    """
+    pass
--- src/probnum/problems/zoo/linalg/_random_spd_matrix.py
+++ src/probnum/problems/zoo/linalg/_random_spd_matrix.py
@@ -13,13 +13,13 @@ def random_spd_matrix(
     dim: IntArgType,
     spectrum: Sequence = None,
 ) -> np.ndarray:
-    """Random symmetric positive definite matrix.
+    r"""Random symmetric positive definite matrix.
 
     Constructs a random symmetric positive definite matrix from a given spectrum. An
-    orthogonal matrix :math:`Q` with :math:`\\operatorname{det}(Q)` (a rotation) is
+    orthogonal matrix :math:`Q` with :math:`\operatorname{det}(Q)` (a rotation) is
     sampled with respect to the Haar measure and the diagonal matrix
     containing the eigenvalues is rotated accordingly resulting in :math:`A=Q
-    \\operatorname{diag}(\\lambda_1, \\dots, \\lambda_n)Q^\\top`. If no spectrum is
+    \operatorname{diag}(\lambda_1, \dots, \lambda_n)Q^\top`. If no spectrum is
     provided, one is randomly drawn from a Gamma distribution.
 
     Parameters
