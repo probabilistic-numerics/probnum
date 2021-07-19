8033f5a657fea1a3e78035e55fb239db80a0d2b4
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-07-19 17:42:29 +0200

    Fix benchmark

--- benchmarks/randprocs.py
+++ benchmarks/randprocs.py
@@ -2,7 +2,7 @@
 import numpy as np
 import scipy.stats
 
-from probnum import config, randprocs, randvars, statespace
+from probnum import config, linops, randprocs, randvars, statespace
 
 
 class MarkovProcessSampling:
@@ -20,29 +20,30 @@ class MarkovProcessSampling:
                 backward_implementation="classic",
             )
 
-        measvar = 0.1024
-        initrv = randvars.Normal(
-            np.ones(dynamics.dimension), measvar * np.eye(dynamics.dimension)
-        )
+            measvar = 0.1024
+            initrv = randvars.Normal(
+                np.ones(dynamics.dimension),
+                measvar * linops.Identity(dynamics.dimension),
+            )
 
-        time_domain = (0.0, float(len_trajectory))
-        self.time_grid = np.arange(*time_domain)
-        self.markov_process = randprocs.MarkovProcess(
-            initarg=time_domain[0], initrv=initrv, transition=dynamics
-        )
+            time_domain = (0.0, float(len_trajectory))
+            self.time_grid = np.arange(*time_domain)
+            self.markov_process = randprocs.MarkovProcess(
+                initarg=time_domain[0], initrv=initrv, transition=dynamics
+            )
 
-        rng = np.random.default_rng(seed=1)
-        self.base_measure_realizations = scipy.stats.norm.rvs(
-            size=((num_samples,) + self.time_grid.shape + initrv.shape),
-            random_state=rng,
-        )
+            rng = np.random.default_rng(seed=1)
+            self.base_measure_realizations = scipy.stats.norm.rvs(
+                size=((num_samples,) + self.time_grid.shape + initrv.shape),
+                random_state=rng,
+            )
 
     def time_sample(self, use_linops, num_samples, len_trajectory, order, dimension):
-
-        for base_measure_real in self.base_measure_realizations:
-            self.markov_process.transition.jointly_transform_base_measure_realization_list_forward(
-                base_measure_realizations=base_measure_real,
-                t=self.time_grid,
-                initrv=self.markov_process.initrv,
-                _diffusion_list=np.ones_like(self.time_grid[:-1]),
-            )
+        with config(statespace_use_linops=use_linops):
+            for base_measure_real in self.base_measure_realizations:
+                self.markov_process.transition.jointly_transform_base_measure_realization_list_forward(
+                    base_measure_realizations=base_measure_real,
+                    t=self.time_grid,
+                    initrv=self.markov_process.initrv,
+                    _diffusion_list=np.ones_like(self.time_grid[:-1]),
+                )
