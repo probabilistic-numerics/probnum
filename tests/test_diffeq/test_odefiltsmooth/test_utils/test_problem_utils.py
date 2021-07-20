5ec08deb366526f50bec5494c2252dc8a5aa75b5
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-20 09:41:24 +0200

    explanations in ivp tests

--- tests/test_diffeq/test_odefiltsmooth/test_utils/test_problem_utils.py
+++ tests/test_diffeq/test_odefiltsmooth/test_utils/test_problem_utils.py
@@ -42,7 +42,8 @@ def test_ivp_to_regression_problem(
     ode_measurement_variance,
     exclude_initial_condition,
 ):
-
+    """Test all possible parametrizations of ivp_to_regression_problem."""
+    # Call function
     ode_information_operator.incorporate_ode(ode=ivp)
     regprob = diffeq.odefiltsmooth.utils.ivp_to_regression_problem(
         ivp=ivp,
@@ -52,6 +53,8 @@ def test_ivp_to_regression_problem(
         ode_measurement_variance=ode_measurement_variance,
         exclude_initial_condition=exclude_initial_condition,
     )
+
+    # Test basic properties
     assert isinstance(regprob, problems.TimeSeriesRegressionProblem)
     assert len(regprob.locations) == len(locations)
     assert len(regprob.locations) == len(locations)
@@ -59,6 +62,9 @@ def test_ivp_to_regression_problem(
     assert isinstance(regprob.measurement_models[1], statespace.DiscreteGaussian)
     assert isinstance(regprob.measurement_models[-1], statespace.DiscreteGaussian)
 
+    # Depending on the desired exclusion of the initial condition,
+    # the first element in the list of measurement models should
+    # be LTIGaussian (for the initial condition) or DiscreteGaussian (for the ODE)
     if exclude_initial_condition:
         assert isinstance(regprob.measurement_models[0], statespace.DiscreteGaussian)
         assert isinstance(regprob.measurement_models[0], statespace.DiscreteGaussian)
@@ -66,6 +72,8 @@ def test_ivp_to_regression_problem(
         assert isinstance(regprob.measurement_models[0], statespace.DiscreteLTIGaussian)
         assert isinstance(regprob.measurement_models[0], statespace.DiscreteLTIGaussian)
 
+    # If the ODE measurement variance is not None, i.e. not zero,
+    # the process noise covariance matrices should be non-zero.
     if ode_measurement_variance is not None:
         cov = regprob.measurement_models[1].proc_noise_cov_mat_fun(locations[0])
         cov_cholesky = regprob.measurement_models[1].proc_noise_cov_cholesky_fun(
@@ -74,6 +82,10 @@ def test_ivp_to_regression_problem(
         assert np.linalg.norm(cov > 0.0)
         assert np.linalg.norm(cov_cholesky > 0.0)
 
+    # If an approximation strategy is passed, the output should be an EKF component
+    # which should suppoert forward_rv().
+    # If not, the output is a generic DiscreteGaussian (which has been tested for above.
+    # Recall that DiscreteEKFComponent implements DiscreteGaussian.)
     if approx_strategy is not None:
         assert isinstance(
             regprob.measurement_models[1],
