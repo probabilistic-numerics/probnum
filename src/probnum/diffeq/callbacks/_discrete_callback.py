fbb1bbb3bc70207408706e514d631ce81466716f
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-25 11:26:07 +0200

    fixed a doc

--- src/probnum/diffeq/callbacks/_discrete_callback.py
+++ src/probnum/diffeq/callbacks/_discrete_callback.py
@@ -9,10 +9,9 @@ from probnum.diffeq.callbacks import _callback
 class DiscreteCallback(_callback.ODESolverCallback):
     """Handle discrete events in an ODE solver.
 
-    A discrete event can be a time-stamp that must be included in the locations. It can
-    also be any event for which it is possible to write down a condition that evaluates
-    to `True` or `False`. If a condition evaluates to `True`, the current state can be
-    modified.
+    A discrete event can be any event for which it is possible to write down a condition
+    that evaluates to `True` or `False`. If a condition evaluates to `True`, the current
+    state can be modified/replaced.
     """
 
     # New init because condition() types are more specific.
