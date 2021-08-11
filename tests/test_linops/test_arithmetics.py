b0e2d96e10ae5482bbd01886b5404579ad6495e6
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-08-11 16:55:24 +0200

    Improve tests for linop arithmetics

--- tests/test_linops/test_arithmetics.py
+++ tests/test_linops/test_arithmetics.py
@@ -1,112 +1,122 @@
 """Tests for linear operator arithmetics."""
+
+import operator
+
 import numpy as np
 import pytest
 
-from probnum import linops
-
-matrix_shape = (4, 4)
-
-kron_factor_shape = (2, 2)
-
-
-def get_kronecker():
-    return linops.Kronecker(
-        np.random.rand(*kron_factor_shape), np.random.rand(*kron_factor_shape)
-    )
-
-
-def get_zero():
-    return linops.Zero(matrix_shape)
-
-
-def get_scaling():
-    return linops.Scaling(factors=np.random.rand(matrix_shape[0]))
-
-
-def get_inverse_linop():
-    rand_matrix = np.random.rand(*matrix_shape)
-    _mat = linops.Matrix(rand_matrix.T @ rand_matrix)
-    return _mat.inv()
-
-
-def get_matrix_linop():
-    return linops.Matrix(np.random.rand(*matrix_shape))
-
-
-def get_idkron_linop():
-    return linops.IdentityKronecker(
-        num_blocks=kron_factor_shape[0], B=np.random.rand(*kron_factor_shape)
-    )
+from probnum.linops._arithmetic import _add_fns, _matmul_fns, _mul_fns, _sub_fns
+from probnum.linops._arithmetic_fallbacks import (
+    NegatedLinearOperator,
+    ProductLinearOperator,
+    ScaledLinearOperator,
+    SumLinearOperator,
+)
+from probnum.linops._kronecker import (
+    IdentityKronecker,
+    Kronecker,
+    SymmetricKronecker,
+    Symmetrize,
+)
+from probnum.linops._linear_operator import (
+    AdjointLinearOperator,
+    BinaryOperandType,
+    Embedding,
+    Identity,
+    LinearOperator,
+    Matrix,
+    Selection,
+    TransposedLinearOperator,
+    _ConjugateLinearOperator,
+    _InverseLinearOperator,
+    _TypeCastLinearOperator,
+)
+from probnum.linops._scaling import Scaling, Zero
+from probnum.problems.zoo.linalg import random_spd_matrix
 
 
-def get_id_linop():
-    return linops.Identity(matrix_shape[0])
-
-
-def get_sum_linop():
-    summand1 = linops.Matrix(np.random.rand(*matrix_shape))
-    summand2 = linops.Matrix(np.random.rand(*matrix_shape))
-    return summand1 + summand2
-
-
-def get_product_linop():
-    factor1 = linops.Matrix(np.random.rand(*matrix_shape))
-    factor2 = linops.Matrix(np.random.rand(*matrix_shape))
-    return factor1 @ factor2
-
-
-def get_negated_linop():
-    op = linops.Matrix(np.random.rand(*matrix_shape))
-    return -op
-
-
-def get_scaled_linop():
-    op = linops.Matrix(np.random.rand(*matrix_shape))
-    return 3.14 * op
-
-
-def get_transposed_linop():
-    op = linops.Matrix(np.random.rand(*matrix_shape))
-    return op.T
+def get_linop(linop_type):
+    if linop_type is Kronecker:
+        return linop_type(np.random.rand(2, 2), np.random.rand(2, 2))
+    elif linop_type is IdentityKronecker:
+        return linop_type(2, np.random.rand(2, 2))
+    elif linop_type is Zero or linop_type is Identity:
+        return linop_type(shape=(4, 4))
+    elif linop_type is Scaling:
+        return linop_type(factors=np.random.rand(4))
+    elif linop_type is Matrix:
+        return linop_type(np.random.rand(4, 4))
+    elif linop_type is _InverseLinearOperator:
+        _posdef_randmat = random_spd_matrix(rng=np.random.default_rng(123), dim=4)
+        return Matrix(_posdef_randmat).inv()
+    elif linop_type is TransposedLinearOperator:
+        return TransposedLinearOperator(linop=Matrix(np.random.rand(4, 4)))
+    elif linop_type is Embedding:
+        return Embedding(take_indices=(0, 1, 2), put_indices=(1, 0, 3), shape=(4, 3))
+    elif linop_type is Selection:
+        return Selection(indices=(1, 0, 3), shape=(3, 4))
+    elif linop_type is NegatedLinearOperator:
+        return NegatedLinearOperator(linop=Matrix(np.random.rand(4, 4)))
+    elif linop_type is ScaledLinearOperator:
+        return ScaledLinearOperator(linop=Matrix(np.random.rand(4, 4)), scalar=3.14)
+    elif linop_type is ProductLinearOperator:
+        return ProductLinearOperator(
+            Matrix(np.random.rand(4, 4)), Matrix(np.random.rand(4, 4))
+        )
+    elif linop_type is SumLinearOperator:
+        return SumLinearOperator(
+            Matrix(np.random.rand(4, 4)), Matrix(np.random.rand(4, 4))
+        )
+    elif linop_type is AdjointLinearOperator:
+        return AdjointLinearOperator(linop=Identity(4))
+    elif linop_type is _ConjugateLinearOperator:
+        return _ConjugateLinearOperator(linop=Identity(4, dtype=np.complex64))
+    elif linop_type is SymmetricKronecker:
+        return SymmetricKronecker(Identity(2), Identity(2))
+    elif linop_type is Symmetrize:
+        return Symmetrize(2)
+    elif linop_type is _TypeCastLinearOperator:
+        return _TypeCastLinearOperator(
+            linop=Matrix(np.random.rand(4, 4)), dtype=np.float32
+        )
+    elif isinstance(linop_type, str) and linop_type == "scalar":
+        return 1.3579
+    else:
+        raise TypeError(f"Don't know what to do with type {linop_type}.")
 
 
 @pytest.mark.parametrize(
-    "linop1",
-    [
-        get_kronecker(),
-        get_zero(),
-        get_scaling(),
-        get_inverse_linop(),
-        get_matrix_linop(),
-        get_idkron_linop(),
-        get_id_linop(),
-        get_sum_linop(),
-        get_product_linop(),
-        get_negated_linop(),
-        get_scaled_linop(),
-        get_transposed_linop(),
-    ],
+    "op", [operator.matmul, operator.mul, operator.add, operator.sub]
 )
-@pytest.mark.parametrize(
-    "linop2",
-    [
-        get_kronecker(),
-        get_zero(),
-        get_scaling(),
-        get_inverse_linop(),
-        get_matrix_linop(),
-        get_idkron_linop(),
-        get_id_linop(),
-        get_sum_linop(),
-        get_product_linop(),
-        get_negated_linop(),
-        get_scaled_linop(),
-        get_transposed_linop(),
-    ],
-)
-def test_arithmetics(linop1, linop2):
+def test_arithmetics(op):
 
-    res_linop = linop1 @ linop2
-    assert res_linop.ndim == 2
-    assert res_linop.shape[0] == linop1.shape[0]
-    assert res_linop.shape[1] == linop2.shape[1]
+    registry = {
+        operator.matmul: _matmul_fns,
+        operator.mul: _mul_fns,
+        operator.add: _add_fns,
+        operator.sub: _sub_fns,
+    }[op]
+
+    for (l_type, r_type) in registry.keys():
+        if (
+            l_type is Selection
+            or l_type is Embedding
+            or r_type is Selection
+            or r_type is Embedding
+        ):
+            # Checked seperatly
+            continue
+
+        linop1 = get_linop(l_type)
+        linop2 = get_linop(r_type)
+
+        res_linop = op(linop1, linop2)
+        assert res_linop.ndim == 2
+
+        if isinstance(l_type, str) and l_type == "scalar":
+            assert res_linop.shape == linop2.shape
+        elif isinstance(r_type, str) and r_type == "scalar":
+            assert res_linop.shape == linop1.shape
+        else:
+            assert res_linop.shape[0] == linop1.shape[0]
+            assert res_linop.shape[1] == linop2.shape[1]
