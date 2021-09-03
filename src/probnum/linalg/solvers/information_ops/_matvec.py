bea7633fca5d335813761634b2809120c4c0a745
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-09-03 11:52:23 +0200

    minor docstring tweaks

--- src/probnum/linalg/solvers/information_ops/_matvec.py
+++ src/probnum/linalg/solvers/information_ops/_matvec.py
@@ -16,5 +16,5 @@ class MatVecInfoOp(LinearSolverInfoOp):
     def __call__(
         self, solver_state: "probnum.linalg.solvers.ProbabilisticLinearSolverState"
     ) -> np.ndarray:
-        r"""Matrix vector product of :math:`A` with the current action."""
+        r"""Matrix vector product with the system matrix :math:`A`."""
         return solver_state.problem.A @ solver_state.action
