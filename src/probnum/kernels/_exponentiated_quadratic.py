Input dimension broadcasting bugfix

src/probnum/kernels/_exponentiated_quadratic.py
@@ -7,12 +7,12 @@ import numpy as np
 import probnum.utils as _utils
 from probnum.typing import IntArgType, ScalarArgType
 
-from ._kernel import Kernel
+from ._kernel import IsotropicMixin, Kernel
 
 _InputType = np.ndarray
 
 
-class ExpQuad(Kernel[_InputType]):
+class ExpQuad(Kernel[_InputType], IsotropicMixin):
     """Exponentiated quadratic / RBF kernel.
 
     Covariance function defined by :math:`k(x_0, x_1) = \\exp \\big(-\\frac{\\lVert
@@ -55,7 +55,7 @@ class ExpQuad(Kernel[_InputType]):
                 shape=x0.shape[:-1] + (1, 1),
             )
 
-        sqdists = np.sum((x0 - x1) ** 2, axis=-1)
+        sqdists = self._squared_euclidean_distances(x0, x1)
         kernmat = np.exp(-1.0 / (2.0 * self.lengthscale ** 2) * sqdists)
 
         return kernmat[..., None, None]

src/probnum/kernels/_linear.py
@@ -44,9 +44,6 @@ class Linear(Kernel[_InputType]):
         super().__init__(input_dim=input_dim, output_dim=1)
 
     def _evaluate(self, x0: _InputType, x1: Optional[_InputType] = None) -> np.ndarray:
-        if x1 is None:
-            x1 = x0
-
-        kernmat = np.sum(x0 * x1, axis=-1) + self.constant
+        kernmat = self._euclidean_inner_products(x0, x1) + self.constant
 
         return kernmat[..., None, None]

src/probnum/kernels/_matern.py
@@ -9,12 +9,12 @@ import scipy.special
 import probnum.utils as _utils
 from probnum.typing import IntArgType, ScalarArgType
 
-from ._kernel import Kernel
+from ._kernel import IsotropicMixin, Kernel
 
 _InputType = np.ndarray
 
 
-class Matern(Kernel[_InputType]):
+class Matern(Kernel[_InputType], IsotropicMixin):
     r"""Matern kernel.
 
     Covariance function defined by
@@ -79,13 +79,7 @@ class Matern(Kernel[_InputType]):
         super().__init__(input_dim=input_dim, output_dim=1)
 
     def _evaluate(self, x0: _InputType, x1: Optional[_InputType] = None) -> np.ndarray:
-        if x1 is None:
-            dists = np.zeros_like(  # pylint: disable=unexpected-keyword-arg
-                x0,
-                shape=x0.shape[:-1],
-            )
-        else:
-            dists = np.linalg.norm(x0 - x1, ord=2, axis=-1)
+        dists = self._euclidean_distances(x0, x1)
 
         # Kernel matrix computation dependent on differentiability
         if self.nu == 0.5:

src/probnum/kernels/_polynomial.py
@@ -52,9 +52,7 @@ class Polynomial(Kernel[_InputType]):
         super().__init__(input_dim=input_dim, output_dim=1)
 
     def _evaluate(self, x0: _InputType, x1: Optional[_InputType] = None) -> np.ndarray:
-        if x1 is None:
-            x1 = x0
-
-        kernmat = (np.sum(x0 * x1, axis=-1) + self.constant) ** self.exponent
+        kernmat = self._euclidean_inner_products(x0, x1) + self.constant
+        kernmat = kernmat ** self.exponent
 
         return kernmat[..., None, None]

src/probnum/kernels/_rational_quadratic.py
@@ -8,12 +8,12 @@ import scipy.spatial.distance
 import probnum.utils as _utils
 from probnum.typing import IntArgType, ScalarArgType
 
-from ._kernel import Kernel
+from ._kernel import IsotropicMixin, Kernel
 
 _InputType = np.ndarray
 
 
-class RatQuad(Kernel[_InputType]):
+class RatQuad(Kernel[_InputType], IsotropicMixin):
     """Rational quadratic kernel.
 
     Covariance function defined by :math:`k(x_0, x_1) = \\big(1 + \\frac{\\lVert x_0 -
@@ -67,7 +67,7 @@ class RatQuad(Kernel[_InputType]):
                 shape=x0.shape[:-1] + (1, 1),
             )
 
-        sqdists = np.sum((x0 - x1) ** 2, axis=-1)
+        sqdists = self._squared_euclidean_distances(x0, x1)
         kernmat = (
             1.0 + sqdists / (2.0 * self.alpha * self.lengthscale ** 2)
         ) ** -self.alpha

tests/test_kernels/conftest.py
@@ -118,14 +118,23 @@ def fixture_kernel_naive(kernel: pn.kernels.Kernel):
         x1: np.ndarray = None,
         squeeze_output_dim: bool = True,
     ):
-        x0 = np.atleast_1d(x0)
+        x0, x1, _ = np.broadcast_arrays(
+            x0,
+            x0 if x1 is None else x1,
+            # This broadcasts x0 and x1 to have `input_dim` elements along their last
+            # dimensions
+            np.empty_like(  # pylint: disable=unexpected-keyword-arg
+                x0,
+                shape=(kernel.input_dim,),
+            ),
+        )
 
-        if x1 is None:
-            x1 = x0
-        else:
-            x1 = np.atleast_1d(x1)
+        assert x0.shape == x1.shape
+        assert x0.shape[-1] == kernel.input_dim and x1.shape[-1] == kernel.input_dim
 
         K = kernel_vectorized(x0, x1, squeeze_output_dim=False)
+
+        assert K.shape[-2:] == 2 * (kernel.output_dim,)
 
         if kernel.output_dim == 1 and squeeze_output_dim:
             K = K.squeeze((-2, -1))