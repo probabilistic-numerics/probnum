66a63e8ac11f67cca40ac3e3bcc81f0cc865755c
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-10-18 12:51:33 +0200

    linear system belief update skeleton

---
+++ src/probnum/linalg/solvers/belief_updates/_linear_system_belief_update.py
@@ -0,0 +1,18 @@
+"""Linear system belief updates.
+
+Class defining how a belief over quantities of interest of a linear system is updated
+given information about the problem.
+"""
+
+
+class LinearSystemBeliefUpdate:
+    r"""Belief update for quantities of interest of a linear system.
+
+
+    Parameters
+    ----------
+
+    """
+
+    def __init__(self) -> None:
+        pass
