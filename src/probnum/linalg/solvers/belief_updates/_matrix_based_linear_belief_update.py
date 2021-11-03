985068efc3e62d0147e5e20f78735a5cdbc6f383
Jonathan Wenger <j.wenger@tum.de>
2021-11-03 16:19:37 +0100

    Update src/probnum/linalg/solvers/belief_updates/_matrix_based_linear_belief_update.py
    Co-authored-by: Marvin Pförtner <marvin.pfoertner@icloud.com>

--- src/probnum/linalg/solvers/belief_updates/_matrix_based_linear_belief_update.py
+++ src/probnum/linalg/solvers/belief_updates/_matrix_based_linear_belief_update.py
@@ -16,7 +16,7 @@ class MatrixBasedLinearBeliefUpdate(LinearSolverBeliefUpdate):
 
     .. math ::
         \begin{align}
-            M_{i+1} &= M_i + (y - M_i s) (s^\top W s)^\dagger s^\top W_i,\\
+            M_{i+1} &= M_i + (y - M_i s) (s^\top W_i s)^\dagger s^\top W_i,\\
             W_{i+1} &= W_i - W_i s (s^\top W s)^\dagger s^\top W_i.
         \end{align}
 
