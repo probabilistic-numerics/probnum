05ecd4af7814e47326ae0df241ec1b06b0fc1a2a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-11-08 18:42:45 +0100

    more structure for stein operator

--- src/probnum/kernels/_stein.py
+++ src/probnum/kernels/_stein.py
@@ -35,51 +35,67 @@ class LangevinSteinKernel(Kernel):
         shape: ShapeArgType = ...,
         kernel_mean: Optional[ArrayLike] = None,
     ):
-        self._base_kernel = base_kernel
-        self._grad_log_density = grad_log_density
+        self.base_kernel = base_kernel
+        self.grad_log_density = grad_log_density
+        self.stein_operator = LangevinSteinOperator(
+            grad_log_density=self._grad_log_density
+        )
         if kernel_mean is None:
             kernel_mean = 0.0
-        self._kernel_mean = kernel_mean
+        self.kernel_mean = kernel_mean
         super().__init__(input_dim, shape=shape)
 
     def _evaluate(
         self, x0: ArrayLike, x1: Optional[ArrayLike]
     ) -> Union[np.ndarray, np.float_]:
 
-        return self._langevin_operator_first_kind(x0=x0, x1=x1) + self._kernel_mean
+        return self.stein_operator(x0=x0, x1=x1) + self._kernel_mean
 
-    def _langevin_operator_of_the_first_kind(
-        self,
-        x0: ArrayLike,
-        x1: ArrayLike,
-    ) -> Union[np.ndarray, np.float_]:
-        r"""Evaluates the Langevin operator of the first kind.
 
-        Computes the Langevin operator of the first kind given by
+class LangevinSteinOperator:
+    r"""Langevin Stein operator.
 
-        .. math ::
-            \mathcal{L}_\pi[u] = \nabla_x \log \pi(x) \cdot u(x) + \nabla \cdot u(x)
+    Stein operator defined via a Langevin diffusion of the first kind given by
 
-        Parameters
-        ----------
+    .. math ::
+        \mathcal{L}_\pi[u] = \nabla_x \log \pi(x) \cdot u(x) + \nabla \cdot u(x)
 
-        Returns
-        -------
 
-        """
-        gradx0_logp = self._grad_log_density(x0)
-        gradx1_logp = self._grad_log_density(x1)
-        gradx0_base_kernel = backend.grad(self._base_kernel, argnums=0)
-        gradx1_base_kernel = backend.grad(self._base_kernel, argnums=1)
-        gradx0_gradx1_base_kernel = backend.grad(self._base_kernel, argnums=(0, 1))
+    Parameters
+    ----------
+    fun
+        Function to which to apply the Stein operator to.
+    """
 
-        score_prod = gradx0_logp @ gradx1_logp.T
+    def __init__(self, grad_log_density: Callable[[np.ndarray], np.ndarray]) -> None:
+        self.grad_log_density = grad_log_density
 
-        return (
-            score_prod * self._base_kernel(x0=x0, x1=x1)
-            + np.einsum(
-                "md,dmn->mn", gradx0_logp, gradx1_base_kernel
-            )  # TODO: double check order of indices after grad
-            + np.einsum("nd,dmn->mn", gradx1_logp, gradx0_base_kernel)
-            + np.einsum("ddmn->mn", gradx0_gradx1_base_kernel)
-        )
+    def __call__(
+        self, fun: Callable[[np.ndarray], float]
+    ) -> Callable[[np.ndarray], float]:
+        raise NotImplementedError
+
+    def apply_to_kernel(
+        self, kernel: Kernel
+    ) -> Callable[[np.ndarray, np.ndarray], np.ndarray]:
+        """Applies the Langevin Stein operator to a given kernel."""
+
+        def _stein_kernel_matrix(x0: np.ndarray, x1: np.ndarray):
+            gradx0_logp = self._grad_log_density(x0)
+            gradx1_logp = self._grad_log_density(x1)
+            gradx0_base_kernel = backend.grad(kernel, argnums=0)
+            gradx1_base_kernel = backend.grad(kernel, argnums=1)
+            gradx0_gradx1_base_kernel = backend.grad(kernel, argnums=(0, 1))
+
+            score_prod = gradx0_logp @ gradx1_logp.T
+
+            return (
+                score_prod * kernel(x0=x0, x1=x1)
+                + np.einsum(
+                    "md,dmn->mn", gradx0_logp, gradx1_base_kernel
+                )  # TODO: double check order of indices after grad
+                + np.einsum("nd,dmn->mn", gradx1_logp, gradx0_base_kernel)
+                + np.einsum("ddmn->mn", gradx0_gradx1_base_kernel)
+            )
+
+        return _stein_kernel_matrix
