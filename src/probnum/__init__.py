c5d2f52c01d4f812d095c6d6ac52fd3ae651d25a
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-11-09 09:07:53 +0100

    .

--- src/probnum/__init__.py
+++ src/probnum/__init__.py
@@ -1,10 +1,9 @@
 """Probabilistic Numerical Methods.
 
-ProbNum implements probabilistic numerical methods in Python. Such
-methods solve numerical problems from linear algebra, optimization,
-quadrature and differential equations using probabilistic inference.
-This approach captures uncertainty arising from finite computational
-resources and stochastic input.
+ProbNum implements probabilistic numerical methods in Python. Such methods solve
+numerical problems from linear algebra, optimization, quadrature and differential
+equations using probabilistic inference. This approach captures uncertainty arising from
+finite computational resources and stochastic input.
 """
 
 # isort: off
@@ -15,13 +14,12 @@ resources and stochastic input.
 # unguarded global state and is hence not thread-safe!
 from ._config import _GLOBAL_CONFIG_SINGLETON as config
 
-# isort: on
-
-# isort: off
-
-# Compute Backends
-from ._backend import *
-from ._backend import __all__ as _backend_fns
+# Abstract interfaces for (components of) probabilistic numerical methods.
+from ._pnmethod import (
+    ProbabilisticNumericalMethod,
+    StoppingCriterion,
+    LambdaStoppingCriterion,
+)
 
 # isort: on
 
@@ -37,7 +35,6 @@ from . import (
     randvars,
     utils,
 )
-from ._probabilistic_numerical_method import ProbabilisticNumericalMethod
 from ._version import version as __version__
 from .randvars import asrandvar
 
@@ -45,7 +42,11 @@ from .randvars import asrandvar
 __all__ = [
     "asrandvar",
     "ProbabilisticNumericalMethod",
-] + _backend_fns
+    "StoppingCriterion",
+    "LambdaStoppingCriterion",
+]
 
 # Set correct module paths. Corrects links and module paths in documentation.
 ProbabilisticNumericalMethod.__module__ = "probnum"
+StoppingCriterion.__module__ = "probnum"
+LambdaStoppingCriterion.__module__ = "probnum"
