97c1d74e447eeeed0a0b05844756e7affe7eef6b
Maren Mahsereci <maren.mhsrc@gmail.com>
2021-11-17 17:40:34 +0100

    small rewording

--- src/probnum/quad/_bayesquad.py
+++ src/probnum/quad/_bayesquad.py
@@ -1,6 +1,6 @@
 """Bayesian Quadrature.
 
-This module provides solvers to integrate functions through Bayesian quadrature,
+This module provides routines to integrate functions through Bayesian quadrature,
 meaning a model over the integrand is constructed in order to actively select evaluation
 points of the integrand to estimate the value of the integral. Bayesian quadrature
 methods return a random variable, specifying the belief about the true value of the
