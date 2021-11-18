ce071e934b0d93066c5d02fbea52778d2722e9bf
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-11-18 17:03:09 +0100

    Fix docs

--- src/probnum/_config.py
+++ src/probnum/_config.py
@@ -130,8 +130,8 @@ _DEFAULT_CONFIG_OPTIONS = [
         "matrix_free",
         False,
         (
-            "If ``True``, wherever possible, ``LinearOperator``s are used instead "
-            "of arrays. ``LinearOperator``s define a matrix-vector product implicitly without "
+            "If ``True``, wherever possible, ``LinearOperator`` s are used instead "
+            "of arrays. ``LinearOperator`` s define a matrix-vector product implicitly without "
             "instantiating the full matrix in memory. This makes them memory- and runtime-efficient "
             "for linear algebra operations."
         ),
@@ -139,7 +139,7 @@ _DEFAULT_CONFIG_OPTIONS = [
     (
         "lazy_matrix_matrix_matmul",
         False,
-        "If ``True``, the matmul operation applied to two ``Matrix``-``LinearOperator``s will "
+        "If ``True``, the matmul operation applied to two ``Matrix``-``LinearOperator`` s will "
         "again yield a ``Matrix``-``LinearOperator`` with the computed matrix-matrix product,"
         " instead of a ``ProductLinearOperator``.",
     ),
