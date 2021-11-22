ac10e59196f39b51b65b1080294b064452bd3e7e
Maren Mahsereci <42842079+mmahsereci@users.noreply.github.com>
2021-11-22 14:07:36 +0100

    BQ stopping criteria use base class (#570)

---
+++ src/probnum/quad/solvers/stopping_criteria/_integral_variance_tol.py
@@ -0,0 +1,23 @@
+"""Stopping criterion based on the absolute value of the integral variance"""
+
+from probnum.quad.solvers.bq_state import BQState
+from probnum.quad.solvers.stopping_criteria import BQStoppingCriterion
+from probnum.typing import FloatArgType
+
+# pylint: disable=too-few-public-methods, fixme
+
+
+class IntegralVarianceTolerance(BQStoppingCriterion):
+    """Stop once the integral variance is below some tolerance.
+
+    Parameters
+    ----------
+    var_tol:
+        Tolerance value of the variance.
+    """
+
+    def __init__(self, var_tol: FloatArgType):
+        self.var_tol = var_tol
+
+    def __call__(self, bq_state: BQState) -> bool:
+        return bq_state.integral_belief.var <= self.var_tol
