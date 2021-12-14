1c6dfa65da13e71b50a8010a9c3c8e431010a765
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2021-12-14 12:31:59 +0100

    Skip tests for select backends

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
-# -*- coding: utf-8 -*-
-"""Dummy conftest.py for probnum.
+import pytest
 
-If you don't know what this is for, just leave it empty. Read more about
-conftest.py under: https://pytest.org/latest/plugins.html
-"""
+from probnum import backend
 
-# import pytest
+
+def pytest_configure(config: "_pytest.config.Config"):
+    config.addinivalue_line(
+        "markers", "skipif_backend(backend): Skip test for the given backend."
+    )
+
+
+def pytest_runtest_setup(item: pytest.Item):
+    # Setup conditional backend skip
+    skipped_backends = [
+        mark.args[0] for mark in item.iter_markers(name="skipif_backend")
+    ]
+
+    if skipped_backends:
+        if backend.BACKEND in skipped_backends:
+            pytest.skip(f"Test skipped for backend {backend.BACKEND}.")
