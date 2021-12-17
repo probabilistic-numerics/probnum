816b2cdfb7f5160abe99bba966dcd9d63c9465a0
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2021-12-17 08:08:13 +0100

    more tests for orthogonalization functions

--- tests/test_utils/test_linalg/test_orthogonalize.py
+++ tests/test_utils/test_linalg/test_orthogonalize.py
@@ -1,10 +1,12 @@
 """Tests for orthogonalization functions."""
 
-from typing import Callable
+from typing import Callable, Union
 
 import numpy as np
+import pytest
 from pytest_cases import parametrize
 
+from probnum import linops
 from probnum.utils.linalg import (
     double_gram_schmidt,
     gram_schmidt,
@@ -12,14 +14,55 @@ from probnum.utils.linalg import (
 )
 
 rng = np.random.default_rng(42)
+n = 100
 
 
-@parametrize("vector", rng.normal(size=(3, 100)))
+@parametrize("vector", rng.normal(size=(3, n)))
 @parametrize("basis_size", [1, 10, 50])
+@parametrize(
+    "inprod",
+    [
+        np.eye(n),
+        linops.Identity(n),
+        linops.Scaling(factors=1.0, shape=(n, n)),
+        lambda v, w: np.inner(v, w),
+    ],
+)
 @parametrize(
     "orthogonalization_fn", [gram_schmidt, modified_gram_schmidt, double_gram_schmidt]
 )
 def test_is_orthogonal(
+    vector: np.ndarray,
+    basis_size: int,
+    inprod: Union[
+        np.ndarray,
+        linops.LinearOperator,
+        Callable[[np.ndarray, np.ndarray], np.ndarray],
+    ],
+    orthogonalization_fn: Callable[[np.ndarray, np.ndarray], np.ndarray],
+):
+    # Compute orthogonal basis
+    orthogonal_basis, _ = np.linalg.qr(rng.normal(size=(vector.shape[0], basis_size)))
+    orthogonal_basis = orthogonal_basis.T
+
+    # Orthogonalize vector
+    ortho_vector = orthogonalization_fn(
+        v=vector, orthogonal_basis=orthogonal_basis, inprod=inprod
+    )
+    np.testing.assert_allclose(
+        orthogonal_basis @ ortho_vector,
+        np.zeros((basis_size,)),
+        atol=1e-12,
+        rtol=1e-12,
+    )
+
+
+@parametrize("vector", rng.normal(size=(3, n)))
+@parametrize("basis_size", [1, 10, 50])
+@parametrize(
+    "orthogonalization_fn", [gram_schmidt, modified_gram_schmidt, double_gram_schmidt]
+)
+def test_is_normalized(
     vector: np.ndarray,
     basis_size: int,
     orthogonalization_fn: Callable[[np.ndarray, np.ndarray], np.ndarray],
@@ -29,10 +72,8 @@ def test_is_orthogonal(
     orthogonal_basis = orthogonal_basis.T
 
     # Orthogonalize vector
-    ortho_vector = orthogonalization_fn(v=vector, orthogonal_basis=orthogonal_basis)
-    np.testing.assert_allclose(
-        orthogonal_basis @ ortho_vector,
-        np.zeros((basis_size,)),
-        atol=1e-12,
-        rtol=1e-12,
+    ortho_vector = orthogonalization_fn(
+        v=vector, orthogonal_basis=orthogonal_basis, normalize=True
     )
+
+    assert np.inner(ortho_vector, ortho_vector) == pytest.approx(1.0)
