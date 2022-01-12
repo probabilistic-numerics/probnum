1d4b9bdd7443c7fb5d70b169a0e1dfcd2417cb27
Nicholas Krämer <nicokraemer@gmx.net>
2022-01-12 10:42:24 +0100

    Line-too-long iterated component

--- src/probnum/filtsmooth/optim/_iterated_component.py
+++ src/probnum/filtsmooth/optim/_iterated_component.py
@@ -19,7 +19,9 @@ class IteratedDiscreteComponent(randprocs.markov.Transition):
 
     Set up an iterated component.
 
-    >>> iwp = IntegratedWienerProcess(initarg=0., num_derivatives=2, wiener_process_dimension=1)
+    >>> iwp = IntegratedWienerProcess(
+    ...     initarg=0., num_derivatives=2, wiener_process_dimension=1
+    ... )
     >>> H0, H1 = iwp.transition.proj2coord(coord=0), iwp.transition.proj2coord(coord=1)
     >>> call = lambda t, x: H1 @ x - H0 @ x * (1 - H0 @ x)
     >>> jacob = lambda t, x: H1 - (1 - 2*(H0 @ x)) @ H0
