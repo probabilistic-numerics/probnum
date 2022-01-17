5217716286cd82d3a153c9cb0d7ccdd999ea7844
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-17 13:07:12 +0100

    restructured init into folder

---
+++ src/probnum/diffeq/odefilter/init/__init__.py
@@ -0,0 +1,6 @@
+"""Initialization routines."""
+
+
+from ._autodiff import ForwardMode, ForwardModeJVP, ReverseMode, TaylorMode
+from ._runge_kutta import RungeKutta
+from ._stack import Stack, StackWithJacobian
