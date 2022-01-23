1aa6a4d2b402c2dddcda3032690ac98aeb438a07
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-01-23 19:49:19 +0100

    Kronecker SPD cases

--- tests/test_linops/test_linops_cases/kronecker_cases.py
+++ tests/test_linops/test_linops_cases/kronecker_cases.py
@@ -1,10 +1,19 @@
-from typing import Tuple
+import itertools
+from typing import Tuple, Union
 
 import numpy as np
 import pytest
 import pytest_cases
 
 import probnum as pn
+from probnum.problems.zoo.linalg import random_spd_matrix
+
+spd_matrices = (
+    pn.linops.Identity(shape=(1, 1)),
+    np.array([[1.0, -2.0], [-2.0, 5.0]]),
+    random_spd_matrix(np.random.default_rng(597), dim=10),
+    random_spd_matrix(np.random.default_rng(597), dim=13),
+)
 
 
 @pytest.mark.parametrize(
@@ -30,6 +39,24 @@ def case_kronecker(
 ) -> Tuple[pn.linops.LinearOperator, np.ndarray]:
     linop = pn.linops.Kronecker(A, B)
     matrix = np.kron(A, B)
+
+    return linop, matrix
+
+
+@pytest_cases.case(tags=["square", "symmetric", "positive-definite"])
+@pytest_cases.parametrize("A,B", itertools.product(spd_matrices, spd_matrices))
+def case_kronecker_positive_definite(
+    A: Union[np.ndarray, pn.linops.LinearOperator],
+    B: Union[np.ndarray, pn.linops.LinearOperator],
+) -> Tuple[pn.linops.LinearOperator, np.ndarray]:
+    A = pn.linops.aslinop(A)
+    A.is_symmetric = True
+
+    B = pn.linops.aslinop(B)
+    B.is_symmetric = True
+
+    linop = pn.linops.Kronecker(A, B)
+    matrix = np.kron(A.todense(), B.todense())
 
     return linop, matrix
 
@@ -72,5 +99,26 @@ def case_symmetric_kronecker_identical_factors(
 ) -> Tuple[pn.linops.LinearOperator, np.ndarray]:
     linop = pn.linops.SymmetricKronecker(A)
     matrix = np.kron(A, A)
+
+    return linop, matrix
+
+
+@pytest_cases.case(
+    tags=[
+        "symmetric_kronecker",
+        "square",
+        "symmetric",
+        "positive-definite",
+    ]
+)
+@pytest_cases.parametrize("A", spd_matrices)
+def case_symmetric_kronecker_identical_factors_positive_definite(
+    A: Union[np.ndarray, pn.linops.LinearOperator],
+) -> Tuple[pn.linops.LinearOperator, np.ndarray]:
+    A = pn.linops.aslinop(A)
+    A.is_symmetric = True
+
+    linop = pn.linops.SymmetricKronecker(A)
+    matrix = np.kron(A.todense(), A.todense())
 
     return linop, matrix
