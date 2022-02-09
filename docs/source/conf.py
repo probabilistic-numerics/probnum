e385f75541444e2d38470cfe0382b16b5a086b19
Nicholas Krämer <nicokraemer@gmx.net>
2022-02-09 19:00:40 +0100

    scipy intersphinx mapping update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -144,7 +144,7 @@ nbsphinx_execute_arguments = [
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy-1.8.0/html-scipyorg/", None),
     "matplotlib": ("https://matplotlib.org/", None),
 }
 
