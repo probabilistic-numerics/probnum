7a8b493429b98c2e0e7dc1160c0e7598c6c9e1c6
Nicholas Krämer <nicokraemer@gmx.net>
2022-02-16 12:26:09 +0100

    Testcase for discrete IEKS

--- tests/test_filtsmooth/test_optim/test_gauss_newton.py
+++ tests/test_filtsmooth/test_optim/test_gauss_newton.py
@@ -1,56 +1,77 @@
 import numpy as np
 import pytest
+import pytest_cases
 
-from probnum import filtsmooth
+from probnum import filtsmooth, problems, randprocs
 import probnum.problems.zoo.filtsmooth as filtsmooth_zoo
 
 
-@pytest.fixture(name="setup", params=[filtsmooth_zoo.logistic_ode])
-def fixture_setup(request):
+def case_logistic_ode():
     """Filter and regression problem."""
-    problem = request.param
-    regression_problem, info = problem()
-
-    stopcrit = filtsmooth.optim.FiltSmoothStoppingCriterion(
-        atol=1e-1, rtol=1e-1, maxit=10
-    )
-
+    regression_problem, info = filtsmooth_zoo.logistic_ode()
     kalman = filtsmooth.gaussian.ContinuousKalman(
         info["prior_process"],
     )
-    gauss_newton = filtsmooth.optim.GaussNewton(kalman, stopping_criterion=stopcrit)
-    return gauss_newton, regression_problem
+    return kalman, regression_problem
 
 
-def test_rmse_filt_smooth(setup):
+def case_pendulum():
+    """Filter and regression problem."""
+    rng = np.random.default_rng(seed=1)
+    regression_problem, info = filtsmooth_zoo.pendulum(rng=rng)
+
+    # Linearize measurement models
+    measmods = regression_problem.measurement_models
+    ekf_meas = [filtsmooth.gaussian.approx.DiscreteEKFComponent(mm) for mm in measmods]
+    regression_problem = problems.TimeSeriesRegressionProblem(
+        locations=regression_problem.locations,
+        observations=regression_problem.observations,
+        measurement_models=ekf_meas,
+        solution=regression_problem.solution,
+    )
+
+    # Linearize prior
+    prior_process = info["prior_process"]
+    ekf_dyna = filtsmooth.gaussian.approx.DiscreteEKFComponent(prior_process.transition)
+    prior_process_linearized = randprocs.markov.MarkovProcess(
+        transition=ekf_dyna,
+        initrv=prior_process.initrv,
+        initarg=prior_process.initarg,
+    )
+
+    # Assemble Kalman filter
+    kalman = filtsmooth.gaussian.DiscreteKalman(prior_process_linearized)
+    return kalman, regression_problem
+
+
+@pytest_cases.parametrize_with_cases("kalman, regression_problem", cases=".")
+def test_rmse_filt_smooth(kalman, regression_problem):
     """Assert that iterated smoothing beats smoothing."""
 
-    np.random.seed(12345)
-    gauss_newton, regression_problem = setup
-    kalman = gauss_newton.kalman
-    truth = regression_problem.solution
+    # Assemble iterated smoother
+    stopcrit = filtsmooth.optim.FiltSmoothStoppingCriterion(
+        atol=1e-1, rtol=1e-1, maxit=7
+    )
+    gauss_newton = filtsmooth.optim.GaussNewton(kalman, stopping_criterion=stopcrit)
 
+    # Compute filtering, smoothing, and iterated smoothing solutions
     posterior, _ = kalman.filter(regression_problem)
     posterior = kalman.smooth(posterior)
-
     iterated_posterior, _ = gauss_newton.solve(
         regression_problem, initial_guess=posterior
     )
-
     filtms = posterior.filtering_posterior.states.mean
     smooms = posterior.states.mean
     iterms = iterated_posterior.states.mean
 
-    if filtms.ndim == 1:
-        filtms = filtms.reshape((-1, 1))
-        smooms = smooms.reshape((-1, 1))
-        iterms = iterms.reshape((-1, 1))
-
-    if truth.ndim == 1:
-        truth = truth.reshape((-1, 1))
-
     # Compare only zeroth component
     # for compatibility with all test cases
+    truth = regression_problem.solution
+    if truth.ndim == 1:
+        truth = truth.reshape((-1, 1))
+    if filtms.ndim == 1:
+        smooms = smooms.reshape((-1, 1))
+        iterms = iterms.reshape((-1, 1))
     smooms_rmse = np.mean(np.abs(smooms[:, 0] - truth[:, 0]))
     iterms_rmse = np.mean(np.abs(iterms[:, 0] - truth[:, 0]))
 
