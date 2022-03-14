a03d502c66cc72441434f6903b5bba7ea1ba67bb
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-03-14 19:19:46 +0100

    renaming a test

--- tests/test_quad/test_bq_state.py
+++ tests/test_quad/test_bq_state.py
@@ -63,7 +63,7 @@ def test_info_from_iteration(info):
 
 def test_info_from_bq_state(bq_state, bq_state_no_data, nevals):
 
-    # no data given
+    # no evaluations/ data given
     new_info = BQIterInfo.from_bq_state(bq_state_no_data)
     assert new_info.iteration == 0
     assert new_info.nevals == 0
@@ -78,11 +78,13 @@ def test_info_from_bq_state(bq_state, bq_state_no_data, nevals):
 
 def test_info_from_stopping_decision(info):
 
+    # solver converged
     new_info = BQIterInfo.from_stopping_decision(info, has_converged=False)
     assert new_info.iteration == info.iteration
     assert new_info.nevals == info.iteration
     assert not new_info.has_converged
 
+    # solver did not converge converged
     new_info = BQIterInfo.from_stopping_decision(info, has_converged=True)
     assert new_info.iteration == info.iteration
     assert new_info.nevals == info.iteration
@@ -121,15 +123,15 @@ def test_state_defaults_shapes(state, request):
     assert s.kernel_means.shape == (0,)
 
 
-def test_state_defaults_shapes2(bq_state, bq_state_no_data, nevals):
+def test_state_defaults_node_shapes(bq_state, bq_state_no_data, nevals):
 
-    # no data given
+    # no evaluations/ data given
     s = bq_state_no_data
 
     assert s.nodes.shape == (0, s.input_dim)
     assert s.fun_evals.shape == (0,)
 
-    # data given
+    # evaluations/ data given
     s = bq_state
 
     assert s.nodes.shape == (nevals, s.input_dim)
