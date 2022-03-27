45c64769a180e55618966d5fc0807c6a153fc6fb
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-03-27 19:00:58 +0100

    Bugfix in `ProductMatern` tests

--- tests/probnum/randprocs/kernels/test_product_matern.py
+++ tests/probnum/randprocs/kernels/test_product_matern.py
@@ -7,31 +7,39 @@ import pytest
 
 from probnum import backend, compat
 from probnum.randprocs import kernels
-from probnum.typing import ArrayLike
+from probnum.typing import ArrayLike, ShapeType
 import tests.utils
 
 
 @pytest.mark.parametrize("lengthscale", [1.25])
 @pytest.mark.parametrize("nu", [0.5, 1.5, 2.5, 3.0])
-def test_kernel_matrix(input_dim: int, lengthscale: float, nu: float):
+def test_kernel_matrix(input_shape: ShapeType, lengthscale: float, nu: float):
     """Check that the product Matérn kernel matrix is an elementwise product of 1D
     Matérn kernel matrices."""
-    matern = kernels.Matern(input_shape=(1,), lengthscale=lengthscale, nu=nu)
+    if len(input_shape) > 1:
+        pytest.skip()
+
+    matern = kernels.Matern(input_shape=(), lengthscale=lengthscale, nu=nu)
     product_matern = kernels.ProductMatern(
-        input_shape=(input_dim,), lengthscales=lengthscale, nus=nu
+        input_shape=input_shape, lengthscales=lengthscale, nus=nu
     )
 
-    num_xs = 15
-    xs_shape = (num_xs, input_dim)
+    xs_shape = (15,) + input_shape
     xs = backend.random.uniform(
         seed=tests.utils.random.seed_from_sampling_args(base_seed=42, shape=xs_shape),
         shape=xs_shape,
     )
 
     kernel_matrix1 = product_matern.matrix(xs)
-    kernel_matrix2 = functools.reduce(
-        operator.mul, (matern.matrix(xs[:, [dim]]) for dim in range(input_dim))
-    )
+
+    if len(input_shape) > 0:
+        assert len(input_shape) == 1
+
+        kernel_matrix2 = functools.reduce(
+            operator.mul, (matern.matrix(xs[:, dim]) for dim in range(input_shape[0]))
+        )
+    else:
+        kernel_matrix2 = matern.matrix(xs)
 
     compat.testing.assert_allclose(kernel_matrix1, kernel_matrix2)
 
