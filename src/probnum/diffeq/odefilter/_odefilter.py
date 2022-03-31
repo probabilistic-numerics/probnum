2bdfb51784427d7813479dd0bafc2bb3c18b4d6b
Anshul Kapoor <anshul.kapoor@level.com>
2022-03-31 10:19:33 -0700

    spacing

--- src/probnum/diffeq/odefilter/_odefilter.py
+++ src/probnum/diffeq/odefilter/_odefilter.py
@@ -16,9 +16,8 @@ from probnum.diffeq.odefilter import (
 
 
 class ODEFilter(_odesolver.ODESolver):
-    """Probabilistic ODE solver based on Gaussian filtering and smoothing.
-
-    This is based on continuous-discrete Gaussian filtering.
+    """Probabilistic ODE solver based on Gaussian filtering and smoothing. This is based
+    on continuous-discrete Gaussian filtering.
 
     Note: this is specific for IVPs and does not apply without
     further considerations to, e.g., BVPs.
