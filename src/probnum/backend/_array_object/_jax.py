8ccfab02133adad46c0ee619b682fbfc2e0fcc7c
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-04-01 15:52:55 +0200

    Bugfixes in backend._array_object

--- src/probnum/backend/_array_object/_jax.py
+++ src/probnum/backend/_array_object/_jax.py
@@ -1,7 +1,7 @@
 """Array object in JAX."""
 
 from jax.numpy import (  # pylint: disable=redefined-builtin, unused-import
-    array as Array,
-    array as Scalar,
     dtype as dtype,
+    ndarray as Array,
+    ndarray as Scalar,
 )
