83d4669a453e30395b06d229e4848d9623d35918
Nicholas Krämer <nicokraemer@gmx.net>
2022-04-23 10:46:46 +0200

    Introduce MarkovSequence (#646)
    * Renamed _markov_process module to _markov
    
    * Extract reusable information from Markov processes
    
    * Distinguish MarkovSequence and MarkovProcess through transition types.
    
    * Import Markov sequence into toplevel namespace
    
    * Markov* inputs are keyword-only
    
    * Raise TypeError if MarkovProcess and MarkovSequence are mixed up.
    
    * moved sample_at_input to interface
    
    * delete redundant tests for auxiliary functions
    
    * Improved error messages.
    
    * Unified the functionality of MarkovProcess and MarkovSequence for now
    
    * Updated output of problem zoo
    
    * Fixed tests
    
    * Update EKF/UKF test to MarkovSequence interface
    
    * Correct else-return
    
    * Corrected string concatenation
    
    * Rudimentary docstring for MarkovSequence
    
    * Updated benchmarks to MarkovSequence where necessary
    
    * Use MarkovSequence in docs
    
    * Added an accidentally-deleted line back in.
    
    * Make codecov ignore an exception branch
    
    Co-authored-by: Marvin Pförtner <marvin.pfoertner@icloud.com>
    
    Co-authored-by: Marvin Pförtner <marvin.pfoertner@icloud.com>

--- src/probnum/randprocs/markov/utils/_generate_measurements.py
+++ src/probnum/randprocs/markov/utils/_generate_measurements.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 
-from probnum.randprocs.markov import _markov_process, _transition
+from probnum.randprocs.markov import _markov, _transition
 
 
 def generate_artificial_measurements(
     rng: np.random.Generator,
-    prior_process: _markov_process.MarkovProcess,
+    prior_process: _markov.MarkovProcess,
     measmod: _transition.Transition,
     times: np.ndarray,
 ):
