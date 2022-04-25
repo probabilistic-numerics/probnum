4ac2acd9f0fd5a23fd93078c74f345e058f6ae16
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2022-04-25 18:42:27 +0200

    Remove refactored tests from old `Normal` test file

--- tests/test_randvars/test_normal.py
+++ tests/test_randvars/test_normal.py
@@ -3,7 +3,6 @@ import itertools
 import unittest
 
 import numpy as np
-import scipy.linalg
 import scipy.sparse
 import scipy.stats
 
@@ -15,64 +14,6 @@ from tests.testing import NumpyAssertions
 
 class NormalTestCase(unittest.TestCase, NumpyAssertions):
     """General test case for the normal distribution."""
-
-    def setUp(self):
-        """Resources for tests."""
-
-        # Seed
-        self.seed = 42
-        self.rng = np.random.default_rng(seed=self.seed)
-
-        # Parameters
-        m = 7
-        n = 3
-        self.constants = [-1, -2.4, 0, 200, np.pi]
-        sparsemat = scipy.sparse.rand(m=m, n=n, density=0.1, random_state=self.rng)
-        self.normal_params = [
-            # Univariate
-            (-1.0, 3.0),
-            (1, 3),
-            # Multivariate
-            (np.random.uniform(size=10), np.eye(10)),
-            (np.random.uniform(size=10), random_spd_matrix(rng=self.rng, dim=10)),
-            # Matrixvariate
-            (
-                np.random.uniform(size=(2, 2)),
-                linops.SymmetricKronecker(
-                    A=np.array([[1.0, 2.0], [2.0, 10.0]]),
-                    B=np.array([[5.0, -1.0], [-1.0, 10.0]]),
-                ).todense(),
-            ),
-            # Operatorvariate
-            (
-                np.array([1.0, -5.0]),
-                linops.Matrix(A=np.array([[2.0, 1.0], [1.0, 1.0]])),
-            ),
-            (
-                linops.Matrix(A=np.array([[0.0, -5.0]])),
-                linops.Identity(shape=(2, 2)),
-            ),
-            (
-                np.array([[1.0, 2.0], [-3.0, -0.4], [4.0, 1.0]]),
-                linops.Kronecker(A=np.eye(3), B=5 * np.eye(2)),
-            ),
-            (
-                linops.Matrix(A=sparsemat.todense()),
-                linops.Kronecker(linops.Identity(m), linops.Identity(n)),
-            ),
-            (
-                linops.Matrix(A=np.random.uniform(size=(2, 2))),
-                linops.SymmetricKronecker(
-                    A=np.array([[1.0, 2.0], [2.0, 10.0]]),
-                    B=np.array([[5.0, -1.0], [-1.0, 10.0]]),
-                ),
-            ),
-            # Symmetric Kronecker Identical Factors
-            (
-                linops.Identity(shape=25),
-                linops.SymmetricKronecker(A=linops.Identity(25)),
-            ),
-        ]
 
     def test_scalarmult(self):
         """Multiply a rv with a normal distribution with a scalar."""
@@ -88,191 +29,6 @@ class NormalTestCase(unittest.TestCase, NumpyAssertions):
                     self.assertIsInstance(normrv, randvars.Normal)
                 else:
                     self.assertIsInstance(normrv, randvars.Constant)
-
-    def test_addition_normal(self):
-        """Add two random variables with a normal distribution."""
-        for (mean0, cov0), (mean1, cov1) in list(
-            itertools.product(self.normal_params, self.normal_params)
-        ):
-            with self.subTest():
-                normrv0 = randvars.Normal(mean=mean0, cov=cov0)
-                normrv1 = randvars.Normal(mean=mean1, cov=cov1)
-
-                if normrv0.shape == normrv1.shape:
-                    try:
-                        newmean = mean0 + mean1
-                        newcov = cov0 + cov1
-                    except TypeError:
-                        continue
-
-                    self.assertIsInstance(normrv0 + normrv1, randvars.Normal)
-                else:
-                    with self.assertRaises(ValueError):
-                        normrv_added = normrv0 + normrv1
-
-    def test_indexing(self):
-        """Indexing with Python integers yields a univariate normal distribution."""
-        for mean, cov in self.normal_params:
-            rv = randvars.Normal(mean=mean, cov=cov)
-
-            with self.subTest():
-                # Sample random index
-                idx = tuple(np.random.randint(dim_size) for dim_size in rv.shape)
-
-                # Index into distribution
-                indexed_rv = rv[idx]
-
-                self.assertIsInstance(indexed_rv, randvars.Normal)
-
-                # Compare with expected parameter values
-                if rv.ndim == 0:
-                    flat_idx = ()
-                elif rv.ndim == 1:
-                    flat_idx = (idx[0],)
-                else:
-                    assert rv.ndim == 2
-
-                    flat_idx = (idx[0] * rv.shape[1] + idx[1],)
-
-                self.assertEqual(indexed_rv.shape, ())
-                self.assertEqual(indexed_rv.mean, rv.dense_mean[idx])
-                self.assertEqual(indexed_rv.var, rv.var[idx])
-                self.assertEqual(indexed_rv.cov, rv.dense_cov[flat_idx + flat_idx])
-
-    def test_slicing(self):
-        """Slicing into a normal distribution yields a normal distribution of the same
-        type."""
-        for mean, cov in self.normal_params:
-            rv = randvars.Normal(mean=mean, cov=cov)
-
-            def _random_slice(dim_size):
-                start = np.random.randint(0, dim_size)
-                stop = np.random.randint(start + 1, dim_size + 1)
-
-                return slice(start, stop)
-
-            with self.subTest():
-                # Sample random slice objects for each dimension
-                slices = tuple(_random_slice(dim_size) for dim_size in rv.shape)
-
-                # Get slice from distribution
-                sliced_rv = rv[slices]
-
-                # Compare with expected parameter values
-                slice_mask = np.zeros_like(rv.dense_mean, dtype=np.bool_)
-                slice_mask[slices] = True
-                slice_mask = slice_mask.ravel()
-
-                self.assertArrayEqual(sliced_rv.mean, rv.dense_mean[slices])
-                self.assertArrayEqual(sliced_rv.var, rv.var[slices])
-
-                if rv.ndim > 0:
-                    self.assertArrayEqual(
-                        sliced_rv.cov, rv.dense_cov[np.ix_(slice_mask, slice_mask)]
-                    )
-                else:
-                    self.assertArrayEqual(sliced_rv.cov, rv.cov)
-
-    def test_array_indexing(self):
-        """Indexing with 1-dim integer arrays yields a multivariate normal."""
-        for mean, cov in self.normal_params:
-            rv = randvars.Normal(mean=mean, cov=cov)
-
-            if rv.ndim == 0:
-                continue
-
-            with self.subTest():
-                # Sample random indices
-                idcs = tuple(
-                    np.random.randint(dim_shape, size=10) for dim_shape in mean.shape
-                )
-
-                # Index into distribution
-                indexed_rv = rv[idcs]
-
-                self.assertIsInstance(indexed_rv, randvars.Normal)
-
-                # Compare with expected parameter values
-                if len(rv.shape) == 1:
-                    flat_idcs = idcs[0]
-                else:
-                    assert len(rv.shape) == 2
-
-                    flat_idcs = idcs[0] * rv.shape[1] + idcs[1]
-
-                self.assertEqual(indexed_rv.shape, (10,))
-
-                self.assertArrayEqual(indexed_rv.mean, rv.dense_mean[idcs])
-                self.assertArrayEqual(indexed_rv.var, rv.var[idcs])
-                self.assertArrayEqual(
-                    indexed_rv.cov, rv.dense_cov[np.ix_(flat_idcs, flat_idcs)]
-                )
-
-    def test_array_indexing_broadcast(self):
-        """Indexing with broadcasted integer arrays yields a matrixvariate normal."""
-        for mean, cov in self.normal_params:
-            rv = randvars.Normal(mean=mean, cov=cov)
-
-            if rv.ndim != 2:
-                continue
-
-            with self.subTest():
-                # Sample random indices
-                idcs = np.ix_(
-                    *tuple(
-                        np.random.randint(dim_shape, size=10) for dim_shape in rv.shape
-                    )
-                )
-
-                # Index into distribution
-                indexed_rv = rv[idcs]
-
-                self.assertIsInstance(indexed_rv, randvars.Normal)
-                self.assertEqual(indexed_rv.shape, (10, 10))
-
-                # Compare with expected parameter values
-                flat_idcs = np.broadcast_arrays(*idcs)
-                flat_idcs = flat_idcs[0] * rv.shape[1] + flat_idcs[1]
-                flat_idcs = flat_idcs.ravel()
-
-                self.assertArrayEqual(indexed_rv.mean, rv.dense_mean[idcs])
-                self.assertArrayEqual(indexed_rv.var, rv.var[idcs])
-                self.assertArrayEqual(
-                    indexed_rv.cov, rv.dense_cov[np.ix_(flat_idcs, flat_idcs)]
-                )
-
-    def test_masking(self):
-        """Masking a multivariate or matrixvariate normal yields a multivariate
-        normal."""
-        for mean, cov in self.normal_params:
-            rv = randvars.Normal(mean=mean, cov=cov)
-
-            with self.subTest():
-                # Sample random indices
-                idcs = tuple(
-                    np.random.randint(dim_shape, size=10) for dim_shape in rv.shape
-                )
-
-                mask = np.zeros_like(rv.dense_mean, dtype=np.bool_)
-                mask[idcs] = True
-
-                # Mask distribution
-                masked_rv = rv[mask]
-
-                self.assertIsInstance(masked_rv, randvars.Normal)
-
-                # Compare with expected parameter values
-                flat_mask = mask.flatten()
-
-                self.assertArrayEqual(masked_rv.mean, rv.dense_mean[mask])
-                self.assertArrayEqual(masked_rv.var, rv.var[mask])
-
-                if rv.ndim == 0:
-                    self.assertArrayEqual(masked_rv.cov, rv.cov)
-                else:
-                    self.assertArrayEqual(
-                        masked_rv.cov, rv.dense_cov[np.ix_(flat_mask, flat_mask)]
-                    )
 
 
 class UnivariateNormalTestCase(unittest.TestCase, NumpyAssertions):
@@ -377,25 +133,6 @@ class UnivariateNormalTestCase(unittest.TestCase, NumpyAssertions):
 
 
 class MultivariateNormalTestCase(unittest.TestCase, NumpyAssertions):
-    def setUp(self):
-
-        self.seed = 42
-        self.rng = np.random.default_rng(self.seed)
-
-        self.params = (
-            self.rng.uniform(size=10),
-            random_spd_matrix(rng=self.rng, dim=10),
-        )
-
-    def test_newaxis(self):
-        vector_rv = randvars.Normal(*self.params)
-
-        matrix_rv = vector_rv[:, np.newaxis]
-
-        self.assertEqual(matrix_rv.shape, (10, 1))
-        self.assertArrayEqual(np.squeeze(matrix_rv.mean), vector_rv.mean)
-        self.assertArrayEqual(matrix_rv.cov, vector_rv.cov)
-
     def test_reshape(self):
         rv = randvars.Normal(*self.params)
 
@@ -548,11 +285,6 @@ class MultivariateNormalTestCase(unittest.TestCase, NumpyAssertions):
 
 
 class MatrixvariateNormalTestCase(unittest.TestCase, NumpyAssertions):
-    def setUp(self):
-        # Seed
-        self.seed = 42
-        self.rng = np.random.default_rng(seed=self.seed)
-
     def test_reshape(self):
         rv = randvars.Normal(
             mean=np.random.uniform(size=(4, 3)),
