5fc05c68de93b4832faad63e00b42cd1df1dd5fc
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-11-17 13:15:40 +0100

    fix

--- src/probnum/quad/_bayesquad.py
+++ src/probnum/quad/_bayesquad.py
@@ -127,7 +127,7 @@ def bayesquad(
     -----
         If multiple stopping conditions are provided, the method stops once one of
         them is satisfied. If no stopping condition is provided, the default values are
-        ``max_evals = 25 * input_dim`` and ``var_tol = 1e-8``.
+        ``max_evals = 25 * input_dim`` and ``var_tol = 1e-6``.
 
     See Also
     --------
