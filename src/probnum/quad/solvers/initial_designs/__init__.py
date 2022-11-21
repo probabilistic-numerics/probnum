de253b69613d5a9028a2b9ab78d6314df3a8427c
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-11-21 15:26:40 +0100

    formatting

--- src/probnum/quad/solvers/initial_designs/__init__.py
+++ src/probnum/quad/solvers/initial_designs/__init__.py
@@ -1,11 +1,13 @@
 """Initial designs for Bayesian quadrature."""
 
+from ._bmc_design import BMCDesign
 from ._initial_design import InitialDesign
+from ._latin_design import LatinDesign
 
 # Public classes and functions. Order is reflected in documentation.
-__all__ = [
-    "InitialDesign",
-]
+__all__ = ["InitialDesign", "BMCDesign", "LatinDesign"]
 
 # Set correct module paths. Corrects links and module paths in documentation.
 InitialDesign.__module__ = "probnum.quad.solvers.initial_designs"
+BMCDesign.__module__ = "probnum.quad.solvers.initial_designs"
+LatinDesign.__module__ = "probnum.quad.solvers.initial_designs"
