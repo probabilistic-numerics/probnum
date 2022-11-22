92cf89c99b18e840db10c81729bee27ad07d099e
Maren Mahsereci <maren.mhsrc@gmail.com>
2022-11-22 19:24:41 +0100

    tests for policies

--- tests/test_quad/test_policy.py
+++ tests/test_quad/test_policy.py
@@ -4,9 +4,90 @@ import numpy as np
 import pytest
 
 from probnum.quad.integration_measures import GaussianMeasure, LebesgueMeasure
-from probnum.quad.solvers.policies import VanDerCorputPolicy
+from probnum.quad.solvers import BQState
+from probnum.quad.solvers.policies import Policy, RandomPolicy, VanDerCorputPolicy
+from probnum.randprocs.kernels import ExpQuad
 
-# Todo: tests for other policies
+
+@pytest.fixture
+def batch_size():
+    return 3
+
+
+@pytest.fixture
+def bq_state_no_data(input_dim):
+    return BQState(
+        measure=LebesgueMeasure(input_dim=input_dim, domain=(0, 1)),
+        kernel=ExpQuad(input_shape=(input_dim,)),
+    )
+
+
+@pytest.fixture
+def bq_state(input_dim):
+    nevals = 5
+    return BQState(
+        measure=LebesgueMeasure(input_dim=input_dim, domain=(0, 1)),
+        kernel=ExpQuad(input_shape=(input_dim,)),
+        nodes=np.zeros([nevals, input_dim]),
+        fun_evals=np.ones(nevals),
+    )
+
+
+@pytest.fixture
+def sample_func(batch_size, input_dim, rng):
+    def f(batch_size, rng):
+        return np.ones([batch_size, input_dim])
+
+    return f
+
+
+@pytest.fixture(
+    params=[
+        pytest.param(sc, id=sc[0].__name__)
+        for sc in [
+            (RandomPolicy, dict(batch_size="batch_size", sample_func="sample_func")),
+        ]
+    ],
+    name="policy",
+)
+def fixture_policy(request) -> Policy:
+    """Policies that only allow univariate inputs need to be handled separately."""
+    params = {}
+    for key in request.param[1]:
+        params[key] = request.getfixturevalue(request.param[1][key])
+    return request.param[0](**params)
+
+
+def test_policy_shapes(policy, batch_size, rng, input_dim, bq_state, bq_state_no_data):
+
+    # bq state contains data
+    assert policy(bq_state=bq_state, rng=rng).shape == (batch_size, input_dim)
+
+    # bq state contains no data yet
+    assert policy(bq_state=bq_state_no_data, rng=rng).shape == (batch_size, input_dim)
+
+
+# Tests specific to VanDerCorputPolicy start here
+
+
+def test_van_der_corput_shapes(batch_size, rng):
+    """This is the same test as test_policies_shapes but for 1d only."""
+    measure = LebesgueMeasure(domain=(0, 1))
+    policy = VanDerCorputPolicy(measure=measure, batch_size=batch_size)
+
+    # bq state contains no data yet
+    bq_state_no_data = BQState(measure=measure, kernel=ExpQuad(input_shape=(1,)))
+    assert policy(bq_state=bq_state_no_data, rng=rng).shape == (batch_size, 1)
+
+    # bq state contains data
+    nevals = 5
+    bq_state = BQState(
+        measure=measure,
+        kernel=ExpQuad(input_shape=(1,)),
+        nodes=np.zeros([nevals, 1]),
+        fun_evals=np.ones(nevals),
+    )
+    assert policy(bq_state=bq_state, rng=rng).shape == (batch_size, 1)
 
 
 def test_van_der_corput_multi_d_error():
